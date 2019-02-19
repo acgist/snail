a65945ea49b58af9f8091fdc2b67ca6f7839a171
acgist <289547414@qq.com>
2019-02-19 16:08:06 +0800

    关于页面

--- src/main/java/com/acgist/snail/pojo/config/SystemConfig.java
+++ src/main/java/com/acgist/snail/pojo/config/SystemConfig.java
@@ -9,8 +9,17 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "acgist.system")
 public class SystemConfig {
 
+	private String author;
 	private String source;
 	private String support;
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
 
 	public String getSource() {
 		return source;
--- src/main/java/com/acgist/snail/window/about/AboutController.java
+++ src/main/java/com/acgist/snail/window/about/AboutController.java
@@ -1,23 +1,41 @@
 package com.acgist.snail.window.about;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.acgist.snail.context.SpringContextUtils;
+import com.acgist.snail.pojo.config.SystemConfig;
+import com.acgist.snail.utils.BrowseUtils;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
 /**
  * 关于
  */
-public class AboutController {
+public class AboutController implements Initializable {
+	
+	private SystemConfig systemConfig;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		systemConfig = SpringContextUtils.getBean(SystemConfig.class);
+	}
 	
 	@FXML
 	public void handleAuthorAction(ActionEvent event) {
+		BrowseUtils.open(systemConfig.getAuthor());
 	}
 	
 	@FXML
 	public void handleSourceAction(ActionEvent event) {
+		BrowseUtils.open(systemConfig.getSource());
 	}
 	
 	@FXML
 	public void handleSupportAction(ActionEvent event) {
+		BrowseUtils.open(systemConfig.getSupport());
 	}
 
 }
