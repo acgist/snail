f858b2ed5995bcdaa23738afb0e8bffefe50dc18
acgist <289547414@qq.com>
2019-02-20 17:45:29 +0800

    path

--- builder/config.bat
+++ builder/config.bat
@@ -5,4 +5,3 @@ set version=1.0.0
 set builder=.\builder\
 set runtime=java
 set jar=%project%-%version%.jar
-set path=.\%runtime%\bin
--- builder/startup.bat
+++ builder/startup.bat
@@ -2,6 +2,8 @@
 
 call config.bat
 
+set path=.\%runtime%\bin
+
 rem javaw -server -Xms256m -Xmx256m -jar %jar%
 call javaw -Xms256m -Xmx256m -jar %jar%
 
