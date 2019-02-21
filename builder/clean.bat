8aeca12cc4ddd513e145a9a92dfc35b2aa2bd731
acgist <289547414@qq.com>
2019-02-21 21:36:02 +0800

    添加删除确认

--- builder/clean.bat
+++ builder/clean.bat
@@ -2,6 +2,10 @@
 
 call config.bat
 
+rem 用户确认
+set /p input=请确认删除全部内容（Y/N）？
+if /i %input%==Y (echo 开始删除) else (exit)
+
 rem 删除文件
 echo -----------------------------------------------
 echo 删除文件
@@ -9,4 +13,5 @@ echo -----------------------------------------------
 if exist %jar% del /F /A /Q %jar%
 if exist %lib% rd /S /Q %lib%
 if exist %logs% rd /S /Q %logs%
+rem if exist %database% rd /S /Q %database%
 if exist %runtime% rd /S /Q %runtime%
