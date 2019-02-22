499db3782c93227ade2ef49dbe4c09b95a54761e
acgist <289547414@qq.com>
2019-02-22 17:34:18 +0800

    设置页面

--- src/main/java/com/acgist/snail/repository/impl/ConfigRepository.java
+++ src/main/java/com/acgist/snail/repository/impl/ConfigRepository.java
@@ -12,4 +12,20 @@ public class ConfigRepository extends BaseRepository<ConfigEntity> {
 		super(ConfigEntity.TABLE_NAME);
 	}
 
+	/**
+	 * 更新配置：如果不存在保存，如果存在更新
+	 */
+	public void updateConfig(String name, String value) {
+		ConfigEntity entity = findOne(ConfigEntity.PROPERTY_NAME, name);
+		if(entity == null) {
+			entity = new ConfigEntity();
+			entity.setName(name);
+			entity.setValue(value);
+			save(entity);
+		} else {
+			entity.setValue(value);
+			update(entity);
+		}
+	}
+
 }
--- src/main/java/com/acgist/snail/service/ConfigService.java
+++ src/main/java/com/acgist/snail/service/ConfigService.java
@@ -1,5 +1,7 @@
 package com.acgist.snail.service;
 
+import java.io.File;
+
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +20,8 @@ public class ConfigService {
 	public static final String DOWNLOAD_PATH = "acgist.download.path";
 	public static final String DOWNLOAD_SIZE = "acgist.download.size";
 	public static final String DOWNLOAD_BUFFER = "acgist.download.buffer";
+	public static final String DOWNLOAD_NOTICE = "acgist.download.notice";
+	public static final String DOWNLOAD_P2P = "acgist.download.p2p";
 	
 	private static final ConfigService INSTANCE = new ConfigService();
 	
@@ -31,6 +35,10 @@ public class ConfigService {
 	private String downloadPath;
 	private Integer downloadSize;
 	private Integer downloadBuffer;
+	private Boolean downloadNotice;
+	private Boolean downloadP2p;
+	
+	private ConfigRepository configRepository = new ConfigRepository();
 	
 	static {
 		LOGGER.info("初始化用户配置");
@@ -43,17 +51,18 @@ public class ConfigService {
 	 * 配置文件加载
 	 */
 	private void initFromProperties() {
-		PropertiesUtils propertiesUtils = PropertiesUtils.getInstance("/config/config.properties");
+		PropertiesUtils propertiesUtils = PropertiesUtils.getInstance("/config/config.download.properties");
 		INSTANCE.downloadPath = propertiesUtils.getString(DOWNLOAD_PATH);
 		INSTANCE.downloadSize = propertiesUtils.getInteger(DOWNLOAD_SIZE);
 		INSTANCE.downloadBuffer = propertiesUtils.getInteger(DOWNLOAD_BUFFER);
+		INSTANCE.downloadNotice = propertiesUtils.getBoolean(DOWNLOAD_NOTICE);
+		INSTANCE.downloadP2p = propertiesUtils.getBoolean(DOWNLOAD_P2P);
 	}
 	
 	/**
 	 * 数据库初始化配置
 	 */
 	private void initFromDB() {
-		ConfigRepository configRepository = new ConfigRepository();
 		ConfigEntity entity = null;
 		entity = configRepository.findOne(ConfigEntity.PROPERTY_NAME, DOWNLOAD_PATH);
 		downloadPath = configString(entity, downloadPath);
@@ -61,6 +70,10 @@ public class ConfigService {
 		downloadSize = configInteger(entity, downloadSize);
 		entity = configRepository.findOne(ConfigEntity.PROPERTY_NAME, DOWNLOAD_BUFFER);
 		downloadBuffer = configInteger(entity, downloadBuffer);
+		entity = configRepository.findOne(ConfigEntity.PROPERTY_NAME, DOWNLOAD_NOTICE);
+		downloadNotice = configBoolean(entity, downloadNotice);
+		entity = configRepository.findOne(ConfigEntity.PROPERTY_NAME, DOWNLOAD_P2P);
+		downloadP2p = configBoolean(entity, downloadP2p);
 	}
 	
 	/**
@@ -87,30 +100,82 @@ public class ConfigService {
 	}
 	
 	/**
+	 * 获取Integer配置
+	 */
+	private Boolean configBoolean(ConfigEntity entity, Boolean defaultValue) {
+		if(entity != null) {
+			String value = entity.getValue();
+			if(StringUtils.isNotEmpty(value)) {
+				return Boolean.valueOf(value);
+			}
+		}
+		return defaultValue;
+	}
+	
+	/**
 	 * 日志记录
 	 */
 	private void loggerInfo() {
 		LOGGER.info("下载路径，属性：{}，值：{}", DOWNLOAD_PATH, downloadPath);
 		LOGGER.info("同时下载任务数量，属性：{}，值：{}", DOWNLOAD_SIZE, downloadSize);
 		LOGGER.info("单个任务下载速度（KB），属性：{}，值：{}", DOWNLOAD_BUFFER, downloadBuffer);
+		LOGGER.info("下载完成弹出提示，属性：{}，值：{}", DOWNLOAD_NOTICE, downloadNotice);
+		LOGGER.info("启用P2P加速，属性：{}，值：{}", DOWNLOAD_P2P, downloadP2p);
 	}
 	
-	/**
-	 * 保存配置到数据库
-	 */
-	public void saveToDB() {
+	public void setDownloadPath(String path) {
+		this.downloadPath = path;
+		configRepository.updateConfig(DOWNLOAD_PATH, path);
 	}
-
+	
 	public String getDownloadPath() {
-		return downloadPath;
+		File file = new File(this.downloadPath);
+		if(file.exists()) {
+			return this.downloadPath;
+		}
+		String path = System.getProperty("user.dir") + this.downloadPath;
+		file = new File(path);
+		if(file.exists()) {
+			return path;
+		}
+		file.mkdirs();
+		return path;
+	}
+	
+	public void setDownloadSize(Integer downloadSize) {
+		this.downloadSize = downloadSize;
+		configRepository.updateConfig(DOWNLOAD_SIZE, String.valueOf(downloadSize));
 	}
 
 	public Integer getDownloadSize() {
 		return downloadSize;
 	}
-
+	
+	public void setDownloadBuffer(Integer downloadBuffer) {
+		this.downloadBuffer = downloadBuffer;
+		configRepository.updateConfig(DOWNLOAD_BUFFER, String.valueOf(downloadBuffer));
+	}
+	
 	public Integer getDownloadBuffer() {
 		return downloadBuffer;
+	}
+
+	public void setDownloadNotice(Boolean downloadNotice) {
+		this.downloadNotice = downloadNotice;
+		configRepository.updateConfig(DOWNLOAD_NOTICE, String.valueOf(downloadNotice));
+	}
+	
+	public Boolean getDownloadNotice() {
+		return downloadNotice;
+	}
+	
+	public void setDownloadP2p(Boolean downloadP2p) {
+		this.downloadP2p = downloadP2p;
+		configRepository.updateConfig(DOWNLOAD_P2P, String.valueOf(downloadP2p));
+	}
+	
+	public Boolean getDownloadP2p() {
+		return downloadP2p;
 	}
 	
 }
