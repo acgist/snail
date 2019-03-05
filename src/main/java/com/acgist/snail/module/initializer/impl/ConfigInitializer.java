af5dd803756ffbd962cc591cef092238d22cc508
acgist <289547414@qq.com>
2019-03-05 15:05:55 +0800

    启动初始化

--- src/main/java/com/acgist/snail/module/initializer/impl/ConfigInitializer.java
+++ src/main/java/com/acgist/snail/module/initializer/impl/ConfigInitializer.java
@@ -3,6 +3,9 @@ package com.acgist.snail.module.initializer.impl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.acgist.snail.module.config.DatabaseConfig;
+import com.acgist.snail.module.config.DownloadConfig;
+import com.acgist.snail.module.config.SystemConfig;
 import com.acgist.snail.module.initializer.Initializer;
 
 /**
@@ -12,9 +15,19 @@ public class ConfigInitializer extends Initializer {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ConfigInitializer.class);
 	
+	private ConfigInitializer() {
+	}
+	
+	public static final ConfigInitializer newInstance() {
+		return new ConfigInitializer();
+	}
+	
 	@Override
 	protected void init() {
 		LOGGER.info("初始化配置信息");
+		SystemConfig.getAuthor();
+		DatabaseConfig.getDriver();
+		DownloadConfig.getDownloadBuffer();
 	}
 
 }
