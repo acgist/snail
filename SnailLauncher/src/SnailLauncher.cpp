aa5f5cfc45203fb5bd40dd066c001d971a1cc442
acgist <289547414@qq.com>
2019-03-18 11:44:29 +0800

    exe launcher

---
+++ SnailLauncher/src/SnailLauncher.cpp
@@ -0,0 +1,133 @@
+// Snail启动器：SnailLauncher
+
+#include "stdafx.h"
+#include "SnailLauncher.h"
+#include "jni.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// 加载JVM启动JAVA程序
+bool startJVM();
+typedef jint(JNICALL* JNICREATEPROC) (JavaVM**, void**, void*);
+
+// SnailLauncher
+
+BEGIN_MESSAGE_MAP(SnailLauncher, CWinApp)
+END_MESSAGE_MAP()
+
+// SnailLauncher 构造
+
+SnailLauncher::SnailLauncher()
+{
+	// 应用程序 ID：CompanyName.ProductName.SubProduct.VersionInformation
+	SetAppID(_T("acgist.Snail.SnailLauncher.1.0.0"));
+}
+
+// 唯一的一个 SnailLauncher 对象
+
+SnailLauncher launcher;
+
+// SnailLauncher 初始化
+
+BOOL SnailLauncher::InitInstance()
+{
+	CWinApp::InitInstance();
+
+	EnableTaskbarInteraction(FALSE);
+
+	startJVM();
+
+	return TRUE;
+}
+
+int SnailLauncher::ExitInstance()
+{
+	return CWinApp::ExitInstance();
+}
+
+// 启动JVM
+bool startJVM(){
+	// JVM动态库
+	TCHAR* jvmPath = _T(".\\java\\bin\\server\\jvm.dll");
+ 
+	//JVM启动参数
+	const int jvmOptionCount = 4;
+	JavaVMOption jvmOptions[jvmOptionCount];
+	jvmOptions[0].optionString = "-server";
+	jvmOptions[1].optionString = "-Xmx128M";
+	jvmOptions[2].optionString = "-Xmx128m";
+	jvmOptions[3].optionString = "-Djava.class.path=.\\snail-1.0.0.jar";
+ 
+	JavaVMInitArgs jvmInitArgs;
+	jvmInitArgs.version = JNI_VERSION_10;
+	jvmInitArgs.options = jvmOptions;
+	jvmInitArgs.nOptions = jvmOptionCount;
+
+	// 忽略无法识别jvm的情况
+	jvmInitArgs.ignoreUnrecognized = JNI_TRUE;
+ 
+	// 设置启动类，注意分隔符：（/），不能设置（.）
+	const char startClass[] = "com/acgist/main/Application";
+
+	// 启动方法，设置main函数
+	const char startMethod[] = "main";
+
+	// 传入参数
+	// int nParamCount = 2;
+	// const char* params[nParamCount] = {"a","b"};
+ 
+	// 加载JVM DLL动态链接库
+	HINSTANCE jvmDLL = LoadLibrary(jvmPath);
+	if(jvmDLL == NULL){
+		DWORD x = GetLastError();
+		//outLog("加载JVM动态库错误",GetLastError());
+		return false;
+	}
+ 
+	// 初始化JVM
+	JNICREATEPROC jvmProcAddress = (JNICREATEPROC) GetProcAddress(jvmDLL, "JNI_CreateJavaVM");
+	if(jvmDLL == NULL){
+		FreeLibrary(jvmDLL);
+		//outLog("初始化JVM物理地址失败", GetLastError());
+		return false;
+	}
+
+	// 创建JVM
+	JNIEnv* env;
+	JavaVM* jvm;
+	jint jvmProc = (jvmProcAddress) (&jvm, (void**) &env, &jvmInitArgs);
+	if(jvmProc < 0 || jvm == NULL ||env == NULL){
+		FreeLibrary(jvmDLL);
+	//	outLog("创建JVM错误",GetLastError());
+		return false;
+	}
+ 
+	// 加载启动类
+	jclass mainClass = env -> FindClass(startClass);
+	if(env -> ExceptionCheck() == JNI_TRUE || mainClass == NULL){
+		env -> ExceptionDescribe();
+		env -> ExceptionClear();
+		FreeLibrary(jvmDLL);
+	//	outLog("加载启动类失败",GetLastError());
+		return false;
+	}
+ 
+	// 加载启动方法
+	jmethodID methedID = env -> GetStaticMethodID(mainClass, startMethod, "([Ljava/lang/String;)V");
+	if(env -> ExceptionCheck() == JNI_TRUE || methedID == NULL){
+		env -> ExceptionDescribe();
+		env -> ExceptionClear();
+		FreeLibrary(jvmDLL);
+	//	outLog("加载启动方法失败",GetLastError());
+		return false;
+	}
+	
+	env -> CallStaticVoidMethod(mainClass, methedID, NULL);
+ 
+	// 释放JVM
+	jvm -> DestroyJavaVM();
+
+	return true;
+}
