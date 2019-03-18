657acb59e7298072bd31493648a01c609aeb0f3f
acgist <289547414@qq.com>
2019-03-18 17:44:40 +0800

    BitSet

--- src/main/java/com/acgist/snail/net/peer/PeerClient.java
+++ src/main/java/com/acgist/snail/net/peer/PeerClient.java
@@ -6,6 +6,7 @@ import com.acgist.snail.net.message.impl.PeerMessageHandler;
 /**
  * Peer客户端<br>
  * 基本协议：TCP<br>
+ * 位操作：BitSet<br>
  * https://blog.csdn.net/p312011150/article/details/81478237
  */
 public class PeerClient extends AbstractTcpClient<PeerMessageHandler> {
