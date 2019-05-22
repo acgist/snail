72189ec169a9d1317256c272bdf0518f4c570ef0
acgist <289547414@qq.com>
2019-05-22 18:51:36 +0800

    代码优化

--- src/test/java/com/acgist/main/UDPTest.java
+++ src/test/java/com/acgist/main/UDPTest.java
@@ -2,13 +2,12 @@ package com.acgist.main;
 
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.DatagramChannel;
 
 import org.junit.Test;
 
 import com.acgist.snail.net.UdpClient;
+import com.acgist.snail.net.UdpServer;
 import com.acgist.snail.system.exception.NetException;
-import com.acgist.snail.utils.NetUtils;
 import com.acgist.snail.utils.ThreadUtils;
 
 public class UDPTest {
@@ -17,35 +16,36 @@ public class UDPTest {
 	public void client() {
 		final int port = 18888;
 		InetSocketAddress address = new InetSocketAddress("127.0.0.1", port);
-		DatagramChannel channel = NetUtils.buildUdpChannel(port);
 		UdpTestMessageHandler handler = new UdpTestMessageHandler();
+		UdpServer<UdpTestMessageHandler> server = new UdpServer<>(port, "TestServer", UdpTestMessageHandler.class);
+		server.handler();
 		UdpClient<UdpTestMessageHandler> client = new UdpClient<UdpTestMessageHandler>("TestClient", handler, address) {
 			@Override
 			public boolean open() {
 				return this.open(channel);
 			}
 		};
-		client.open(channel);
+		client.open(server.channel());
 		UdpClient<UdpTestMessageHandler> clients = new UdpClient<UdpTestMessageHandler>("TestClient", handler, address) {
 			@Override
 			public boolean open() {
 				return this.open(channel);
 			}
 		};
-		clients.open(channel);
+		clients.open(server.channel());
 		while (true) {
 			try {
 				final String message = System.currentTimeMillis() + "";
 				final String sendMessage = message.repeat(1);
 				System.out.println("发送消息：" + sendMessage);
 				System.out.println("消息长度：" + sendMessage.length());
-				client.send(ByteBuffer.wrap(sendMessage.getBytes()), new InetSocketAddress("127.0.0.1", port));
-				clients.send(ByteBuffer.wrap(sendMessage.getBytes()), new InetSocketAddress("127.0.0.1", port));
+				client.send(ByteBuffer.wrap((sendMessage + "A").getBytes()), new InetSocketAddress("127.0.0.1", port));
+				clients.send(ByteBuffer.wrap((sendMessage + "B").getBytes()), new InetSocketAddress("127.0.0.1", port));
 			} catch (NetException e) {
 				e.printStackTrace();
 			}
 //			ThreadUtils.sleep(Long.MAX_VALUE);
-			ThreadUtils.sleep(1000);
+			ThreadUtils.sleep(10);
 		}
 	}
 	
