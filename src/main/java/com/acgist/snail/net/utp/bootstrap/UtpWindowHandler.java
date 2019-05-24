0067384f6c50708f5c46ae413b4dfe7bf2a089bd
acgist <289547414@qq.com>
2019-05-24 18:55:50 +0800

    消息处理

--- src/main/java/com/acgist/snail/net/utp/bootstrap/UtpWindowHandler.java
+++ src/main/java/com/acgist/snail/net/utp/bootstrap/UtpWindowHandler.java
@@ -1,10 +1,13 @@
 package com.acgist.snail.net.utp.bootstrap;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import com.acgist.snail.system.config.UtpConfig;
+import com.acgist.snail.system.exception.NetException;
 
 /**
  * 滑块窗口
@@ -37,14 +40,30 @@ public class UtpWindowHandler {
 	/**
 	 * 设置buffer，如果是下一个滑块直接返回，否者缓存，等待下一个返回null。
 	 */
-	public synchronized ByteBuffer put(final short seqnr, final ByteBuffer buffer) {
-		put(seqnr, buffer);
-		ByteBuffer tmp = take(this.nextSeqnr);
-		if(tmp == null) {
-			return null;
-		} else {
-			
+	public synchronized ByteBuffer put(final short seqnr, final ByteBuffer buffer) throws NetException {
+		storage(seqnr, buffer);
+		byte[] tmpBytes;
+		ByteBuffer tmpBuffer;
+		final ByteArrayOutputStream output = new ByteArrayOutputStream();
+		while(true) {
+			tmpBuffer = take(this.nextSeqnr);
+			if(tmpBuffer == null) {
+				break;
+			} else {
+				tmpBytes = new byte[tmpBuffer.remaining()];
+				tmpBuffer.get(tmpBytes);
+				try {
+					output.write(tmpBytes);
+				} catch (IOException e) {
+					throw new NetException("UTP消息处理异常", e);
+				}
+			}
 		}
+		final byte[] bytes = output.toByteArray();
+		if(bytes.length == 0) {
+			return null;
+		}
+		return ByteBuffer.wrap(bytes);
 	}
 	
 	private ByteBuffer take(final short seqnr) {
