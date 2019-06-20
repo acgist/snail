46b2cc89f7ca87cac1d3d1191cc384651736f9bb
acgist <289547414@qq.com>
2019-06-20 15:15:35 +0800

    修复迅雷链接==问题

--- src/test/java/com/acgist/snail/thunder/ThunderTest.java
+++ src/test/java/com/acgist/snail/thunder/ThunderTest.java
@@ -10,11 +10,10 @@ public class ThunderTest {
 
 	@Test
 	public void test() {
-		String url = "thunder://QUFmdHA6Ly95Z2R5ODp5Z2R5OEB5ZzQ1LmR5ZHl0dC5uZXQ6NDEyMi/R9LnitefTsHd3dy55Z2R5OC5jb20usNfJ36O61LXG8C5IRC43MjBwLrn60+/W0NfWLm1rdlpa";
+		String url = "thunder://QUFodHRwOi8vdXBvcy1oei1taXJyb3Jic3l1LmFjZ3ZpZGVvLmNvbS91cGdjeGNvZGUvMjIvNjkvMTI0NDY5MjIvMTI0NDY5MjItMS02NC5mbHY/ZT1pZzhldXhaTTJyTmNOYmhIaGJVVmhvTXpuV05CaHdkRXRvOGc1WDEwdWdOY1hCQl8mZGVhZGxpbmU9MTU2MTAyMTI1NCZnZW49cGxheXVybCZuYnM9MSZvaT0xNzAzMTc4Nzk0Jm9zPWJzeXUmcGxhdGZvcm09aHRtbDUmdHJpZD1kZWIzMTdkMjI0NDc0ZDg5YWI4YmI1ZDgzNWMzMGY3MyZ1aXBrPTUmdXBzaWc9YWY3NmExOTUyYjFlNjZhYmQ0NzBiNmRmOWYyNTA2MWImdXBhcmFtcz1lLGRlYWRsaW5lLGdlbixuYnMsb2ksb3MscGxhdGZvcm0sdHJpZCx1aXBrJm1pZD00NTU5MjY3Wlo==";
 		url = url.substring(ThunderProtocol.THUNDER_PREFIX.length());
-		String newUrl = new String(Base64.getDecoder().decode(url));
+		String newUrl = new String(Base64.getMimeDecoder().decode(url));
 		newUrl = newUrl.substring(2, newUrl.length() - 2);
-		
 		System.out.println(newUrl);
 	}
 	
