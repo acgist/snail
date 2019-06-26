3573de490fb38610f2986701b95ec82376e2578c
acgist <289547414@qq.com>
2019-06-26 17:12:27 +0800

    WebSocket Tracker

--- src/main/java/com/acgist/snail/net/torrent/tracker/bootstrap/impl/WssTrackerClient.java
+++ src/main/java/com/acgist/snail/net/torrent/tracker/bootstrap/impl/WssTrackerClient.java
@@ -1,10 +1,16 @@
 package com.acgist.snail.net.torrent.tracker.bootstrap.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.acgist.snail.net.torrent.peer.bootstrap.PeerService;
 import com.acgist.snail.net.torrent.tracker.bootstrap.TrackerClient;
 import com.acgist.snail.net.wss.WebSocketClient;
 import com.acgist.snail.pojo.session.TorrentSession;
 import com.acgist.snail.system.config.ProtocolConfig.Protocol;
+import com.acgist.snail.system.config.TrackerConfig;
 import com.acgist.snail.system.exception.NetException;
+import com.acgist.snail.utils.StringUtils;
 
 /**
  * <p>Tracker WSS（WebSocket） 客户端</p>
@@ -15,6 +21,8 @@ import com.acgist.snail.system.exception.NetException;
  */
 public class WssTrackerClient extends TrackerClient {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(WssTrackerClient.class);
+	
 	private final WebSocketClient client;
 	
 	private WssTrackerClient(String scrapeUrl, String announceUrl) throws NetException {
@@ -28,19 +36,56 @@ public class WssTrackerClient extends TrackerClient {
 	
 	@Override
 	public void announce(Integer sid, TorrentSession torrentSession) throws NetException {
-		this.client.send("1234");
+		this.client.send(buildAnnounceMessage(sid, torrentSession, TrackerConfig.Event.started));
 	}
 
 	@Override
 	public void complete(Integer sid, TorrentSession torrentSession) {
+		try {
+			this.client.send(buildAnnounceMessage(sid, torrentSession, TrackerConfig.Event.completed));
+		} catch (NetException e) {
+			LOGGER.error("WebSocket请求异常", e);
+		}
 	}
 
 	@Override
 	public void stop(Integer sid, TorrentSession torrentSession) {
+		try {
+			this.client.send(buildAnnounceMessage(sid, torrentSession, TrackerConfig.Event.stopped));
+		} catch (NetException e) {
+			LOGGER.error("WebSocket请求异常", e);
+		}
 	}
 
 	@Override
 	public void scrape(Integer sid, TorrentSession torrentSession) throws NetException {
+		// TODO：刮檫
+	}
+	
+	/**
+	 * Announce请求
+	 */
+	private String buildAnnounceMessage(Integer sid, TorrentSession torrentSession, TrackerConfig.Event event) {
+		long download = 0L, remain = 0L, upload = 0L;
+		final var taskSession = torrentSession.taskSession();
+		if(taskSession != null) {
+			var statistics = taskSession.statistics();
+			download = statistics.downloadSize();
+			remain = taskSession.entity().getSize() - download;
+			upload = statistics.uploadSize();
+		}
+		final StringBuffer buffer = new StringBuffer();
+		buffer.append("{");
+		buffer.append("\"info_hash\"").append(":").append("\"").append(StringUtils.hex(torrentSession.infoHash().infoHash())).append("\"").append(",");
+		buffer.append("\"peer_id\"").append(":").append("\"").append(StringUtils.hex(PeerService.getInstance().peerId())).append("\"").append(",");
+		buffer.append("\"uploaded\"").append(":").append(upload).append(",");
+		buffer.append("\"downloaded\"").append(":").append(download).append(",");
+		buffer.append("\"left\"").append(":").append(remain).append(",");
+		buffer.append("\"event\"").append(":").append("\"").append(event.name()).append("\"").append(",");
+		buffer.append("\"action\"").append(":").append("\"").append(TrackerConfig.Action.announce.name()).append("\"").append(",");
+		buffer.append("\"numwant\"").append(":").append("50");
+		buffer.append("}");
+		return buffer.toString();
 	}
 	
 }
--- src/main/java/com/acgist/snail/net/wss/bootstrap/WebSocketListener.java
+++ src/main/java/com/acgist/snail/net/wss/bootstrap/WebSocketListener.java
@@ -4,6 +4,9 @@ import java.net.http.WebSocket;
 import java.nio.ByteBuffer;
 import java.util.concurrent.CompletionStage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * WebSocket监听器
  * 
@@ -11,15 +14,48 @@ import java.util.concurrent.CompletionStage;
  * @since 1.1.0
  */
 public class WebSocketListener implements WebSocket.Listener {
-
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(WebSocketListener.class);
+	
+	@Override
+	public void onOpen(WebSocket webSocket) {
+		LOGGER.debug("WebSocket打开成功：{}", webSocket);
+		WebSocket.Listener.super.onOpen(webSocket);
+	}
+	
+	@Override
+	public CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
+		LOGGER.debug("WebSocket接收数据：{}", data);
+		return WebSocket.Listener.super.onText(webSocket, data, last);
+	}
+	
 	@Override
 	public CompletionStage<?> onBinary(WebSocket webSocket, ByteBuffer data, boolean last) {
-		System.out.println("----");
+		LOGGER.debug("WebSocket接收数据：{}", data);
 		return WebSocket.Listener.super.onBinary(webSocket, data, last);
 	}
 	
 	@Override
+	public CompletionStage<?> onPing(WebSocket webSocket, ByteBuffer message) {
+		LOGGER.debug("WebSocket Ping");
+		return WebSocket.Listener.super.onPing(webSocket, message);
+	}
+	
+	@Override
+	public CompletionStage<?> onPong(WebSocket webSocket, ByteBuffer message) {
+		LOGGER.debug("WebSocket Pong");
+		return WebSocket.Listener.super.onPong(webSocket, message);
+	}
+	
+	@Override
+	public CompletionStage<?> onClose(WebSocket webSocket, int statusCode, String reason) {
+		LOGGER.debug("WebSocket关闭：{}，{}", statusCode, reason);
+		return WebSocket.Listener.super.onClose(webSocket, statusCode, reason);
+	}
+	
+	@Override
 	public void onError(WebSocket webSocket, Throwable error) {
+		LOGGER.debug("WebSocket发生异常", error);
 		WebSocket.Listener.super.onError(webSocket, error);
 	}
 	
