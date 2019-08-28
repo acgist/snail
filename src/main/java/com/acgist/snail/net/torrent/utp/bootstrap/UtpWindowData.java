19b1f8b22e19182513a4bfb4d51ea1a0a4f35514
acgist <289547414@qq.com>
2019-08-28 09:31:39 +0800

    修复UTP超时定时任务重试握手消息负载空指针。

--- src/main/java/com/acgist/snail/net/torrent/utp/bootstrap/UtpWindowData.java
+++ src/main/java/com/acgist/snail/net/torrent/utp/bootstrap/UtpWindowData.java
@@ -15,7 +15,7 @@ public class UtpWindowData {
 	private final short seqnr; // seqnr
 	private int timestamp; // 时间戳（微秒），发送时重新修改。
 	private byte pushTimes; // 发送次数
-	private final byte[] data; // 数据
+	private final byte[] data; // 负载数据：握手时没有负载数据
 	private final int length; // 数据长度
 	
 	private UtpWindowData(short seqnr, int timestamp, byte[] data) {
@@ -55,6 +55,14 @@ public class UtpWindowData {
 
 	public ByteBuffer buffer() {
 		return ByteBuffer.wrap(this.data);
+	}
+	
+	/**
+	 * 验证数据是否正确，正确的数据才能发送。
+	 * 握手失败，导致超时定时任务执行时负载数据为空，导致发送时空指针。
+	 */
+	public boolean verify() {
+		return this.data != null;
 	}
 	
 	/**
