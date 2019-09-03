0c00ef5d73c227d737410d59a5da41298e4591fe
acgist <289547414@qq.com>
2019-09-03 10:04:45 +0800

    修复HTTP Tracker状态判断错误导致的无法获取Peer错误

--- src/test/java/com/acgist/snail/tracker/TrackerClientHttpTest.java
+++ src/test/java/com/acgist/snail/tracker/TrackerClientHttpTest.java
@@ -14,8 +14,8 @@ public class TrackerClientHttpTest {
 	public void test() throws DownloadException, NetException {
 		String path = "e:/snail/16b1233b33143700fe47910898fcaaf0f05d2d09.torrent";
 		TorrentSession session = TorrentManager.getInstance().newTorrentSession(path);
-//		HttpTrackerClient client = HttpTrackerClient.newInstance("http://anidex.moe:6969/announce");
 		HttpTrackerClient client = HttpTrackerClient.newInstance("http://t.nyaatracker.com/announce");
+//		HttpTrackerClient client = HttpTrackerClient.newInstance("http://opentracker.acgnx.se/announce");
 		client.announce(1000, session);
 	}
 	
