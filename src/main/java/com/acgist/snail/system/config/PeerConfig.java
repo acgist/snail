14b3dff03243794e3034a7e7109bb981913e731d
acgist <289547414@qq.com>
2019-09-11 21:22:11 +0800

    代码优化

--- src/main/java/com/acgist/snail/system/config/PeerConfig.java
+++ src/main/java/com/acgist/snail/system/config/PeerConfig.java
@@ -31,6 +31,11 @@ public class PeerConfig {
 	public static final int MAX_PEER_CONNECT_TIMEOUT = 6;
 	
 	/**
+	 * Peer创建超时时间（秒）
+	 */
+	public static final int MAX_PEER_BUILD_TIMEOUT = MAX_PEER_CONNECT_TIMEOUT + 4;
+	
+	/**
 	 * PeerId长度
 	 */
 	public static final int PEER_ID_LENGTH = 20;
