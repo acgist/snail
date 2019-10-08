77a21414596ea478afe02f8518b371c0a7c76efe
acgist <289547414@qq.com>
2019-10-08 17:40:58 +0800

    代码优化

--- src/main/java/com/acgist/snail/net/torrent/peer/bootstrap/ltep/PeerExchangeMessageHandler.java
+++ src/main/java/com/acgist/snail/net/torrent/peer/bootstrap/ltep/PeerExchangeMessageHandler.java
@@ -28,6 +28,7 @@ import com.acgist.snail.utils.PeerUtils;
  * <p>Peer Exchange (PEX)</p>
  * <p>Peer交换，在优化PeerLauncher后获取有效的Peer发送此消息。</p>
  * <p>协议链接：http://www.bittorrent.org/beps/bep_0011.html</p>
+ * 
  * TODO：IPv6
  * 
  * @author acgist
