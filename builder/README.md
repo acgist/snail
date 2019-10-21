eaf00492b068ab3c22b2e8029918c336845cf570
acgist <289547414@qq.com>
2019-10-21 16:27:12 +0800

    Linux

--- builder/README.md
+++ builder/README.md
@@ -1,5 +1,14 @@
 # 构建工具
 
+## Mac
+
+## Linux
+
+- config.sh：配置信息
+- clean.sh：清理脚本
+- build.sh：构建脚本
+- startup.sh：启动脚本
+
 ## Windows
 
 - config.bat：配置信息
@@ -7,11 +16,4 @@
 - build.bat：构建脚本
 - startup.bat：启动脚本
 
-## Linux
-
-- config.bat：配置信息
-- clean.bat：清理脚本
-- build.bat：构建脚本
-- startup.bat：启动脚本
-
-## Mac
+> 脚本只能在builder目录中执行
--- builder/config.sh
+++ builder/config.sh
@@ -1,1 +1,27 @@
 #!/bin/sh
+
+# 软件名称
+project=snail
+# 软件版本
+version=1.2.0
+
+# 编译路径
+builder=./builder/
+# 打包目录
+target="${builder}${project}/"
+
+# 依赖包文件夹
+lib=lib
+# 运行环境文件夹
+runtime=java
+
+# JAR文件
+jar="${project}-${version}.jar"
+
+# SH配置文件
+config=config.sh
+# SH启动文件
+startup=startup.sh
+
+# JAVA依赖模块
+modules="java.xml,java.sql,java.base,java.desktop,java.naming,java.compiler,java.logging,java.scripting,java.instrument,java.management,java.net.http,java.transaction.xa,jdk.crypto.ec,jdk.unsupported"
