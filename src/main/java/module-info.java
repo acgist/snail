5982abc02a293c71f14bc1e4b1c9fc59687e3397
acgist <289547414@qq.com>
2019-11-29 07:24:15 +0800

    system、context

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 /**
  * <h1>Sanil（蜗牛）</h1>
  * <p>基于Java、JavaFX开发的下载工具，支持下载协议：BT（BitTorrent）、FTP、HTTP。</p>
+ * <p>开放整个模块（反射使用）</p>
  * 
  * TODO：稀有块、阻塞算法（Peer）、图表选中切换颜色
  * 
@@ -9,9 +10,7 @@
  */
 open module com.acgist.snail {
 
-	/*
-	 * 限定导出
-	 */
+	//================导出================//
 	exports com.acgist.main;
 	exports com.acgist.snail.gui;
 	exports com.acgist.snail.gui.event;
@@ -20,21 +19,11 @@ open module com.acgist.snail {
 	exports com.acgist.snail.pojo.message;
 	exports com.acgist.snail.protocol;
 	exports com.acgist.snail.downloader;
-
-	/*
-	 * 反射使用
-	 * opens com.acgist.main;
-	 * open module：整个模块
-	 */
-
-	/*
-	 * Java：不能使用transitive
-	 */
-	requires java.base;
+	exports com.acgist.snail.system;
+	exports com.acgist.snail.system.context;
 	
-	/*
-	 * Java
-	 */
+	//================Java================//
+	requires java.base; // java.base不能使用transitive修饰
 	requires transitive java.sql;
 	requires transitive java.xml;
 	requires transitive java.naming;
@@ -47,22 +36,16 @@ open module com.acgist.snail {
 	requires transitive java.management;
 	requires transitive java.transaction.xa;
 	
-	/*
-	 * JDK
-	 */
+	//================JDK================//
 	requires transitive jdk.crypto.ec;
 	requires transitive jdk.unsupported;
-
-	/*
-	 * JavaFX
-	 */
+	
+	//================JavaFX================//
 	requires transitive javafx.fxml;
 	requires transitive javafx.controls;
 	requires transitive javafx.graphics;
 
-	/*
-	 * 其他：数据库、日志
-	 */
+	//================其他依赖================//
 	requires transitive org.slf4j;
 	requires transitive com.h2database;
 	requires transitive ch.qos.logback.core;
