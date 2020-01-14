d8a69084575246d6eac1f268b03d289063c85080
acgist <289547414@qq.com>
2020-01-15 07:40:43 +0800

    修复HTTP下载文件名称获取错误

--- src/main/java/com/acgist/snail/pojo/wrapper/HttpHeaderWrapper.java
+++ src/main/java/com/acgist/snail/pojo/wrapper/HttpHeaderWrapper.java
@@ -109,8 +109,9 @@ public final class HttpHeaderWrapper extends HeaderWrapper {
 	/**
 	 * <p>获取文件名称</p>
 	 * <p>下载文件名称：如果不存在返回默认的文件名称</p>
-	 * <p>Content-Disposition:attachment;filename=snail.jar?version=1.0.0</p>
 	 * <p>Content-Disposition:attachment;filename="snail.jar"</p>
+	 * <p>Content-Disposition:attachment;filename=snail.jar?version=1.0.0</p>
+	 * <p>Content-Disposition:inline;filename="snail.jar";filename*=utf-8''snail.jar</p>
 	 * 
 	 * @param defaultName 默认文件名称
 	 * 
@@ -123,58 +124,99 @@ public final class HttpHeaderWrapper extends HeaderWrapper {
 		}
 		final String fileNameLower = fileName.toLowerCase();
 		if(fileNameLower.contains(FILENAME)) { // 包含文件名称
-			fileName = UrlUtils.decode(fileName); // URL解码
-			int index = fileName.indexOf("=");
-			if(index != -1) {
-				// 截取名称
-				fileName = fileName.substring(index + 1);
-				// 去掉引号
-				if(fileName.startsWith("\"") && fileName.endsWith("\"")) {
-					fileName = fileName.substring(1, fileName.length() - 1);
-				}
-				// 去掉参数
-				index = fileName.indexOf("?");
-				if(index != -1) {
-					fileName = fileName.substring(0, index);
-				}
-			}
-			fileName = fileName.trim();
-			if(StringUtils.isEmpty(fileName)) {
-				return defaultName;
-			}
-			final var encoder = Charset.forName(SystemConfig.CHARSET_GBK).newEncoder();
-			// 只是进行URL编码
-			if(encoder.canEncode(fileName)) {
-				return fileName;
-			}
-			// HttpClient工具汉字ISO-8859-1字符转为char没有去掉符号（& 0xFF）
-			final char[] chars = fileName.toCharArray();
-			for (int i = 0; i < chars.length; i++) {
-				// 去掉符号
-				chars[i] = (char) (chars[i] & 0x00FF);
-			}
-			fileName = new String(chars);
-			// 处理ISO-8859-1编码
-			// GBK
-			final String fileNameGBK = StringUtils.charset(fileName, SystemConfig.CHARSET_ISO_8859_1, SystemConfig.CHARSET_GBK);
-			// UTF-8
-			final String fileNameUTF8 = StringUtils.charsetFrom(fileName, SystemConfig.CHARSET_ISO_8859_1);
-			/*
-			 * <p>判断依据</p>
-			 * <p>GBK转为UTF8基本乱码</p>
-			 * <p>UTF8转为GBK也会乱码，可能只是不是原来的字符，但是可以也是属于GBK字符。</p>
-			 */
-			if(encoder.canEncode(fileNameUTF8)) {
-				return fileNameUTF8;
-			}
-			if(encoder.canEncode(fileNameGBK)) {
-				return fileNameGBK;
-			}
-			// 其他编码直接返回
-			return fileName;
+			fileName = extractFileName(fileName);
+			return charsetFileName(fileName, defaultName);
 		} else {
 			return defaultName;
 		}
+	}
+
+	/**
+	 * <p>头部信息名称提取</p>
+	 * 
+	 * @param fileName 文件名称
+	 * 
+	 * @return 文件名称
+	 */
+	private String extractFileName(String fileName) {
+		int index;
+		// URL解码
+		fileName = UrlUtils.decode(fileName);
+		// 删除：filename
+		index = fileName.indexOf(FILENAME);
+		if(index != -1) {
+			fileName = fileName.substring(index + FILENAME.length());
+		}
+		// 删除：等号
+		index = fileName.indexOf("=");
+		if(index != -1) {
+			fileName = fileName.substring(index + 1);
+		}
+		// 删除：分号
+		index = fileName.indexOf(";");
+		if(index != -1) {
+			fileName = fileName.substring(0, index);
+		}
+		// 删除：空格
+		fileName = fileName.trim();
+		// 删除：引号
+		if(fileName.startsWith("\'") && fileName.endsWith("\'")) {
+			fileName = fileName.substring(1, fileName.length() - 1);
+		}
+		if(fileName.startsWith("\"") && fileName.endsWith("\"")) {
+			fileName = fileName.substring(1, fileName.length() - 1);
+		}
+		// 删除：参数
+		index = fileName.indexOf("?");
+		if(index != -1) {
+			fileName = fileName.substring(0, index);
+		}
+		// 删除：空格
+		return fileName.trim();
+	}
+	
+	/**
+	 * <p>文件名称解码</p>
+	 * 
+	 * @param fileName 文件名称
+	 * @param defaultName 默认文件名称
+	 * 
+	 * @return 文件名称
+	 */
+	private String charsetFileName(String fileName, final String defaultName) {
+		if(StringUtils.isEmpty(fileName)) {
+			return defaultName;
+		}
+		final var encoder = Charset.forName(SystemConfig.CHARSET_GBK).newEncoder();
+		// 只是进行URL编码
+		if(encoder.canEncode(fileName)) {
+			return fileName;
+		}
+		// HttpClient工具汉字ISO-8859-1字符转为char没有去掉符号（& 0xFF）
+		final char[] chars = fileName.toCharArray();
+		for (int index = 0; index < chars.length; index++) {
+			// 去掉符号
+			chars[index] = (char) (chars[index] & 0x00FF);
+		}
+		fileName = new String(chars);
+		// 处理ISO-8859-1编码
+		// GBK
+		final String fileNameGBK = StringUtils.charset(fileName, SystemConfig.CHARSET_ISO_8859_1, SystemConfig.CHARSET_GBK);
+		// UTF-8
+		final String fileNameUTF8 = StringUtils.charsetFrom(fileName, SystemConfig.CHARSET_ISO_8859_1);
+		/*
+		 * <p>判断依据</p>
+		 * <p>GBK转为UTF8基本乱码</p>
+		 * <p>UTF8转为GBK也会乱码，可能只是不是原来的字符，但是可以也是属于GBK字符。</p>
+		 */
+		if(encoder.canEncode(fileNameUTF8)) {
+			return fileNameUTF8;
+		}
+		if(encoder.canEncode(fileNameGBK)) {
+			return fileNameGBK;
+		}
+		// 其他编码直接返回
+		return fileName;
 	}
 	
 	/**
--- src/test/java/com/acgist/snail/HTTPClientTest.java
+++ src/test/java/com/acgist/snail/HTTPClientTest.java
@@ -1,12 +1,19 @@
 package com.acgist.snail;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.UnsupportedEncodingException;
+import java.net.http.HttpHeaders;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 
 import org.junit.Test;
 
 import com.acgist.snail.net.http.HTTPClient;
+import com.acgist.snail.pojo.wrapper.HttpHeaderWrapper;
 import com.acgist.snail.system.exception.NetException;
 
 public class HTTPClientTest extends BaseTest {
@@ -48,10 +55,45 @@ public class HTTPClientTest extends BaseTest {
 	
 	@Test
 	public void testFileName() throws NetException {
-		var header = HTTPClient.newInstance("https://g18.gdl.netease.com/MY-1.246.1.apk").head();
+//		var header = HTTPClient.newInstance("https://g18.gdl.netease.com/MY-1.246.1.apk").head();
+		var header = HTTPClient.newInstance("http://share.qiniu.easepan.xyz/tool/7tt_setup.exe").head();
 //		var header = HTTPClient.newInstance("https://g37.gdl.netease.com/onmyoji_setup_9.4.0.zip").head();
 		this.log(header);
 		this.log("文件名称：" + header.fileName("test"));
+	}
+	
+	@Test
+	public void mockFileName() throws UnsupportedEncodingException {
+		var headers = HttpHeaders.of(Map.of("Content-Disposition", List.of("attachment;filename='snail.jar'")), (a, b) -> true);
+		HttpHeaderWrapper headerWrapper = HttpHeaderWrapper.newInstance(headers);
+		String filename = headerWrapper.fileName("错误");
+		this.log(filename);
+		assertEquals(filename, "snail.jar");
+		headers = HttpHeaders.of(Map.of("Content-Disposition", List.of("attachment;filename='%e6%b5%8b%e8%af%95.exe'")), (a, b) -> true);
+		headerWrapper = HttpHeaderWrapper.newInstance(headers);
+		filename = headerWrapper.fileName("错误");
+		this.log(filename);
+		assertEquals(filename, "测试.exe");
+		headers = HttpHeaders.of(Map.of("Content-Disposition", List.of("attachment;filename='" + new String("测试.exe".getBytes("GBK"), "ISO-8859-1") + "'")), (a, b) -> true);
+		headerWrapper = HttpHeaderWrapper.newInstance(headers);
+		filename = headerWrapper.fileName("错误");
+		this.log(filename);
+		assertEquals(filename, "测试.exe");
+		headers = HttpHeaders.of(Map.of("Content-Disposition", List.of("attachment;filename=\"snail.jar\"")), (a, b) -> true);
+		headerWrapper = HttpHeaderWrapper.newInstance(headers);
+		filename = headerWrapper.fileName("错误");
+		this.log(filename);
+		assertEquals(filename, "snail.jar");
+		headers = HttpHeaders.of(Map.of("Content-Disposition", List.of("attachment;filename=snail.jar?version=1.0.0")), (a, b) -> true);
+		headerWrapper = HttpHeaderWrapper.newInstance(headers);
+		filename = headerWrapper.fileName("错误");
+		this.log(filename);
+		assertEquals(filename, "snail.jar");
+		headers = HttpHeaders.of(Map.of("Content-Disposition", List.of("inline;filename=\"snail.jar\";filename*=utf-8''snail.jar")), (a, b) -> true);
+		headerWrapper = HttpHeaderWrapper.newInstance(headers);
+		filename = headerWrapper.fileName("错误");
+		this.log(filename);
+		assertEquals(filename, "snail.jar");
 	}
 	
 	@Test
