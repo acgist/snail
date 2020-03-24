16313aff8d07d85feb24484966e21f418da53403
acgist <289547414@qq.com>
2020-03-24 15:54:44 +0800

    代码优化

--- src/main/java/com/acgist/snail/gui/build/BuildWindow.java
+++ src/main/java/com/acgist/snail/gui/build/BuildWindow.java
@@ -35,8 +35,8 @@ public final class BuildWindow extends Window<BuildController> {
 	@Override
 	public void start(Stage stage) throws Exception {
 		this.buildWindow(stage, "新建下载", 600, 300, "/fxml/build.fxml", Modality.APPLICATION_MODAL);
-		disableResize();
-		dialogWindow();
+		this.disableResize();
+		this.dialogWindow();
 	}
 	
 	/**
