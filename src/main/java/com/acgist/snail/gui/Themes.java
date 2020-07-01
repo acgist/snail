f81e2cd2190da31dc6f4ae0692faacf002a16f56
acgist <289547414@qq.com>
2020-07-01 11:05:19 +0800

    主题颜色优化

--- src/main/java/com/acgist/snail/gui/Themes.java
+++ src/main/java/com/acgist/snail/gui/Themes.java
@@ -1,15 +1,11 @@
 package com.acgist.snail.gui;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.acgist.snail.gui.theme.Theme;
+import com.acgist.snail.gui.theme.WindowsTheme;
 import com.acgist.snail.system.context.SystemContext.SystemType;
-import com.acgist.snail.utils.IoUtils;
-import com.acgist.snail.utils.StringUtils;
 
 import javafx.scene.paint.Color;
 
@@ -26,7 +22,7 @@ public final class Themes {
 	/**
 	 * <p>默认主题颜色</p>
 	 */
-	private static final Color DEFAULT_THEME_COLOR = Color.rgb(0, 153, 204);
+	public static final Color DEFAULT_THEME_COLOR = Color.rgb(0, 153, 204);
 	/**
 	 * <p>系统主题颜色</p>
 	 */
@@ -35,35 +31,27 @@ public final class Themes {
 	 * <p>系统主题样式</p>
 	 */
 	private static final String SYSTEM_THEME_STYLE;
-	/**
-	 * <p>Windows主题颜色PATH</p>
-	 */
-	private static final String THEME_COLOR_PATH = "HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\History\\Colors";
-	/**
-	 * <p>Windows主题颜色KEY</p>
-	 */
-	private static final String THEME_COLOR_KEY = "ColorHistory0";
-	/**
-	 * <p>Windows获取主题颜色命令</p>
-	 */
-	private static final String THEME_COLOR_COMMAND = "REG QUERY " + THEME_COLOR_PATH + " /v " + THEME_COLOR_KEY;
-	
+
 	static {
 		final SystemType systemType = SystemType.local();
-		// 获取主题颜色
-		Color color;
-		if(systemType == null) {
-			color = DEFAULT_THEME_COLOR;
-		} else {
+		Theme themeHandler = null;
+		if(systemType != null) {
 			switch (systemType) {
 			case WINDOWS:
-				color = windowsThemeColor();
+				themeHandler = WindowsTheme.newInstance();
 				break;
 				// TODO：Mac、Linux
 			default:
-				color = DEFAULT_THEME_COLOR;
+				LOGGER.info("系统主题没有适配系统类型：{}", systemType);
 				break;
 			}
+		}
+		// 系统主题颜色
+		Color color;
+		if(themeHandler != null) {
+			color = themeHandler.systemThemeColor();
+		} else {
+			color = DEFAULT_THEME_COLOR;
 		}
 		SYSTEM_THEME_COLOR = color;
 		// 设置主题样式
@@ -81,69 +69,6 @@ public final class Themes {
 	 */
 	public static final String getThemeStyle() {
 		return SYSTEM_THEME_STYLE;
-	}
-	
-	/**
-	 * <p>获取Windows主题颜色</p>
-	 * 
-	 * @return 主题颜色
-	 */
-	private static final Color windowsThemeColor() {
-		String color = null;
-		Process process = null;
-		OutputStream output = null;
-		BufferedReader reader = null;
-		try {
-			process = Runtime.getRuntime().exec(THEME_COLOR_COMMAND);
-			output = process.getOutputStream();
-			reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			String line;
-			while ((line = reader.readLine()) != null) {
-				line = line.trim();
-				if(StringUtils.startsWith(line, THEME_COLOR_KEY)) {
-					final int index = line.indexOf("0x");
-					color = line.substring(index).trim();
-					break;
-				}
-			}
-		} catch (Exception e) {
-			LOGGER.error("获取Windows主题颜色异常", e);
-		} finally {
-			IoUtils.close(output);
-			IoUtils.close(reader);
-			if(process != null) {
-				process.destroy();
-			}
-		}
-		return convertWindowsColor(color);
-	}
-
-	/**
-	 * <p>Windows颜色转换</p>
-	 * 
-	 * @param colorValue 十六进制颜色字符：0xffffff、0xffffffff
-	 * 
-	 * @return 颜色
-	 */
-	private static final Color convertWindowsColor(String colorValue) {
-		Color theme;
-		if(colorValue == null) {
-			theme = DEFAULT_THEME_COLOR;
-		} else {
-			final int value = (int) Long.parseLong(colorValue.substring(2), 16);
-			final int alpha = (int) ((value >> 24) & 0xFF); // 透明度：可能不存在
-			final int blud = (int) ((value >> 16) & 0xFF);
-			final int green = (int) ((value >> 8) & 0xFF);
-			final int red = (int) (value & 0xFF);
-			final double opacity = alpha >= 255D ? 1D : alpha / 255D;
-			if(alpha == 0) { // 没有透明度默认设置不透明
-				theme = Color.rgb(red, green, blud);
-			} else {
-				theme = Color.rgb(red, green, blud, opacity);
-			}
-		}
-		LOGGER.info("Windows系统主题颜色：{}-{}", colorValue, theme);
-		return theme;
 	}
 	
 }
