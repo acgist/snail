4ed4a262b9dbdf4efb18bf03785fece32b035a8c
acgist <289547414@qq.com>
2020-11-11 10:33:04 +0800

    测试优化

--- snail/src/test/java/com/acgist/snail/utils/UrlUtilsTest.java
+++ snail/src/test/java/com/acgist/snail/utils/UrlUtilsTest.java
@@ -2,6 +2,10 @@ package com.acgist.snail.utils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
 import org.junit.jupiter.api.Test;
 
 import com.acgist.snail.BaseTest;
@@ -28,6 +32,15 @@ public class UrlUtilsTest extends BaseTest {
 		assertEquals(UrlUtils.redirect("https://www.acgist.com/a/b/", " /index.html "), path);
 		assertEquals(UrlUtils.redirect("https://www.acgist.com/a/b/", "\"/index.html\""), path);
 		assertEquals(UrlUtils.redirect("https://www.acgist.com/a/b/", " \"/index.html\" "), path);
+	}
+	
+	@Test
+	public void encode() throws UnsupportedEncodingException {
+		// 空格编码变成加号：加号解码变成空格
+		System.out.println(URLEncoder.encode("+ +", "UTF-8"));
+		System.out.println(URLDecoder.decode("%2B+%2B", "UTF-8"));
+		System.out.println(UrlUtils.encode("+ +"));
+		System.out.println(UrlUtils.decode("%2B%20%2B"));
 	}
 	
 }
