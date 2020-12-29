acc25a12d13f1341a6fe1367a34d81580fa4eec2
acgist <289547414@qq.com>
2020-12-29 16:09:57 +0800

    测试

---
+++ snail-javafx/src/test/java/com/acgist/snail/gui/javafx/AlertsTest.java
@@ -0,0 +1,35 @@
+package com.acgist.snail.gui.javafx;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+
+import com.acgist.snail.gui.GuiManager.MessageType;
+import com.acgist.snail.utils.Performance;
+
+import javafx.application.Platform;
+import javafx.scene.control.ButtonType;
+
+public class AlertsTest extends Performance {
+
+	@Test
+	public void testAlerts() throws InterruptedException {
+		final Object lock = new Object();
+		Platform.startup(() -> {});
+		Platform.runLater(() -> {
+			Alerts.info("测试", "测试信息");
+			Alerts.warn("警告", "警告信息");
+			Optional<ButtonType> optional;
+			do {
+				optional = Alerts.build("确认", "是否删除？", MessageType.CONFIRM);
+			} while (optional.isPresent() && optional.get() != ButtonType.OK);
+			synchronized (lock) {
+				lock.notify();
+			}
+		});
+		synchronized (lock) {
+			lock.wait();
+		}
+	}
+	
+}
