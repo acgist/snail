6553d48d8204d60ea56dc3e13a47e88370854270
acgist <289547414@qq.com>
2021-01-20 13:41:04 +0800

    优化代码：Tooltips

--- snail-javafx/src/main/java/com/acgist/snail/gui/javafx/Tooltips.java
+++ snail-javafx/src/main/java/com/acgist/snail/gui/javafx/Tooltips.java
@@ -5,37 +5,40 @@ import javafx.scene.control.Tooltip;
 import javafx.util.Duration;
 
 /**
- * <p>提示框工具</p>
+ * <p>工具提示助手</p>
  * 
  * @author acgist
  */
 public final class Tooltips {
-
+	
 	/**
-	 * <p>不允许实例化</p>
+	 * <p>默认显示时间（毫秒）：{@value}</p>
 	 */
+	public static final int DEFAULT_SHOW_DELAY = 200;
+	
 	private Tooltips() {
 	}
 	
 	/**
-	 * <p>创建提示框</p>
-	 * <p>默认显示时间（毫秒）：200</p>
+	 * <p>创建工具提示</p>
 	 * 
 	 * @param value 提示内容
 	 * 
-	 * @return 提示框
+	 * @return 工具提示
+	 * 
+	 * @see #DEFAULT_SHOW_DELAY
 	 */
 	public static final Tooltip newTooltip(String value) {
-		return newTooltip(value, 200);
+		return newTooltip(value, DEFAULT_SHOW_DELAY);
 	}
 	
 	/**
-	 * <p>创建提示框</p>
+	 * <p>创建工具提示</p>
 	 * 
 	 * @param value 提示内容
 	 * @param millis 显示时间：毫秒
 	 * 
-	 * @return 提示框
+	 * @return 工具提示
 	 */
 	public static final Tooltip newTooltip(String value, int millis) {
 		final Tooltip tooltip = new Tooltip(value);
--- snail-javafx/src/main/java/module-info.java
+++ snail-javafx/src/main/java/module-info.java
@@ -1,20 +1,19 @@
 /**
  * <h1>Sanil（蜗牛）</h1>
- * <p>通过JavaFX开发的图形界面管理Snail（蜗牛）</p>
+ * <p>基于JavaFX开发的Snail图形管理界面</p>
  * <p>官网地址：https://gitee.com/acgist/snail</p>
  * 
  * @author acgist
  */
 open module com.acgist.snail.javafx {
 
-	//================导出================//
 	exports com.acgist.main;
 	exports com.acgist.snail.gui.javafx;
 	exports com.acgist.snail.gui.javafx.window;
 	
 	//================Java================//
 	requires java.base;
-	// GUI依赖：AWT、Swing
+	// AWT、Swing依赖
 	requires transitive java.desktop;
 	// JavaFX依赖
 	requires transitive java.scripting;
