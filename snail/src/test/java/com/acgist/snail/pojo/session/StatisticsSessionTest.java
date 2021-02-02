a42b6af5d12b5709a784025a54a847f5a08b0074
acgist <289547414@qq.com>
2021-02-02 14:36:12 +0800

    优化统计信息

---
+++ snail/src/test/java/com/acgist/snail/pojo/session/StatisticsSessionTest.java
@@ -0,0 +1,23 @@
+package com.acgist.snail.pojo.session;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.acgist.snail.utils.Performance;
+
+public class StatisticsSessionTest extends Performance {
+
+	@Test
+	public void testStatisticsSession() {
+		final StatisticsSession parent = new StatisticsSession();
+		final StatisticsSession session = new StatisticsSession(true, parent);
+		session.upload(1024);
+		assertEquals(1024, parent.uploadSize());
+		assertEquals(1024, session.uploadSize());
+		session.download(1024);
+		assertEquals(1024, parent.downloadSize());
+		assertEquals(1024, session.downloadSize());
+	}
+	
+}
