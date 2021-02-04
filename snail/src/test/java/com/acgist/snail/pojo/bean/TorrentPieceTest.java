b9343431e5312f2c3a38b33d390b91da9a17a079
acgist <289547414@qq.com>
2021-02-04 10:04:50 +0800

    代码优化

--- snail/src/test/java/com/acgist/snail/pojo/bean/TorrentPieceTest.java
+++ snail/src/test/java/com/acgist/snail/pojo/bean/TorrentPieceTest.java
@@ -1,33 +1,40 @@
 package com.acgist.snail.pojo.bean;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
+import com.acgist.snail.utils.DigestUtils;
 import com.acgist.snail.utils.Performance;
 
 public class TorrentPieceTest extends Performance {
 
 	@Test
-	public void testRead() {
-		final int pieceLength = 1024; // Piece长度
-		final int begin = 10; // 开始偏移
-		final int end = 20; // 结束偏移
-		final var piece = TorrentPiece.newInstance(pieceLength, 0, begin, end, null, false);
-		final byte[] bytes = new byte[end - begin];
-		for (int index = begin; index < end; index++) {
-			bytes[index - begin] = (byte) index;
-		}
-		piece.write(begin, bytes);
-		this.log(piece.read(0, 20)); // 完全包含
-		this.log(piece.read(11, 2)); // 包含部分
-		this.log(piece.read(0, 12)); // 包含开始
-		this.log(piece.read(18, 20)); // 包含结束
-		this.log(piece.read(0, 10)); // 不包含
-		this.log(piece.read(20, 10)); // 不包含
+	public void testWrite() {
+		final var verify = new byte[2 * 16 * 1024 - 2];
+		verify[1] = 100;
+		final var piece = TorrentPiece.newInstance(2 * 16 * 1024, 10, 0, 2 * 16 * 1024 - 2, DigestUtils.sha1().digest(verify), true);
+		assertFalse(piece.completed());
+		assertEquals(20 * 16 * 1024, piece.beginPos());
+		assertEquals(22 * 16 * 1024 - 2, piece.endPos());
+		assertEquals(0, piece.position());
+		assertEquals(16 * 1024, piece.length());
+		final var data = new byte[16 * 1024];
+		data[1] = 100;
+		piece.write(0, data);
+		assertFalse(piece.completed());
+		piece.write(piece.position(), new byte[16 * 1024 - 2]);
+		assertTrue(piece.completed());
+		assertTrue(piece.verify());
 	}
 	
-
 	@Test
-	public void testCost() {
+	public void testRead() {
 		final int pieceLength = 1024;
 		final int begin = 10;
 		final int end = 20;
@@ -37,11 +44,26 @@ public class TorrentPieceTest extends Performance {
 			bytes[index - begin] = (byte) index;
 		}
 		piece.write(begin, bytes);
-		this.cost();
-		for (int i = 0; i < 100000; i++) {
-			piece.read(0, 20);
+		assertArrayEquals(bytes, piece.read(0, 20));
+		assertArrayEquals(new byte[] {11, 12}, piece.read(11, 2));
+		assertArrayEquals(new byte[] {10, 11}, piece.read(0, 12));
+		assertArrayEquals(new byte[] {18, 19}, piece.read(18, 20));
+		assertNull(piece.read(0, 10));
+		assertNull(piece.read(20, 10));
+	}
+
+	@Test
+	public void testCosted() {
+		final int pieceLength = 1024;
+		final int begin = 10;
+		final int end = 20;
+		final var piece = TorrentPiece.newInstance(pieceLength, 0, begin, end, null, false);
+		final byte[] bytes = new byte[end - begin];
+		for (int index = begin; index < end; index++) {
+			bytes[index - begin] = (byte) index;
 		}
-		this.costed();
+		assertDoesNotThrow(() -> this.costed(100000, () -> piece.write(begin, bytes)));
+		assertDoesNotThrow(() -> this.costed(100000, () -> piece.read(0, 20)));
 	}
 	
 }
