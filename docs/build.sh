68320def4507561bcb8eb7235ddc85caee3a3816
acgist <289547414@qq.com>
2021-02-18 15:49:53 +0800

    参数优化

--- docs/build.sh
+++ docs/build.sh
@@ -16,22 +16,22 @@ echo "Snail $version $os building..."
 # 删除文件
 
 mvn clean
-rm -rf "./build/"
+rm -rf ./build/
 
 # 编译项目
 
-mvn package -P release -D "javafx.platform=$os" -D skipTests
-mkdir -p "./build/snail/"
-cp -vr "./LICENSE" "./build/"
-cp -vr "./docs/logo.ico" "./build/"
-cp -vr "./snail-javafx/target/lib" "./build/snail/"
-cp -vr "./snail-javafx/target/snail.javafx-${version}.jar" "./build/snail/"
+mvn package -P release -D javafx.platform=$os -D skipTests
+mkdir -p ./build/snail/
+cp -vr ./LICENSE ./build/
+cp -vr ./docs/logo.ico ./build/
+cp -vr ./snail-javafx/target/lib ./build/snail/
+cp -vr ./snail-javafx/target/snail.javafx-${version}.jar ./build/snail/
 
 # 打包项目
 
 if [[ $pack == "pack" ]]; then
 # 运行环境
-    jlink --add-modules "java.base,java.xml,java.desktop,java.scripting,jdk.unsupported" --output "./build/runtime"
+    jlink --add-modules java.base,java.xml,java.desktop,java.scripting,jdk.unsupported --output ./build/runtime
 # 系统参数
     if [[ $os == "win" ]]; then
 #       win:msi|exe
@@ -52,13 +52,13 @@ if [[ $pack == "pack" ]]; then
         --vendor acgist \
         --copyright acgist \
         --description "acgist snail" \
-        --input "./build/snail/" \
-        --main-jar "snail.javafx-${version}.jar" \
-        --runtime-image "./build/runtime" \
-        --icon "./build/logo.ico" \
-        --license-file "./build/LICENSE" \
+        --input ./build/snail/ \
+        --main-jar snail.javafx-${version}.jar \
+        --runtime-image ./build/runtime \
+        --icon ./build/logo.ico \
+        --license-file ./build/LICENSE \
         --java-options "-server -Xms128m -Xmx256m -XX:NewRatio=2 -XX:SurvivorRatio=2 -Dfile.encoding=UTF-8" \
-        --dest "./build/" \
+        --dest ./build/ \
         $args
 fi
 
