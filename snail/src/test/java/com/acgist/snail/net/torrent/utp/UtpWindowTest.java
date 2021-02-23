d8a06f838f795f37c4cf3002b3e2ea630f811c3a
acgist <289547414@qq.com>
2021-02-23 17:35:50 +0800

    优化UTP窗口ack

---
+++ snail/src/test/java/com/acgist/snail/net/torrent/utp/UtpWindowTest.java
@@ -0,0 +1,27 @@
+package com.acgist.snail.net.torrent.utp;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.Test;
+
+import com.acgist.snail.logger.LoggerConfig;
+import com.acgist.snail.utils.Performance;
+
+class UtpWindowTest extends Performance {
+	
+	void ack(UtpWindow window) {
+		for (int index = 0; index < 15; index++) {
+			window.build();
+		}
+		final short seqnr = window.build().getSeqnr();
+		window.ack(seqnr, 100);
+	}
+	
+	@Test
+	void testAck() {
+		LoggerConfig.off();
+		final UtpWindow window = UtpWindow.newSendInstance();
+		assertDoesNotThrow(() -> this.costed(100000, () -> this.ack(window)));
+	}
+
+}
