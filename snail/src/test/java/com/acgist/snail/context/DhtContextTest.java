a40f257d173863d240e17832f97ccbe7889fbf41
acgist <289547414@qq.com>
2021-07-01 18:44:01 +0800

    DHT消息ID优化

--- snail/src/test/java/com/acgist/snail/context/DhtContextTest.java
+++ snail/src/test/java/com/acgist/snail/context/DhtContextTest.java
@@ -1,6 +1,9 @@
 package com.acgist.snail.context;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
@@ -22,6 +25,15 @@ class DhtContextTest extends Performance {
 	void testCosted() {
 		this.costed(100000, () -> this.testRequest());
 //		this.costed(100000, () -> DhtContext.getInstance().buildRequestId());
+	}
+
+	@Test
+	void testRequestId() {
+		this.log(DhtContext.getInstance().buildRequestId());
+		this.log(DhtContext.getInstance().buildRequestId());
+		this.costed(100000, () -> {
+			assertFalse(Arrays.equals(DhtContext.getInstance().buildRequestId(), DhtContext.getInstance().buildRequestId()));
+		});
 	}
 	
 }
