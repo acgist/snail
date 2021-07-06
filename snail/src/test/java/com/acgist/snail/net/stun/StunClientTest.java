c54f59e53e9bae0df209f97fba52b3364654f265
acgist <289547414@qq.com>
2021-07-06 16:04:51 +0800

    STUN配置多个服务器

--- snail/src/test/java/com/acgist/snail/net/stun/StunClientTest.java
+++ snail/src/test/java/com/acgist/snail/net/stun/StunClientTest.java
@@ -14,15 +14,13 @@ class StunClientTest extends Performance {
 	@Test
 	void testMappedAddress() {
 //		final StunClient client = StunClient.newInstance("stun.l.google.com", 19302);
-		final StunClient client = StunClient.newInstance("stun1.l.google.com", 19302);
+//		final StunClient client = StunClient.newInstance("stun1.l.google.com", 19302);
 //		final StunClient client = StunClient.newInstance("stun2.l.google.com", 19302);
 //		final StunClient client = StunClient.newInstance("stun3.l.google.com", 19302);
 //		final StunClient client = StunClient.newInstance("stun4.l.google.com", 19302);
 //		final StunClient client = StunClient.newInstance("stun.xten.com");
 //		final StunClient client = StunClient.newInstance("stunserver.org");
-//		final StunClient client = StunClient.newInstance("numb.viagenie.ca");
-//		final StunClient client = StunClient.newInstance("stun.softjoys.com");
-//		final StunClient client = StunClient.newInstance("stun.stunprotocol.org", 3478);
+		final StunClient client = StunClient.newInstance("stun.stunprotocol.org", 3478);
 		assertNull(SystemConfig.getExternalIPAddress());
 		int index = 0;
 		client.mappedAddress();
