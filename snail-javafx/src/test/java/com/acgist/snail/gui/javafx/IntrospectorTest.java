8fe6651a2669ae59f119eb91621ce7c091a0a076
acgist <289547414@qq.com>
2021-07-15 11:46:40 +0800

    内省

---
+++ snail-javafx/src/test/java/com/acgist/snail/gui/javafx/IntrospectorTest.java
@@ -0,0 +1,26 @@
+package com.acgist.snail.gui.javafx;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+
+import org.junit.jupiter.api.Test;
+
+import com.acgist.snail.pojo.entity.ConfigEntity;
+import com.acgist.snail.utils.Performance;
+
+class IntrospectorTest extends Performance {
+
+	@Test
+	void testBeans() throws IntrospectionException {
+		final var bean = Introspector.getBeanInfo(ConfigEntity.class);
+		final var properties = bean.getPropertyDescriptors();
+		for (PropertyDescriptor property : properties) {
+			this.log(property);
+		}
+		assertNotNull(bean);
+	}
+	
+}
