a7e0e9479fdc9ca10da42235e6335762b32aa6dc
acgist <289547414@qq.com>
2021-07-22 17:12:29 +0800

    代码优化

--- snail/src/main/java/org/slf4j/impl/StaticLoggerBinder.java
+++ snail/src/main/java/org/slf4j/impl/StaticLoggerBinder.java
@@ -14,6 +14,8 @@ public final class StaticLoggerBinder implements LoggerFactoryBinder {
 
 	private static final StaticLoggerBinder INSTANCE = new StaticLoggerBinder();
 	
+	public static String REQUESTED_API_VERSION = "1.7.30";
+	
 	/**
 	 * <p>SL4J绑定方法</p>
 	 * 
