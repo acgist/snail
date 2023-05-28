49160f1871b68d27253457f947378f8128172569
acgist <289547414@qq.com>
2023-05-28 08:13:02 +0800

    [*] 日常优化

--- snail/src/main/java/com/acgist/snail/context/TaskContext.java
+++ snail/src/main/java/com/acgist/snail/context/TaskContext.java
@@ -22,171 +22,171 @@ import com.acgist.snail.utils.CollectionUtils;
  */
 public final class TaskContext implements IContext {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(TaskContext.class);
-	
-	private static final TaskContext INSTANCE = new TaskContext();
-	
-	public static final TaskContext getInstance() {
-		return INSTANCE;
-	}
-	
-	/**
-	 * 任务队列
-	 */
-	private final List<ITaskSession> tasks;
-	/**
-	 * 下载器线程池
-	 */
-	private final ExecutorService executor;
-	
-	private TaskContext() {
-		this.tasks = new ArrayList<>(DownloadConfig.getSize());
-		this.executor = SystemThreadContext.newCacheExecutor(0, 60L, SystemThreadContext.SNAIL_THREAD_DOWNLOADER);
-	}
-	
-	/**
-	 * 新建下载任务
-	 * 添加下载任务同时开始下载
-	 * 
-	 * @param url 下载链接
-	 * 
-	 * @return 下载任务
-	 * 
-	 * @throws DownloadException 下载异常
-	 */
-	public ITaskSession download(String url) throws DownloadException {
-		final ITaskSession session = ProtocolContext.getInstance().buildTaskSession(url);
-		session.start();
-		return session;
-	}
-	
-	/**
-	 * 添加下载任务
-	 * 只添加下载任务不修改任务状态
-	 * 
-	 * @param taskSession 任务信息
-	 * 
-	 * @throws DownloadException 下载异常
-	 */
-	public void submit(ITaskSession taskSession) throws DownloadException {
-		if(ProtocolContext.getInstance().available()) {
-			synchronized (this.tasks) {
-				if(taskSession == null) {
-					throw new DownloadException("任务信息为空");
-				}
-				// 任务添加必须新建下载器
-				taskSession.buildDownloader();
-				if(this.tasks.contains(taskSession)) {
-					LOGGER.debug("任务已经存在：{}", taskSession);
-				} else {
-					this.tasks.add(taskSession);
-					GuiContext.getInstance().refreshTaskList();
-				}
-			}
-		} else {
-			throw new DownloadException("下载协议未初始化");
-		}
-	}
-	
-	/**
-	 * 删除下载任务
-	 * 
-	 * @param taskSession 下载任务
-	 */
-	public void remove(ITaskSession taskSession) {
-		synchronized (this.tasks) {
-			LOGGER.debug("删除下载任务：{}", taskSession);
-			this.tasks.remove(taskSession);
-		}
-		GuiContext.getInstance().refreshTaskList();
-	}
-	
-	/**
-	 * @return 所有下载任务列表
-	 */
-	public List<ITaskSession> allTask() {
-		synchronized (this.tasks) {
-			return this.tasks.stream().collect(Collectors.toList());
-		}
-	}
-	
-	/**
-	 * @return 是否含有下载任务
-	 */
-	public boolean running() {
-		return this.allTask().stream()
-			.anyMatch(ITaskSession::statusRunning);
-	}
-	
-	/**
-	 * 刷新下载任务
-	 */
-	public void refresh() {
-		synchronized (this.tasks) {
-			// 当前正在下载任务数量
-			final long downloadCount = this.tasks.stream()
-				.filter(ITaskSession::statusDownload)
-				.count();
-			final int downloadSize = DownloadConfig.getSize();
-			if(downloadCount == downloadSize) {
-				LOGGER.debug("下载任务数量正常：{} - {}", downloadSize, downloadCount);
-			} else if(downloadCount > downloadSize) {
-				LOGGER.debug("暂停部分下载任务：{} - {}", downloadSize, downloadCount);
-				this.tasks.stream()
-					.filter(ITaskSession::statusDownload)
-					.skip(downloadSize)
-					.forEach(ITaskSession::await);
-			} else {
-				LOGGER.debug("开始部分下载任务：{} - {}", downloadSize, downloadCount);
-				this.tasks.stream()
-					.filter(ITaskSession::statusAwait)
-					.limit(downloadSize - downloadCount)
-					.map(ITaskSession::getDownloader)
-					.forEach(this.executor::submit);
-			}
-		}
-	}
+    private static final Logger LOGGER = LoggerFactory.getLogger(TaskContext.class);
+    
+    /**
+     * 任务队列
+     */
+    private final List<ITaskSession> tasks;
+    /**
+     * 下载器线程池
+     */
+    private final ExecutorService executor;
 
-	/**
-	 * 加载任务实体列表
-	 */
-	public void load() {
-		final List<TaskEntity> list = EntityContext.getInstance().allTask();
-		if(CollectionUtils.isNotEmpty(list)) {
-			list.forEach(this::load);
-			this.refresh();
-		}
-	}
-	
-	/**
-	 * 加载任务实体
-	 * 
-	 * @param entity 任务实体
-	 */
-	private void load(TaskEntity entity) {
-		try {
-			final ITaskSession taskSession = TaskSession.newInstance(entity);
-			// 重置状态
-			taskSession.reset();
-			this.submit(taskSession);
-		} catch (Exception e) {
-			LOGGER.error("添加下载任务异常：{}", entity, e);
-			EntityContext.getInstance().deleteTask(entity);
-		}
-	}
-	
-	/**
-	 * 关闭任务上下文
-	 */
-	public void shutdown() {
-		LOGGER.debug("关闭任务上下文");
-		// 暂停所有任务
-		synchronized (this.tasks) {
-			this.tasks.stream()
-				.filter(ITaskSession::statusRunning)
-				.forEach(ITaskSession::pause);
-		}
-		SystemThreadContext.shutdown(this.executor);
-	}
+    private static final TaskContext INSTANCE = new TaskContext();
+    
+    public static final TaskContext getInstance() {
+        return INSTANCE;
+    }
+    
+    private TaskContext() {
+        this.tasks    = new ArrayList<>(DownloadConfig.getSize());
+        this.executor = SystemThreadContext.newCacheExecutor(0, 60L, SystemThreadContext.SNAIL_THREAD_DOWNLOADER);
+    }
+    
+    /**
+     * 新建下载任务
+     * 添加下载任务同时开始下载
+     * 
+     * @param url 下载链接
+     * 
+     * @return 下载任务
+     * 
+     * @throws DownloadException 下载异常
+     */
+    public ITaskSession download(String url) throws DownloadException {
+        final ITaskSession session = ProtocolContext.getInstance().buildTaskSession(url);
+        session.start();
+        return session;
+    }
+    
+    /**
+     * 添加下载任务
+     * 只添加下载任务不修改任务状态
+     * 
+     * @param taskSession 任务信息
+     * 
+     * @throws DownloadException 下载异常
+     */
+    public void submit(ITaskSession taskSession) throws DownloadException {
+        if(taskSession == null) {
+            throw new DownloadException("任务信息为空");
+        }
+        if(ProtocolContext.getInstance().available()) {
+            synchronized (this.tasks) {
+                // 任务添加必须新建下载器
+                taskSession.buildDownloader();
+                if(this.tasks.contains(taskSession)) {
+                    LOGGER.debug("任务已经存在：{}", taskSession);
+                } else {
+                    this.tasks.add(taskSession);
+                }
+            }
+            GuiContext.getInstance().refreshTaskList();
+        } else {
+            throw new DownloadException("下载协议未初始化");
+        }
+    }
+    
+    /**
+     * 删除下载任务
+     * 
+     * @param taskSession 下载任务
+     */
+    public void remove(ITaskSession taskSession) {
+        synchronized (this.tasks) {
+            LOGGER.debug("删除下载任务：{}", taskSession);
+            this.tasks.remove(taskSession);
+        }
+        GuiContext.getInstance().refreshTaskList();
+    }
+    
+    /**
+     * @return 所有下载任务列表
+     */
+    public List<ITaskSession> allTask() {
+        synchronized (this.tasks) {
+            return this.tasks.stream().collect(Collectors.toList());
+        }
+    }
+    
+    /**
+     * @return 是否含有下载任务
+     */
+    public boolean running() {
+        return this.allTask().stream()
+            .anyMatch(ITaskSession::statusRunning);
+    }
+    
+    /**
+     * 刷新下载任务
+     */
+    public void refresh() {
+        synchronized (this.tasks) {
+            // 当前正在下载任务数量
+            final long downloadCount = this.tasks.stream()
+                .filter(ITaskSession::statusDownload)
+                .count();
+            final int downloadSize = DownloadConfig.getSize();
+            if(downloadCount == downloadSize) {
+                LOGGER.debug("下载任务数量正常：{} - {}", downloadSize, downloadCount);
+            } else if(downloadCount > downloadSize) {
+                LOGGER.debug("暂停部分下载任务：{} - {}", downloadSize, downloadCount);
+                this.tasks.stream()
+                    .filter(ITaskSession::statusDownload)
+                    .skip(downloadSize)
+                    .forEach(ITaskSession::await);
+            } else {
+                LOGGER.debug("开始部分下载任务：{} - {}", downloadSize, downloadCount);
+                this.tasks.stream()
+                    .filter(ITaskSession::statusAwait)
+                    .limit(downloadSize - downloadCount)
+                    .map(ITaskSession::getDownloader)
+                    .forEach(this.executor::submit);
+            }
+        }
+    }
+
+    /**
+     * 加载任务实体列表
+     */
+    public void load() {
+        final List<TaskEntity> list = EntityContext.getInstance().allTask();
+        if(CollectionUtils.isNotEmpty(list)) {
+            list.forEach(this::load);
+            this.refresh();
+        }
+    }
+    
+    /**
+     * 加载任务实体
+     * 
+     * @param entity 任务实体
+     */
+    private void load(TaskEntity entity) {
+        try {
+            final ITaskSession taskSession = TaskSession.newInstance(entity);
+            // 重置状态
+            taskSession.reset();
+            this.submit(taskSession);
+        } catch (Exception e) {
+            LOGGER.error("添加下载任务异常：{}", entity, e);
+            EntityContext.getInstance().deleteTask(entity);
+        }
+    }
+    
+    /**
+     * 关闭任务上下文
+     */
+    public void shutdown() {
+        LOGGER.debug("关闭任务上下文");
+        // 暂停所有任务
+        synchronized (this.tasks) {
+            this.tasks.stream()
+                .filter(ITaskSession::statusRunning)
+                .forEach(ITaskSession::pause);
+        }
+        SystemThreadContext.shutdown(this.executor);
+    }
 
 }
--- snail/src/test/java/com/acgist/snail/context/TaskContextTest.java
+++ snail/src/test/java/com/acgist/snail/context/TaskContextTest.java
@@ -20,58 +20,58 @@ import com.acgist.snail.utils.Performance;
 
 class TaskContextTest extends Performance {
 
-	@Test
-	void testDownloader() {
-		assertTrue(TaskContext.getInstance().allTask().isEmpty());
-	}
-	
-	@Test
-	void testNewTask() throws DownloadException {
-		SnailBuilder.newBuilder().buildSync();
-		final var taskContext = TaskContext.getInstance();
-		var exception = assertThrows(DownloadException.class, () -> taskContext.download("https://www.acgist.com"));
-		this.log(exception.getMessage());
-		exception = assertThrows(DownloadException.class, () -> taskContext.submit(null));
-		this.log(exception.getMessage());
-		ProtocolContext.getInstance().register(HttpProtocol.getInstance());
-		final ITaskSession taskSession = taskContext.download("https://www.acgist.com");
-		assertTrue(taskContext.allTask().size() > 0);
-		FileUtils.delete(taskSession.getFile());
-		taskSession.delete();
-	}
-	
-	@Test
-	void testRefresh() throws DownloadException {
-		SnailBuilder.newBuilder().enableAllProtocol().buildSync();
-		final var taskContext = TaskContext.getInstance();
-		final ITaskSession taskSession = taskContext.download("1234567890123456789012345678901234567890");
-		assertNotNull(taskSession);
-		DownloadConfig.setSize(0);
-		taskContext.refresh();
-		DownloadConfig.setSize(4);
-		FileUtils.delete(taskSession.getFile());
-		taskSession.delete();
-	}
-	
-	@Test
-	void testRestart() throws DownloadException {
-		SnailBuilder.newBuilder().enableAllProtocol().buildSync();
-		final var taskContext = TaskContext.getInstance();
-		ITaskSession taskSession = taskContext.download("1234567890123456789012345678901234567890");
-		assertNotNull(taskSession);
-		assertTrue(taskContext.allTask().size() > 0);
-		this.log(taskSession.getClass());
-		taskSession.setType(Type.TORRENT);
-		taskSession.setStatus(Status.AWAIT);
-		assertTrue(taskSession.buildDownloader() instanceof MagnetDownloader);
-		taskSession.setTorrent("E://snail/902FFAA29EE632C8DC966ED9AB573409BA9A518E.torrent");
-		taskSession.restart();
-		assertNotNull(taskSession);
-		assertTrue(taskContext.allTask().size() > 0);
-		assertTrue(taskSession.buildDownloader() instanceof TorrentDownloader);
-		this.log(taskSession.getClass());
-		FileUtils.delete(taskSession.getFile());
-		taskSession.delete();
-	}
-	
+    @Test
+    void testDownloader() {
+        assertTrue(TaskContext.getInstance().allTask().isEmpty());
+    }
+    
+    @Test
+    void testNewTask() throws DownloadException {
+        SnailBuilder.newBuilder().buildSync();
+        final var taskContext = TaskContext.getInstance();
+        var exception = assertThrows(DownloadException.class, () -> taskContext.download("https://www.acgist.com"));
+        this.log(exception.getMessage());
+        exception = assertThrows(DownloadException.class, () -> taskContext.submit(null));
+        this.log(exception.getMessage());
+        ProtocolContext.getInstance().register(HttpProtocol.getInstance());
+        final ITaskSession taskSession = taskContext.download("https://www.acgist.com");
+        assertTrue(taskContext.allTask().size() > 0);
+        FileUtils.delete(taskSession.getFile());
+        taskSession.delete();
+    }
+    
+    @Test
+    void testRefresh() throws DownloadException {
+        SnailBuilder.newBuilder().enableAllProtocol().buildSync();
+        final var taskContext = TaskContext.getInstance();
+        final ITaskSession taskSession = taskContext.download("1234567890123456789012345678901234567890");
+        assertNotNull(taskSession);
+        DownloadConfig.setSize(0);
+        taskContext.refresh();
+        DownloadConfig.setSize(4);
+        FileUtils.delete(taskSession.getFile());
+        taskSession.delete();
+    }
+    
+    @Test
+    void testRestart() throws DownloadException {
+        SnailBuilder.newBuilder().enableAllProtocol().buildSync();
+        final var taskContext = TaskContext.getInstance();
+        ITaskSession taskSession = taskContext.download("1234567890123456789012345678901234567890");
+        assertNotNull(taskSession);
+        assertTrue(taskContext.allTask().size() > 0);
+        this.log(taskSession.getClass());
+        taskSession.setType(Type.TORRENT);
+        taskSession.setStatus(Status.AWAIT);
+        assertTrue(taskSession.buildDownloader() instanceof MagnetDownloader);
+        taskSession.setTorrent("E://snail/902FFAA29EE632C8DC966ED9AB573409BA9A518E.torrent");
+        taskSession.restart();
+        assertNotNull(taskSession);
+        assertTrue(taskContext.allTask().size() > 0);
+        assertTrue(taskSession.buildDownloader() instanceof TorrentDownloader);
+        this.log(taskSession.getClass());
+        FileUtils.delete(taskSession.getFile());
+        taskSession.delete();
+    }
+    
 }
