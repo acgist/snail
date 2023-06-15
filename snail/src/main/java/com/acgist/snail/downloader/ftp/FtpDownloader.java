4d354cde1e40dd96659bff50b17ff0a0e954c4d9
acgist <289547414@qq.com>
2023-06-15 08:15:14 +0800

    [*] 日常优化

--- snail/src/main/java/com/acgist/snail/downloader/ftp/FtpDownloader.java
+++ snail/src/main/java/com/acgist/snail/downloader/ftp/FtpDownloader.java
@@ -17,59 +17,59 @@ import com.acgist.snail.utils.IoUtils;
  * @author acgist
  */
 public final class FtpDownloader extends MonofileDownloader {
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(FtpDownloader.class);
-	
-	/**
-	 * FTP客户端
-	 */
-	private FtpClient client;
-	
-	/**
-	 * @param taskSession 任务信息
-	 */
-	private FtpDownloader(ITaskSession taskSession) {
-		super(taskSession);
-	}
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(FtpDownloader.class);
+    
+    /**
+     * FTP客户端
+     */
+    private FtpClient client;
+    
+    /**
+     * @param taskSession 任务信息
+     */
+    private FtpDownloader(ITaskSession taskSession) {
+        super(taskSession);
+    }
 
-	/**
-	 * @param taskSession 任务信息
-	 * 
-	 * @return {@link FtpDownloader}
-	 */
-	public static final FtpDownloader newInstance(ITaskSession taskSession) {
-		return new FtpDownloader(taskSession);
-	}
+    /**
+     * @param taskSession 任务信息
+     * 
+     * @return {@link FtpDownloader}
+     */
+    public static final FtpDownloader newInstance(ITaskSession taskSession) {
+        return new FtpDownloader(taskSession);
+    }
 
-	@Override
-	public void release() {
-		if(this.client != null) {
-			this.client.close();
-		}
-		IoUtils.close(this.input);
-		IoUtils.close(this.output);
-		super.release();
-	}
-	
-	@Override
-	protected void buildInput() throws NetException {
-		// 连接
-		this.client = FtpClient.newInstance(this.taskSession.getUrl());
-		if(this.client.connect()) {
-			// 打开流
-			final long downloadSize = FileUtils.fileSize(this.taskSession.getFile());
-			this.input = Channels.newChannel(this.client.download(downloadSize));
-			// 断点续传
-			if(this.client.range()) {
-				LOGGER.debug("FTP断点下载：{}", downloadSize);
-				this.taskSession.setDownloadSize(downloadSize);
-			} else {
-				LOGGER.debug("FTP重新下载：{}", downloadSize);
-				this.taskSession.setDownloadSize(0L);
-			}
-		} else {
-			this.fail("FTP服务器连接失败");
-		}
-	}
-	
+    @Override
+    public void release() {
+        if(this.client != null) {
+            this.client.close();
+        }
+        IoUtils.close(this.input);
+        IoUtils.close(this.output);
+        super.release();
+    }
+    
+    @Override
+    protected void buildInput() throws NetException {
+        // 连接
+        this.client = FtpClient.newInstance(this.taskSession.getUrl());
+        if(this.client.connect()) {
+            // 打开流
+            final long downloadSize = FileUtils.fileSize(this.taskSession.getFile());
+            this.input = Channels.newChannel(this.client.download(downloadSize));
+            // 断点续传
+            if(this.client.range()) {
+                LOGGER.debug("FTP断点下载：{}", downloadSize);
+                this.taskSession.setDownloadSize(downloadSize);
+            } else {
+                LOGGER.debug("FTP重新下载：{}", downloadSize);
+                this.taskSession.setDownloadSize(0L);
+            }
+        } else {
+            this.fail("FTP服务器连接失败");
+        }
+    }
+    
 }
--- snail/src/test/java/com/acgist/snail/downloader/ftp/FtpDownloaderTest.java
+++ snail/src/test/java/com/acgist/snail/downloader/ftp/FtpDownloaderTest.java
@@ -14,30 +14,31 @@ import com.acgist.snail.utils.FileUtils;
 import com.acgist.snail.utils.Performance;
 
 class FtpDownloaderTest extends Performance {
-	
-	@Test
-	void testFtpDownloaderBuild() throws DownloadException {
-		final String url = "ftp://localhost/ftp/FTPserver.exe";
-		ProtocolContext.getInstance().register(FtpProtocol.getInstance()).available(true);
-		final var taskSession = FtpProtocol.getInstance().buildTaskSession(url);
-		final var downloader = taskSession.buildDownloader();
-//		downloader.run(); // 不下载
-		assertNotNull(downloader);
-		taskSession.delete();
-	}
+    
+    @Test
+    void testFtpDownloaderBuild() throws DownloadException {
+        final String url = "ftp://localhost/ftp/FTPserver.exe";
+        ProtocolContext.getInstance().register(FtpProtocol.getInstance()).available(true);
+        final var taskSession = FtpProtocol.getInstance().buildTaskSession(url);
+        final var downloader = taskSession.buildDownloader();
+       // 不下载
+//      downloader.run();
+        assertNotNull(downloader);
+        taskSession.delete();
+    }
 
-	@Test
-	void testFtpDownloader() throws DownloadException {
-		final String url = "ftp://localhost/ftp/中文文件.exe";
-//		final String url = "ftp://localhost/ftp/FTPserver.exe";
-		ProtocolContext.getInstance().register(FtpProtocol.getInstance()).available(true);
-		final var taskSession = FtpProtocol.getInstance().buildTaskSession(url);
-		final var downloader = taskSession.buildDownloader();
-		downloader.run();
-		final var file = new File(taskSession.getFile());
-		assertTrue(file.exists());
-		FileUtils.delete(taskSession.getFile());
-		taskSession.delete();
-	}
-	
+    @Test
+    void testFtpDownloader() throws DownloadException {
+        final String url = "ftp://localhost/ftp/中文文件.exe";
+//      final String url = "ftp://localhost/ftp/FTPserver.exe";
+        ProtocolContext.getInstance().register(FtpProtocol.getInstance()).available(true);
+        final var taskSession = FtpProtocol.getInstance().buildTaskSession(url);
+        final var downloader = taskSession.buildDownloader();
+        downloader.run();
+        final var file = new File(taskSession.getFile());
+        assertTrue(file.exists());
+        FileUtils.delete(taskSession.getFile());
+        taskSession.delete();
+    }
+    
 }
