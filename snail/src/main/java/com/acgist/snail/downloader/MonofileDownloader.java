e1d6fbcf5ae9af4d6243686a6337e0d9e69fb865
acgist <289547414@qq.com>
2023-06-19 07:46:24 +0800

    [*] 日常优化

--- snail/src/main/java/com/acgist/snail/downloader/MonofileDownloader.java
+++ snail/src/main/java/com/acgist/snail/downloader/MonofileDownloader.java
@@ -23,109 +23,107 @@ import com.acgist.snail.utils.IoUtils;
  * @author acgist
  */
 public abstract class MonofileDownloader extends Downloader {
-	
-	/**
-	 * 快速失败时间（毫秒）
-	 * 注意：建议不要超过任务删除等待时间
-	 */
-	private static final long FAST_CHECK_TIME = 2L * SystemConfig.ONE_SECOND_MILLIS;
-	
-	/**
-	 * 输入流
-	 */
-	protected ReadableByteChannel input;
-	/**
-	 * 输出流
-	 */
-	protected WritableByteChannel output;
-	/**
-	 * 快速失败检测时间
-	 */
-	private volatile long fastCheckTime;
-	
-	/**
-	 * @param taskSession 任务信息
-	 */
-	protected MonofileDownloader(ITaskSession taskSession) {
-		super(taskSession);
-	}
+    
+    /**
+     * 快速失败时间（毫秒）
+     * 注意：建议不要超过任务删除等待时间
+     */
+    private static final long FAST_CHECK_TIME = 2L * SystemConfig.ONE_SECOND_MILLIS;
+    
+    /**
+     * 输入流
+     */
+    protected ReadableByteChannel input;
+    /**
+     * 输出流
+     */
+    protected WritableByteChannel output;
+    /**
+     * 快速失败检测时间
+     */
+    private volatile long fastCheckTime;
+    
+    /**
+     * @param taskSession 任务信息
+     */
+    protected MonofileDownloader(ITaskSession taskSession) {
+        super(taskSession);
+    }
 
-	@Override
-	public void open() throws NetException, DownloadException {
-		this.buildInput();
-		this.buildOutput();
-	}
+    @Override
+    public void open() throws NetException, DownloadException {
+        this.buildInput();
+        this.buildOutput();
+    }
 
-	@Override
-	public void download() throws DownloadException {
-		int length = 0;
-		final long fileSize = this.taskSession.getSize();
-		final ByteBuffer buffer = ByteBuffer.allocateDirect(SystemConfig.DEFAULT_EXCHANGE_LENGTH);
-		try {
-			while(this.downloadable()) {
-				length = this.input.read(buffer);
-				if(length >= 0) {
-					buffer.flip();
-					this.output.write(buffer);
-					buffer.compact();
-					this.statistics.download(length);
-					this.statistics.downloadLimit(length);
-					this.fastCheckTime = System.currentTimeMillis();
-				}
-				if(Downloader.checkFinish(length, this.taskSession.getDownloadSize(), fileSize)) {
-					this.completed = true;
-					break;
-				}
-			}
-		} catch (IOException e) {
-			// 防止偶然下载失败：通过验证下载时间、下载数据大小进行重试下载
-			throw new DownloadException("数据流操作失败", e);
-		}
-	}
-	
-	@Override
-	public void unlockDownload() {
-		super.unlockDownload();
-		// 快速失败检测：防止长时间阻塞
-		if(System.currentTimeMillis() - this.fastCheckTime > FAST_CHECK_TIME) {
-			IoUtils.close(this.input);
-		}
-	}
+    @Override
+    public void download() throws DownloadException {
+        int length = 0;
+        final long fileSize     = this.taskSession.getSize();
+        final ByteBuffer buffer = ByteBuffer.allocateDirect(SystemConfig.DEFAULT_EXCHANGE_LENGTH);
+        try {
+            while(this.downloadable()) {
+                length = this.input.read(buffer);
+                if(length >= 0) {
+                    buffer.flip();
+                    this.output.write(buffer);
+                    buffer.compact();
+                    this.statistics.download(length);
+                    this.statistics.downloadLimit(length);
+                    this.fastCheckTime = System.currentTimeMillis();
+                }
+                if(Downloader.checkFinish(length, this.taskSession.getDownloadSize(), fileSize)) {
+                    this.completed = true;
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            // 防止偶然下载失败：通过验证下载时间、下载数据大小进行重试下载
+            throw new DownloadException("数据流操作失败", e);
+        }
+    }
+    
+    @Override
+    public void unlockDownload() {
+        super.unlockDownload();
+        // 快速失败检测：防止长时间阻塞
+        if(System.currentTimeMillis() - this.fastCheckTime > FAST_CHECK_TIME) {
+            IoUtils.close(this.input);
+        }
+    }
 
-	/**
-	 * 创建输出流
-	 * 先创建输入流：通过判断任务已经下载大小验证是否支持断点续传
-	 * 
-	 * @throws DownloadException 下载异常
-	 * 
-	 * @see #buildInput()
-	 */
-	protected void buildOutput() throws DownloadException {
-		try {
-			final long size = this.taskSession.getDownloadSize();
-			final int bufferSize = DownloadConfig.getMemoryBufferByte(this.taskSession.getSize());
-			OutputStream outputStream;
-			if(size > 0L) {
-				// 支持断点续传
-				outputStream = new FileOutputStream(this.taskSession.getFile(), true);
-			} else {
-				// 不支持断点续传
-				outputStream = new FileOutputStream(this.taskSession.getFile());
-			}
-			final BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream, bufferSize);
-			this.output = Channels.newChannel(bufferedOutputStream);
-		} catch (FileNotFoundException e) {
-			throw new DownloadException("下载文件打开失败", e);
-		}
-	}
-	
-	/**
-	 * 新建输入流
-	 * 验证是否支持断点续传：如果支持重新设置任务已经下载大小
-	 * 
-	 * @throws NetException 网络异常
-	 * @throws DownloadException 下载异常
-	 */
-	protected abstract void buildInput() throws NetException, DownloadException;
+    /**
+     * 创建输出流
+     * 
+     * @throws DownloadException 下载异常
+     * 
+     * @see #buildInput()
+     */
+    protected void buildOutput() throws DownloadException {
+        try {
+            final long size      = this.taskSession.getDownloadSize();
+            final int bufferSize = DownloadConfig.getMemoryBufferByte(this.taskSession.getSize());
+            OutputStream outputStream;
+            if(size > 0L) {
+                // 支持断点续传
+                outputStream = new FileOutputStream(this.taskSession.getFile(), true);
+            } else {
+                // 不支持断点续传
+                outputStream = new FileOutputStream(this.taskSession.getFile());
+            }
+            final BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream, bufferSize);
+            this.output = Channels.newChannel(bufferedOutputStream);
+        } catch (FileNotFoundException e) {
+            throw new DownloadException("下载文件打开失败", e);
+        }
+    }
+    
+    /**
+     * 创建输入流
+     * 
+     * @throws NetException      网络异常
+     * @throws DownloadException 下载异常
+     */
+    protected abstract void buildInput() throws NetException, DownloadException;
 
 }
--- snail/src/main/java/com/acgist/snail/downloader/MultifileDownloader.java
+++ snail/src/main/java/com/acgist/snail/downloader/MultifileDownloader.java
@@ -13,69 +13,69 @@ import com.acgist.snail.utils.FileUtils;
  * @author acgist
  */
 public abstract class MultifileDownloader extends Downloader {
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(MultifileDownloader.class);
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(MultifileDownloader.class);
 
-	/**
-	 * 下载锁
-	 * 下载时阻塞下载任务线程
-	 */
-	protected final Object downloadLock = new Object();
-	
-	/**
-	 * @param taskSession 任务信息
-	 */
-	protected MultifileDownloader(ITaskSession taskSession) {
-		super(taskSession);
-	}
-	
-	@Override
-	public void open() throws NetException, DownloadException {
-		// 新建文件目录：防止删除目录导致任务下载失败
-		FileUtils.buildFolder(this.taskSession.getFile());
-		// 加载下载
-		this.loadDownload();
-	}
+    /**
+     * 下载锁
+     * 下载时阻塞下载任务线程
+     */
+    protected final Object downloadLock = new Object();
+    
+    /**
+     * @param taskSession 任务信息
+     */
+    protected MultifileDownloader(ITaskSession taskSession) {
+        super(taskSession);
+    }
+    
+    @Override
+    public void open() throws NetException, DownloadException {
+        // 新建文件目录：防止删除目录导致任务下载失败
+        FileUtils.buildFolder(this.taskSession.getFile());
+        // 加载下载
+        this.loadDownload();
+    }
 
-	@Override
-	public void download() throws DownloadException {
-		if(this.downloadable()) {
-			synchronized (this.downloadLock) {
-				while(this.downloadable()) {
-					try {
-						// 修改等待时间防止过长时间下载（失败时间等待）：验证下载数据是否变化判断任务是否失败
-						this.downloadLock.wait(Long.MAX_VALUE);
-					} catch (InterruptedException e) {
-						Thread.currentThread().interrupt();
-						LOGGER.debug("线程等待异常", e);
-					}
-					this.completed = this.checkCompleted();
-				}
-			}
-		}
-	}
-	
-	@Override
-	public void unlockDownload() {
-		super.unlockDownload();
-		synchronized (this.downloadLock) {
-			this.downloadLock.notifyAll();
-		}
-	}
-	
-	/**
-	 * 加载下载
-	 * 加载下载所需资源同时开始下载
-	 * 
-	 * @throws DownloadException 下载异常
-	 */
-	protected abstract void loadDownload() throws DownloadException;
+    @Override
+    public void download() throws DownloadException {
+        if(this.downloadable()) {
+            synchronized (this.downloadLock) {
+                while(this.downloadable()) {
+                    try {
+                        // 修改等待时间防止过长时间下载（失败时间等待）：验证下载数据是否变化判断任务是否失败
+                        this.downloadLock.wait(Long.MAX_VALUE);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        LOGGER.debug("线程等待异常", e);
+                    }
+                    this.completed = this.checkCompleted();
+                }
+            }
+        }
+    }
+    
+    @Override
+    public void unlockDownload() {
+        super.unlockDownload();
+        synchronized (this.downloadLock) {
+            this.downloadLock.notifyAll();
+        }
+    }
+    
+    /**
+     * 加载下载
+     * 加载下载所需资源同时开始下载
+     * 
+     * @throws DownloadException 下载异常
+     */
+    protected abstract void loadDownload() throws DownloadException;
 
-	/**
-	 * 判断是否下载完成
-	 * 
-	 * @return 是否下载完成
-	 */
-	protected abstract boolean checkCompleted();
-	
+    /**
+     * 判断是否下载完成
+     * 
+     * @return 是否下载完成
+     */
+    protected abstract boolean checkCompleted();
+    
 }
