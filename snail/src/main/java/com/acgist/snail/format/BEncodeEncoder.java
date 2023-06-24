8381cff5bf8067083baf806a31d79612f41004de
acgist <289547414@qq.com>
2023-06-25 07:37:28 +0800

    [*] 日常优化

--- snail/src/main/java/com/acgist/snail/format/BEncodeEncoder.java
+++ snail/src/main/java/com/acgist/snail/format/BEncodeEncoder.java
@@ -12,312 +12,308 @@ import com.acgist.snail.logger.Logger;
 import com.acgist.snail.logger.LoggerFactory;
 
 /**
- * <p>B编码编码器</p>
- * <p>支持数据类型：Number、String、byte[]、List、Map</p>
+ * B编码编码器
+ * 支持数据类型：Number、String、byte[]、List、Map
  * 
  * @author acgist
  */
 public final class BEncodeEncoder {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(BEncodeEncoder.class);
-	
-	/**
-	 * <p>List</p>
-	 */
-	private List<Object> list;
-	/**
-	 * <p>Map</p>
-	 */
-	private Map<String, Object> map;
-	/**
-	 * <p>数据类型</p>
-	 */
-	private BEncodeDecoder.Type type;
-	/**
-	 * <p>输出数据</p>
-	 */
-	private final ByteArrayOutputStream outputStream;
-	
-	private BEncodeEncoder() {
-		this.outputStream = new ByteArrayOutputStream();
-	}
-	
-	/**
-	 * <p>新建B编码编码器</p>
-	 * 
-	 * @return {@link BEncodeEncoder}
-	 */
-	public static final BEncodeEncoder newInstance() {
-		return new BEncodeEncoder();
-	}
+    private static final Logger LOGGER = LoggerFactory.getLogger(BEncodeEncoder.class);
+    
+    /**
+     * List
+     */
+    private List<Object> list;
+    /**
+     * Map
+     */
+    private Map<String, Object> map;
+    /**
+     * 数据类型
+     */
+    private BEncodeDecoder.Type type;
+    /**
+     * 输出数据
+     */
+    private final ByteArrayOutputStream outputStream;
+    
+    private BEncodeEncoder() {
+        this.outputStream = new ByteArrayOutputStream();
+    }
+    
+    /**
+     * 新建B编码编码器
+     * 
+     * @return {@link BEncodeEncoder}
+     */
+    public static final BEncodeEncoder newInstance() {
+        return new BEncodeEncoder();
+    }
 
-	/**
-	 * <p>新建List</p>
-	 * 
-	 * @return {@link BEncodeEncoder}
-	 */
-	public BEncodeEncoder newList() {
-		this.type = Type.LIST;
-		this.list = new ArrayList<>();
-		return this;
-	}
-	
-	/**
-	 * <p>新建Map</p>
-	 * 
-	 * @return {@link BEncodeEncoder}
-	 */
-	public BEncodeEncoder newMap() {
-		this.type = Type.MAP;
-		this.map = new LinkedHashMap<>();
-		return this;
-	}
-	
-	/**
-	 * <p>向List中添加数据</p>
-	 * 
-	 * @param value 数据
-	 * 
-	 * @return {@link BEncodeEncoder}
-	 */
-	public BEncodeEncoder put(Object value) {
-		if(this.type == Type.LIST) {
-			this.list.add(value);
-		}
-		return this;
-	}
-	
-	/**
-	 * <p>向List中添加数据</p>
-	 * 
-	 * @param list 数据
-	 * 
-	 * @return {@link BEncodeEncoder}
-	 */
-	public BEncodeEncoder put(List<?> list) {
-		if(this.type == Type.LIST) {
-			this.list.addAll(list);
-		}
-		return this;
-	}
-	
-	/**
-	 * <p>向Map中添加数据</p>
-	 * 
-	 * @param key 键
-	 * @param value 值
-	 * 
-	 * @return {@link BEncodeEncoder}
-	 */
-	public BEncodeEncoder put(String key, Object value) {
-		if(this.type == Type.MAP) {
-			this.map.put(key, value);
-		}
-		return this;
-	}
-	
-	/**
-	 * <p>向Map中添加数据</p>
-	 * 
-	 * @param map 数据
-	 * 
-	 * @return {@link BEncodeEncoder}
-	 */
-	public BEncodeEncoder put(Map<String, ?> map) {
-		if(this.type == Type.MAP) {
-			this.map.putAll(map);
-		}
-		return this;
-	}
+    /**
+     * 新建List
+     * 
+     * @return {@link BEncodeEncoder}
+     */
+    public BEncodeEncoder newList() {
+        this.type = Type.LIST;
+        this.list = new ArrayList<>();
+        return this;
+    }
+    
+    /**
+     * 新建Map
+     * 
+     * @return {@link BEncodeEncoder}
+     */
+    public BEncodeEncoder newMap() {
+        this.type = Type.MAP;
+        this.map  = new LinkedHashMap<>();
+        return this;
+    }
+    
+    /**
+     * 向List中添加数据
+     * 
+     * @param value 数据
+     * 
+     * @return {@link BEncodeEncoder}
+     */
+    public BEncodeEncoder put(Object value) {
+        if(this.type == Type.LIST) {
+            this.list.add(value);
+        }
+        return this;
+    }
+    
+    /**
+     * 向List中添加数据
+     * 
+     * @param list 数据
+     * 
+     * @return {@link BEncodeEncoder}
+     */
+    public BEncodeEncoder put(List<?> list) {
+        if(this.type == Type.LIST) {
+            this.list.addAll(list);
+        }
+        return this;
+    }
+    
+    /**
+     * 向Map中添加数据
+     * 
+     * @param key   键
+     * @param value 值
+     * 
+     * @return {@link BEncodeEncoder}
+     */
+    public BEncodeEncoder put(String key, Object value) {
+        if(this.type == Type.MAP) {
+            this.map.put(key, value);
+        }
+        return this;
+    }
+    
+    /**
+     * 向Map中添加数据
+     * 
+     * @param map 数据
+     * 
+     * @return {@link BEncodeEncoder}
+     */
+    public BEncodeEncoder put(Map<String, ?> map) {
+        if(this.type == Type.MAP) {
+            this.map.putAll(map);
+        }
+        return this;
+    }
 
-	/**
-	 * <p>将数据写入字符流</p>
-	 * 
-	 * @return {@link BEncodeEncoder}
-	 */
-	public BEncodeEncoder flush() {
-		if(this.type == Type.MAP) {
-			this.writeMap(this.map);
-		} else if(this.type == Type.LIST) {
-			this.writeList(this.list);
-		} else {
-			LOGGER.warn("B编码错误（未知类型）：{}", this.type);
-		}
-		return this;
-	}
-	
-	/**
-	 * <p>写入字节数组</p>
-	 * 
-	 * @param bytes 数据
-	 * 
-	 * @return {@link BEncodeEncoder}
-	 */
-	public BEncodeEncoder write(byte[] bytes) {
-		try {
-			if(bytes != null) {
-				this.outputStream.write(bytes);
-			}
-		} catch (IOException e) {
-			LOGGER.error("B编码输出异常", e);
-		}
-		return this;
-	}
-	
-	/**
-	 * <p>写入字符</p>
-	 * 
-	 * @param value 数据
-	 */
-	private void write(char value) {
-		this.outputStream.write(value);
-	}
-	
-	/**
-	 * <p>写入B编码List</p>
-	 * 
-	 * @param list 数据
-	 * 
-	 * @return {@link BEncodeEncoder}
-	 */
-	private BEncodeEncoder writeList(List<?> list) {
-		if(list == null) {
-			return this;
-		}
-		this.write(BEncodeDecoder.TYPE_L);
-		list.forEach(this::writeObject);
-		this.write(BEncodeDecoder.TYPE_E);
-		return this;
-	}
-	
-	/**
-	 * <p>写入B编码Map</p>
-	 * 
-	 * @param map 数据
-	 * 
-	 * @return {@link BEncodeEncoder}
-	 */
-	private BEncodeEncoder writeMap(Map<?, ?> map) {
-		if(map == null) {
-			return this;
-		}
-		this.write(BEncodeDecoder.TYPE_D);
-		map.forEach((key, value) -> {
-			this.writeObject(key);
-			this.writeObject(value);
-		});
-		this.write(BEncodeDecoder.TYPE_E);
-		return this;
-	}
-	
-	/**
-	 * <p>写入B编码数据</p>
-	 * 
-	 * @param value 数据
-	 */
-	private void writeObject(Object value) {
-		if(value instanceof String string) {
-			this.writeBytes(string.getBytes());
-		} else if(value instanceof Number number) {
-			this.writeNumber(number);
-		} else if(value instanceof byte[] bytes) {
-			this.writeBytes(bytes);
-		} else if(value instanceof Map<?, ?> map) {
-			this.writeMap(map);
-		} else if(value instanceof List<?> list) {
-			this.writeList(list);
-		} else if(value == null) {
-			this.writeBytes(new byte[0]);
-		} else {
-			this.writeBytes(value.toString().getBytes());
-		}
-	}
-	
-	/**
-	 * <p>写入B编码数值</p>
-	 * 
-	 * @param number 数据
-	 */
-	private void writeNumber(Number number) {
-		this.write(BEncodeDecoder.TYPE_I);
-		this.write(number.toString().getBytes());
-		this.write(BEncodeDecoder.TYPE_E);
-	}
-	
-	/**
-	 * <p>写入B编码字节数组</p>
-	 * 
-	 * @param bytes 数据
-	 */
-	private void writeBytes(byte[] bytes) {
-		this.write(String.valueOf(bytes.length).getBytes());
-		this.write(BEncodeDecoder.SEPARATOR);
-		this.write(bytes);
-	}
-	
-	/**
-	 * <p>获取字节数组</p>
-	 * 
-	 * @return 字节数组
-	 */
-	public byte[] bytes() {
-		return this.outputStream.toByteArray();
-	}
+    /**
+     * 将数据写入字符流
+     * 
+     * @return {@link BEncodeEncoder}
+     */
+    public BEncodeEncoder flush() {
+        if(this.type == Type.MAP) {
+            this.writeMap(this.map);
+        } else if(this.type == Type.LIST) {
+            this.writeList(this.list);
+        } else {
+            LOGGER.warn("B编码错误（未知类型）：{}", this.type);
+        }
+        return this;
+    }
+    
+    /**
+     * 写入字节数组
+     * 
+     * @param bytes 数据
+     * 
+     * @return {@link BEncodeEncoder}
+     */
+    public BEncodeEncoder write(byte[] bytes) {
+        try {
+            if(bytes != null) {
+                this.outputStream.write(bytes);
+            }
+        } catch (IOException e) {
+            LOGGER.error("B编码输出异常", e);
+        }
+        return this;
+    }
+    
+    /**
+     * 写入字符
+     * 
+     * @param value 数据
+     */
+    private void write(char value) {
+        this.outputStream.write(value);
+    }
+    
+    /**
+     * 写入B编码List
+     * 
+     * @param list 数据
+     * 
+     * @return {@link BEncodeEncoder}
+     */
+    private BEncodeEncoder writeList(List<?> list) {
+        if(list == null) {
+            return this;
+        }
+        this.write(BEncodeDecoder.TYPE_L);
+        list.forEach(this::writeObject);
+        this.write(BEncodeDecoder.TYPE_E);
+        return this;
+    }
+    
+    /**
+     * 写入B编码Map
+     * 
+     * @param map 数据
+     * 
+     * @return {@link BEncodeEncoder}
+     */
+    private BEncodeEncoder writeMap(Map<?, ?> map) {
+        if(map == null) {
+            return this;
+        }
+        this.write(BEncodeDecoder.TYPE_D);
+        map.forEach((key, value) -> {
+            this.writeObject(key);
+            this.writeObject(value);
+        });
+        this.write(BEncodeDecoder.TYPE_E);
+        return this;
+    }
+    
+    /**
+     * 写入B编码数据
+     * 
+     * @param value 数据
+     */
+    private void writeObject(Object value) {
+        if(value instanceof String string) {
+            this.writeBytes(string.getBytes());
+        } else if(value instanceof Number number) {
+            this.writeNumber(number);
+        } else if(value instanceof byte[] bytes) {
+            this.writeBytes(bytes);
+        } else if(value instanceof Map<?, ?> map) {
+            this.writeMap(map);
+        } else if(value instanceof List<?> list) {
+            this.writeList(list);
+        } else if(value == null) {
+            this.writeBytes(new byte[0]);
+        } else {
+            this.writeBytes(value.toString().getBytes());
+        }
+    }
+    
+    /**
+     * 写入B编码数值
+     * 
+     * @param number 数据
+     */
+    private void writeNumber(Number number) {
+        this.write(BEncodeDecoder.TYPE_I);
+        this.write(number.toString().getBytes());
+        this.write(BEncodeDecoder.TYPE_E);
+    }
+    
+    /**
+     * 写入B编码字节数组
+     * 
+     * @param bytes 数据
+     */
+    private void writeBytes(byte[] bytes) {
+        this.write(String.valueOf(bytes.length).getBytes());
+        this.write(BEncodeDecoder.SEPARATOR);
+        this.write(bytes);
+    }
+    
+    /**
+     * @return 字节数组
+     */
+    public byte[] bytes() {
+        return this.outputStream.toByteArray();
+    }
 
-	/**
-	 * <p>获取字符串</p>
-	 * 
-	 * @return 字符串
-	 * 
-	 * @see #bytes()
-	 */
-	@Override
-	public String toString() {
-		return new String(this.bytes());
-	}
-	
-	/**
-	 * <p>List转为B编码字节数组</p>
-	 * 
-	 * @param list List
-	 * 
-	 * @return B编码字节数组
-	 */
-	public static final byte[] encodeList(List<?> list) {
-		return newInstance().writeList(list).bytes();
-	}
-	
-	/**
-	 * <p>List转为B编码字符串</p>
-	 * 
-	 * @param list List
-	 * 
-	 * @return B编码字符串
-	 */
-	public static final String encodeListString(List<?> list) {
-		return new String(encodeList(list));
-	}
-	
-	/**
-	 * <p>Map转为B编码字节数组</p>
-	 * 
-	 * @param map Map
-	 * 
-	 * @return B编码字节数组
-	 */
-	public static final byte[] encodeMap(Map<?, ?> map) {
-		return newInstance().writeMap(map).bytes();
-	}
-	
-	/**
-	 * <p>Map转为B编码字符串</p>
-	 * 
-	 * @param map Map
-	 * 
-	 * @return B编码字符串
-	 */
-	public static final String encodeMapString(Map<?, ?> map) {
-		return new String(encodeMap(map));
-	}
-	
+    /**
+     * @return 字符串
+     * 
+     * @see #bytes()
+     */
+    @Override
+    public String toString() {
+        return new String(this.bytes());
+    }
+    
+    /**
+     * List转为B编码字节数组
+     * 
+     * @param list List
+     * 
+     * @return B编码字节数组
+     */
+    public static final byte[] encodeList(List<?> list) {
+        return newInstance().writeList(list).bytes();
+    }
+    
+    /**
+     * List转为B编码字符串
+     * 
+     * @param list List
+     * 
+     * @return B编码字符串
+     */
+    public static final String encodeListString(List<?> list) {
+        return new String(encodeList(list));
+    }
+    
+    /**
+     * Map转为B编码字节数组
+     * 
+     * @param map Map
+     * 
+     * @return B编码字节数组
+     */
+    public static final byte[] encodeMap(Map<?, ?> map) {
+        return newInstance().writeMap(map).bytes();
+    }
+    
+    /**
+     * Map转为B编码字符串
+     * 
+     * @param map Map
+     * 
+     * @return B编码字符串
+     */
+    public static final String encodeMapString(Map<?, ?> map) {
+        return new String(encodeMap(map));
+    }
+    
 }
--- snail/src/test/java/com/acgist/snail/format/BEncodeEncoderTest.java
+++ snail/src/test/java/com/acgist/snail/format/BEncodeEncoderTest.java
@@ -16,115 +16,115 @@ import com.acgist.snail.utils.Performance;
 
 class BEncodeEncoderTest extends Performance {
 
-	@Test
-	void testBEncode() throws PacketSizeException {
-		final BEncodeEncoder encoder = BEncodeEncoder.newInstance();
-		encoder
-			.newList().put(List.of("a", "b")).flush()
-			.newMap().put(Map.of("1", "2")).flush()
-			.write("xxxx".getBytes());
-		final String content = encoder.toString();
-		this.log(content);
-		assertEquals("l1:a1:bed1:11:2exxxx", content);
-		final BEncodeDecoder decoder = BEncodeDecoder.newInstance(content.getBytes());
-		final var list = decoder.nextList();
-		list.forEach(value -> this.log(new String((byte[]) value)));
-		final var map = decoder.nextMap();
-		map.forEach((key, value) -> {
-				if(value instanceof Long) {
-					this.log(key + "=" + value);
-				} else {
-					this.log(key + "=" + new String((byte[]) value));
-				}
-			});
-		final String odd = decoder.toString();
-		this.log(odd);
-		assertEquals("xxxx", odd);
-		assertEquals(1, map.size());
-		assertEquals(2, list.size());
-		final String mix = BEncodeEncoder.newInstance()
-			.newList().put(List.of("a", List.of("b", "c"), Map.of("d", "e"))).flush().toString();
-		this.log(mix);
-		assertEquals("l1:al1:b1:ced1:d1:eee", mix);
-		final var mixList = BEncodeDecoder.newInstance(mix).nextList();
-		assertEquals("a", new String((byte[]) mixList.get(0)));
-		final List<?> mixListList = (List<?>) mixList.get(1);
-		assertEquals("b", new String((byte[]) mixListList.get(0)));
-		assertEquals("c", new String((byte[]) mixListList.get(1)));
-		final Map<?, ?> mixListMap = (Map<?, ?>) mixList.get(2);
-		assertEquals("e", new String((byte[]) mixListMap.get("d")));
-	}
-	
-	@Test
-	void testNull() throws PacketSizeException {
-		final Map<String, Object> map = new LinkedHashMap<>();
-		map.put("a", 1);
-		map.put("b", null);
-		map.put("c", "c");
-		map.put("d", "");
-		map.put(null, "c");
-		final String content = new String(BEncodeEncoder.encodeMap(map));
-		this.log(content);
-		assertEquals("d1:ai1e1:b0:1:c1:c1:d0:0:1:ce", content);
-		final var decoder = BEncodeDecoder.newInstance(content);
-		final var decodeMap = decoder.nextMap();
-		decodeMap.forEach((key, value) -> {
-			if(value instanceof Number) {
-				this.log(key + "-" + value);
-			} else {
-				this.log(key + "-" + new String((byte[]) value));
-			}
-		});
-		assertEquals(map.size(), decodeMap.size());
-	}
-	
-	@Test
-	void testEncode() {
-		final String map = BEncodeEncoder.encodeMapString(Map.of("1", "2"));
-		final String list = BEncodeEncoder.encodeListString(List.of("1", "2"));
-		assertEquals("d1:11:2e", map);
-		assertEquals("l1:11:2e", list);
-	}
+    @Test
+    void testBEncode() throws PacketSizeException {
+        final BEncodeEncoder encoder = BEncodeEncoder.newInstance();
+        encoder
+            .newList().put(List.of("a", "b")).flush()
+            .newMap().put(Map.of("1", "2")).flush()
+            .write("xxxx".getBytes());
+        final String content = encoder.toString();
+        this.log(content);
+        assertEquals("l1:a1:bed1:11:2exxxx", content);
+        final BEncodeDecoder decoder = BEncodeDecoder.newInstance(content.getBytes());
+        final var list = decoder.nextList();
+        list.forEach(value -> this.log(new String((byte[]) value)));
+        final var map = decoder.nextMap();
+        map.forEach((key, value) -> {
+                if(value instanceof Long) {
+                    this.log(key + "=" + value);
+                } else {
+                    this.log(key + "=" + new String((byte[]) value));
+                }
+            });
+        final String odd = decoder.toString();
+        this.log(odd);
+        assertEquals("xxxx", odd);
+        assertEquals(1, map.size());
+        assertEquals(2, list.size());
+        final String mix = BEncodeEncoder.newInstance()
+            .newList().put(List.of("a", List.of("b", "c"), Map.of("d", "e"))).flush().toString();
+        this.log(mix);
+        assertEquals("l1:al1:b1:ced1:d1:eee", mix);
+        final var mixList = BEncodeDecoder.newInstance(mix).nextList();
+        assertEquals("a", new String((byte[]) mixList.get(0)));
+        final List<?> mixListList = (List<?>) mixList.get(1);
+        assertEquals("b", new String((byte[]) mixListList.get(0)));
+        assertEquals("c", new String((byte[]) mixListList.get(1)));
+        final Map<?, ?> mixListMap = (Map<?, ?>) mixList.get(2);
+        assertEquals("e", new String((byte[]) mixListMap.get("d")));
+    }
+    
+    @Test
+    void testNull() throws PacketSizeException {
+        final Map<String, Object> map = new LinkedHashMap<>();
+        map.put("a", 1);
+        map.put("b", null);
+        map.put("c", "c");
+        map.put("d", "");
+        map.put(null, "c");
+        final String content = new String(BEncodeEncoder.encodeMap(map));
+        this.log(content);
+        assertEquals("d1:ai1e1:b0:1:c1:c1:d0:0:1:ce", content);
+        final var decoder = BEncodeDecoder.newInstance(content);
+        final var decodeMap = decoder.nextMap();
+        decodeMap.forEach((key, value) -> {
+            if(value instanceof Number) {
+                this.log(key + "-" + value);
+            } else {
+                this.log(key + "-" + new String((byte[]) value));
+            }
+        });
+        assertEquals(map.size(), decodeMap.size());
+    }
+    
+    @Test
+    void testEncode() {
+        final String map = BEncodeEncoder.encodeMapString(Map.of("1", "2"));
+        final String list = BEncodeEncoder.encodeListString(List.of("1", "2"));
+        assertEquals("d1:11:2e", map);
+        assertEquals("l1:11:2e", list);
+    }
 
-	@Test
-	void testException() throws PacketSizeException {
-		assertThrows(IllegalArgumentException.class, () -> BEncodeDecoder.newInstance("l"));
-	}
-	
-	@Test
-	void testEmpty() throws PacketSizeException {
-		var decoder = BEncodeDecoder.newInstance("d1:11:2e");
-		assertTrue(decoder.isEmpty());
-		decoder.nextType();
-		assertTrue(decoder.isNotEmpty());
-		decoder = BEncodeDecoder.newInstance("de");
-		assertEquals(BEncodeDecoder.Type.MAP, decoder.nextType());
-		assertTrue(decoder.isEmpty());
-		decoder = BEncodeDecoder.newInstance("le");
-		assertEquals(BEncodeDecoder.Type.LIST, decoder.nextType());
-		assertTrue(decoder.isEmpty());
-		decoder = BEncodeDecoder.newInstance("xx");
-		decoder.nextType();
-		assertTrue(decoder.isEmpty());
-	}
-	
-	@Test
-	void testNext() throws PacketSizeException {
-		var decoder = BEncodeDecoder.newInstance("d1:11:2e").next();
-		assertFalse(decoder.isEmpty());
-		decoder = BEncodeDecoder.newInstance("de").next();
-		assertTrue(decoder.isEmpty());
-		decoder = BEncodeDecoder.newInstance("le").next();
-		assertTrue(decoder.isEmpty());
-	}
-	
-	@Test
-	void testCosted() {
-		final long costed = this.costed(100000, () -> {
-			BEncodeEncoder.encodeMapString(Map.of("1", "2"));
-			BEncodeEncoder.encodeListString(List.of("1", "2"));
-		});
-		assertTrue(costed < 1000);
-	}
-	
+    @Test
+    void testException() throws PacketSizeException {
+        assertThrows(IllegalArgumentException.class, () -> BEncodeDecoder.newInstance("l"));
+    }
+    
+    @Test
+    void testEmpty() throws PacketSizeException {
+        var decoder = BEncodeDecoder.newInstance("d1:11:2e");
+        assertTrue(decoder.isEmpty());
+        decoder.nextType();
+        assertTrue(decoder.isNotEmpty());
+        decoder = BEncodeDecoder.newInstance("de");
+        assertEquals(BEncodeDecoder.Type.MAP, decoder.nextType());
+        assertTrue(decoder.isEmpty());
+        decoder = BEncodeDecoder.newInstance("le");
+        assertEquals(BEncodeDecoder.Type.LIST, decoder.nextType());
+        assertTrue(decoder.isEmpty());
+        decoder = BEncodeDecoder.newInstance("xx");
+        decoder.nextType();
+        assertTrue(decoder.isEmpty());
+    }
+    
+    @Test
+    void testNext() throws PacketSizeException {
+        var decoder = BEncodeDecoder.newInstance("d1:11:2e").next();
+        assertFalse(decoder.isEmpty());
+        decoder = BEncodeDecoder.newInstance("de").next();
+        assertTrue(decoder.isEmpty());
+        decoder = BEncodeDecoder.newInstance("le").next();
+        assertTrue(decoder.isEmpty());
+    }
+    
+    @Test
+    void testCosted() {
+        final long costed = this.costed(100000, () -> {
+            BEncodeEncoder.encodeMapString(Map.of("1", "2"));
+            BEncodeEncoder.encodeListString(List.of("1", "2"));
+        });
+        assertTrue(costed < 1000);
+    }
+    
 }
