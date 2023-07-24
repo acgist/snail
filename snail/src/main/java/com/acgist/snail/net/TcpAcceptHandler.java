90a33d24e0684f18b8eaf0cdd0c464f282c4d64b
acgist <289547414@qq.com>
2023-07-25 07:32:09 +0800

    [*] 日常优化

--- snail/src/main/java/com/acgist/snail/net/TcpAcceptHandler.java
+++ snail/src/main/java/com/acgist/snail/net/TcpAcceptHandler.java
@@ -9,7 +9,7 @@ import com.acgist.snail.logger.LoggerFactory;
 import com.acgist.snail.utils.BeanUtils;
 
 /**
- * <p>TCP消息接收代理</p>
+ * TCP消息接收代理
  * 
  * @param <T> TCP消息代理类型
  * 
@@ -17,50 +17,50 @@ import com.acgist.snail.utils.BeanUtils;
  */
 public final class TcpAcceptHandler<T extends TcpMessageHandler> implements CompletionHandler<AsynchronousSocketChannel, AsynchronousServerSocketChannel> {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(TcpAcceptHandler.class);
-	
-	/**
-	 * <p>消息代理类型</p>
-	 */
-	private final Class<T> clazz;
-	/**
-	 * <p>消息代理上下文</p>
-	 */
-	private final MessageHandlerContext context;
-	
-	/**
-	 * @param clazz 消息代理类型
-	 */
-	private TcpAcceptHandler(Class<T> clazz) {
-		this.clazz = clazz;
-		this.context = MessageHandlerContext.getInstance();
-	}
-	
-	/**
-	 * <p>新建TCP消息接收代理</p>
-	 * 
-	 * @param <T> 消息代理类型
-	 * 
-	 * @param clazz 消息代理类型
-	 * 
-	 * @return TCP消息接收代理
-	 */
-	public static final <T extends TcpMessageHandler> TcpAcceptHandler<T> newInstance(Class<T> clazz) {
-		return new TcpAcceptHandler<>(clazz);
-	}
-	
-	@Override
-	public void completed(AsynchronousSocketChannel channel, AsynchronousServerSocketChannel server) {
-		LOGGER.debug("TCP连接成功：{}", channel);
-		server.accept(server, this);
-		final T handler = BeanUtils.newInstance(this.clazz);
-		handler.handle(channel);
-		this.context.newInstance(handler);
-	}
-	
-	@Override
-	public void failed(Throwable throwable, AsynchronousServerSocketChannel server) {
-		LOGGER.error("TCP连接异常：{}", server, throwable);
-	}
-	
+    private static final Logger LOGGER = LoggerFactory.getLogger(TcpAcceptHandler.class);
+    
+    /**
+     * 消息代理类型
+     */
+    private final Class<T> clazz;
+    /**
+     * 消息代理上下文
+     */
+    private final MessageHandlerContext context;
+    
+    /**
+     * @param clazz 消息代理类型
+     */
+    private TcpAcceptHandler(Class<T> clazz) {
+        this.clazz   = clazz;
+        this.context = MessageHandlerContext.getInstance();
+    }
+    
+    /**
+     * 新建TCP消息接收代理
+     * 
+     * @param <T> 消息代理类型
+     * 
+     * @param clazz 消息代理类型
+     * 
+     * @return TCP消息接收代理
+     */
+    public static final <T extends TcpMessageHandler> TcpAcceptHandler<T> newInstance(Class<T> clazz) {
+        return new TcpAcceptHandler<>(clazz);
+    }
+    
+    @Override
+    public void completed(AsynchronousSocketChannel channel, AsynchronousServerSocketChannel server) {
+        LOGGER.debug("TCP连接成功：{}", channel);
+        server.accept(server, this);
+        final T handler = BeanUtils.newInstance(this.clazz);
+        handler.handle(channel);
+        this.context.newInstance(handler);
+    }
+    
+    @Override
+    public void failed(Throwable throwable, AsynchronousServerSocketChannel server) {
+        LOGGER.error("TCP连接异常：{}", server, throwable);
+    }
+    
 }
--- snail/src/main/java/com/acgist/snail/net/TcpClient.java
+++ snail/src/main/java/com/acgist/snail/net/TcpClient.java
@@ -5,6 +5,7 @@ import java.net.StandardSocketOptions;
 import java.nio.channels.AsynchronousChannelGroup;
 import java.nio.channels.AsynchronousSocketChannel;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -16,7 +17,7 @@ import com.acgist.snail.utils.IoUtils;
 import com.acgist.snail.utils.NetUtils;
 
 /**
- * <p>TCP客户端</p>
+ * TCP客户端
  * 
  * @param <T> TCP消息代理类型
  * 
@@ -24,97 +25,97 @@ import com.acgist.snail.utils.NetUtils;
  */
 public abstract class TcpClient<T extends TcpMessageHandler> extends Client<T> {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(TcpClient.class);
-	
-	/**
-	 * <p>客户端线程池</p>
-	 */
-	private static final AsynchronousChannelGroup GROUP;
-	
-	static {
-		AsynchronousChannelGroup group = null;
-		try {
-			final var executor = SystemThreadContext.newCacheExecutor(0, 60L, SystemThreadContext.SNAIL_THREAD_TCP_CLIENT);
-			group = AsynchronousChannelGroup.withThreadPool(executor);
-		} catch (IOException e) {
-			LOGGER.error("启动TCP Client Group异常", e);
-		}
-		GROUP = group;
-	}
-	
-	/**
-	 * <p>超时时间（秒）</p>
-	 */
-	private final int timeout;
-	
-	/**
-	 * <p>TCP客户端</p>
-	 * 
-	 * @param name 客户端名称
-	 * @param timeout 超时时间（秒）
-	 * @param handler 消息代理
-	 */
-	protected TcpClient(String name, int timeout, T handler) {
-		super(name, handler);
-		this.timeout = timeout;
-	}
-	
-	/**
-	 * <p>连接服务端</p>
-	 * 
-	 * @return 连接状态
-	 * 
-	 * @see #connect(String, int)
-	 */
-	public abstract boolean connect();
-	
-	/**
-	 * <p>连接服务端</p>
-	 * 
-	 * @param host 服务端地址
-	 * @param port 服务端端口
-	 * 
-	 * @return 连接状态
-	 */
-	protected boolean connect(final String host, final int port) {
-		boolean success = true;
-		AsynchronousSocketChannel channel = null;
-		try {
-			channel = AsynchronousSocketChannel.open(GROUP);
-//			channel.setOption(StandardSocketOptions.TCP_NODELAY, true);
-			channel.setOption(StandardSocketOptions.SO_REUSEADDR, true);
-			channel.setOption(StandardSocketOptions.SO_KEEPALIVE, true);
-			final Future<Void> future = channel.connect(NetUtils.buildSocketAddress(host, port));
-			future.get(this.timeout, TimeUnit.SECONDS);
-			this.handler.handle(channel);
-		} catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
-			LOGGER.error("TCP客户端连接异常：{}-{}", host, port, e);
-			success = false;
-		} catch (IOException | ExecutionException | TimeoutException e) {
-			LOGGER.error("TCP客户端连接异常：{}-{}", host, port, e);
-			success = false;
-		} finally {
-			if(!success) {
-				IoUtils.close(channel);
-				this.close();
-			}
-		}
-		return success;
-	}
-	
-	@Override
-	public void close() {
-		LOGGER.debug("关闭TCP Client：{}", this.name);
-		super.close();
-	}
+    private static final Logger LOGGER = LoggerFactory.getLogger(TcpClient.class);
+    
+    /**
+     * 客户端线程池
+     */
+    private static final AsynchronousChannelGroup GROUP;
+    
+    static {
+        AsynchronousChannelGroup group = null;
+        try {
+            final ExecutorService executor = SystemThreadContext.newCacheExecutor(0, 60L, SystemThreadContext.SNAIL_THREAD_TCP_CLIENT);
+            group = AsynchronousChannelGroup.withThreadPool(executor);
+        } catch (IOException e) {
+            LOGGER.error("启动TCP Client线程池异常", e);
+        }
+        GROUP = group;
+    }
+    
+    /**
+     * 超时时间（秒）
+     */
+    private final int timeout;
+    
+    /**
+     * TCP客户端
+     * 
+     * @param name    客户端名称
+     * @param timeout 超时时间（秒）
+     * @param handler 消息代理
+     */
+    protected TcpClient(String name, int timeout, T handler) {
+        super(name, handler);
+        this.timeout = timeout;
+    }
+    
+    /**
+     * 连接服务端
+     * 
+     * @return 连接状态
+     * 
+     * @see #connect(String, int)
+     */
+    public abstract boolean connect();
+    
+    /**
+     * 连接服务端
+     * 
+     * @param host 服务端地址
+     * @param port 服务端端口
+     * 
+     * @return 连接状态
+     */
+    protected boolean connect(final String host, final int port) {
+        boolean success = true;
+        AsynchronousSocketChannel channel = null;
+        try {
+            channel = AsynchronousSocketChannel.open(GROUP);
+//          channel.setOption(StandardSocketOptions.TCP_NODELAY, true);
+            channel.setOption(StandardSocketOptions.SO_REUSEADDR, true);
+            channel.setOption(StandardSocketOptions.SO_KEEPALIVE, true);
+            final Future<Void> future = channel.connect(NetUtils.buildSocketAddress(host, port));
+            future.get(this.timeout, TimeUnit.SECONDS);
+            this.handler.handle(channel);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            LOGGER.error("TCP客户端连接异常：{} - {}", host, port, e);
+            success = false;
+        } catch (IOException | ExecutionException | TimeoutException e) {
+            LOGGER.error("TCP客户端连接异常：{} - {}", host, port, e);
+            success = false;
+        } finally {
+            if(!success) {
+                IoUtils.close(channel);
+                this.close();
+            }
+        }
+        return success;
+    }
+    
+    @Override
+    public void close() {
+        LOGGER.debug("关闭TCP Client：{}", this.name);
+        super.close();
+    }
 
-	/**
-	 * <p>关闭TCP Client线程池</p>
-	 */
-	public static final void shutdown() {
-		LOGGER.debug("关闭TCP Client线程池");
-		SystemThreadContext.shutdown(GROUP);
-	}
+    /**
+     * 关闭TCP Client线程池
+     */
+    public static final void shutdown() {
+        LOGGER.debug("关闭TCP Client线程池");
+        SystemThreadContext.shutdown(GROUP);
+    }
 
 }
