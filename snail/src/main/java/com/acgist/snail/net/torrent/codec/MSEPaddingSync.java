e0622624ad3318da4e2ccce5487deacd954b26eb
acgist <289547414@qq.com>
2023-07-30 07:25:43 +0800

    [*] 日常优化

--- snail/src/main/java/com/acgist/snail/net/torrent/codec/MSEPaddingSync.java
+++ snail/src/main/java/com/acgist/snail/net/torrent/codec/MSEPaddingSync.java
@@ -10,110 +10,110 @@ import com.acgist.snail.utils.BeanUtils;
 import com.acgist.snail.utils.StringUtils;
 
 /**
- * <p>Padding数据同步工具</p>
- * <p>同步数据格式：(len(padding) + padding)+</p>
- * <p>长度数据类型：short</p>
+ * Padding数据同步工具
+ * 同步数据格式：(len(padding) + padding) + ...
+ * 长度数据类型：short
  * 
  * @author acgist
  */
 public final class MSEPaddingSync {
 
-	/**
-	 * <p>Padding数据数量</p>
-	 */
-	private int count;
-	/**
-	 * <p>当前Padding数据</p>
-	 */
-	private byte[] bytes;
-	/**
-	 * <p>剩余Padding数据长度</p>
-	 */
-	private short length;
-	/**
-	 * <p>Padding数据集合</p>
-	 */
-	private final List<byte[]> list;
-	
-	/**
-	 * @param count Padding数据数量
-	 */
-	private MSEPaddingSync(int count) {
-		this.length = -1;
-		this.count = count;
-		this.list = new ArrayList<>(count);
-	}
-	
-	/**
-	 * <p>新建同步工具</p>
-	 * 
-	 * @param count Padding数据数量
-	 * 
-	 * @return 同步工具
-	 */
-	public static final MSEPaddingSync newInstance(int count) {
-		return new MSEPaddingSync(count);
-	}
-	
-	/**
-	 * <p>Padding数据同步</p>
-	 * 
-	 * @param buffer Padding数据
-	 * 
-	 * @return 是否同步完成
-	 * 
-	 * @throws PacketSizeException 网络包大小异常
-	 */
-	public boolean sync(ByteBuffer buffer) throws PacketSizeException {
-		if(this.count == 0) {
-			return true;
-		}
-		// 没有剩余数据
-		if(!buffer.hasRemaining()) {
-			return false;
-		}
-		// 开始新的同步数据
-		if(this.length == -1) {
-			if(buffer.remaining() < 2) {
-				// 数据长度不够
-				return false;
-			}
-			// 获取数据长度
-			this.length = buffer.getShort();
-			PacketSizeException.verify(this.length);
-			this.bytes = new byte[this.length];
-		}
-		final int remaining = buffer.remaining();
-		if(this.length == 0) {
-			// 数据完整：没有数据
-			this.count--;
-			this.length = -1;
-			this.list.add(this.bytes);
-			buffer.compact().flip();
-			return this.sync(buffer);
-		} else if(remaining >= this.length) {
-			// 数据完整：含有数据
-			buffer.get(this.bytes, this.bytes.length - this.length, this.length);
-			this.count--;
-			this.length = -1;
-			this.list.add(this.bytes);
-			buffer.compact().flip();
-			return this.sync(buffer);
-		} else {
-			// 数据缺失
-			buffer.get(this.bytes, this.bytes.length - this.length, remaining);
-			this.length -= remaining;
-			buffer.compact();
-			return false;
-		}
-	}
-	
-	@Override
-	public String toString() {
-		final var padding = this.list.stream()
-			.map(StringUtils::hex)
-			.collect(Collectors.toList());
-		return BeanUtils.toString(this, this.count, this.length, padding);
-	}
-	
+    /**
+     * Padding数据数量
+     */
+    private int count;
+    /**
+     * 当前Padding数据
+     */
+    private byte[] bytes;
+    /**
+     * 剩余Padding数据长度
+     */
+    private short length;
+    /**
+     * Padding数据集合
+     */
+    private final List<byte[]> list;
+    
+    /**
+     * @param count Padding数据数量
+     */
+    private MSEPaddingSync(int count) {
+        this.length = -1;
+        this.count  = count;
+        this.list   = new ArrayList<>(count);
+    }
+    
+    /**
+     * 新建同步工具
+     * 
+     * @param count Padding数据数量
+     * 
+     * @return 同步工具
+     */
+    public static final MSEPaddingSync newInstance(int count) {
+        return new MSEPaddingSync(count);
+    }
+    
+    /**
+     * Padding数据同步
+     * 
+     * @param buffer Padding数据
+     * 
+     * @return 是否同步完成
+     * 
+     * @throws PacketSizeException 网络包大小异常
+     */
+    public boolean sync(ByteBuffer buffer) throws PacketSizeException {
+        if(this.count == 0) {
+            return true;
+        }
+        // 没有剩余数据
+        if(!buffer.hasRemaining()) {
+            return false;
+        }
+        // 开始新的同步数据
+        if(this.length == -1) {
+            if(buffer.remaining() < 2) {
+                // 数据长度不够
+                return false;
+            }
+            // 获取数据长度
+            this.length = buffer.getShort();
+            PacketSizeException.verify(this.length);
+            this.bytes  = new byte[this.length];
+        }
+        final int remaining = buffer.remaining();
+        if(this.length == 0) {
+            // 数据完整：没有数据
+            this.count--;
+            this.length = -1;
+            this.list.add(this.bytes);
+            buffer.compact().flip();
+            return this.sync(buffer);
+        } else if(remaining >= this.length) {
+            // 数据完整：含有数据
+            buffer.get(this.bytes, this.bytes.length - this.length, this.length);
+            this.count--;
+            this.length = -1;
+            this.list.add(this.bytes);
+            buffer.compact().flip();
+            return this.sync(buffer);
+        } else {
+            // 数据缺失
+            buffer.get(this.bytes, this.bytes.length - this.length, remaining);
+            this.length -= remaining;
+            buffer.compact();
+            return false;
+        }
+    }
+    
+    @Override
+    public String toString() {
+        final var padding = this.list.stream()
+            .map(StringUtils::hex)
+            .collect(Collectors.toList());
+        return BeanUtils.toString(this, this.count, this.length, padding);
+    }
+    
 }
--- snail/src/test/java/com/acgist/snail/net/torrent/codec/MSEPaddingSyncTest.java
+++ snail/src/test/java/com/acgist/snail/net/torrent/codec/MSEPaddingSyncTest.java
@@ -14,41 +14,41 @@ import com.acgist.snail.utils.Performance;
 
 class MSEPaddingSyncTest extends Performance {
 
-	@Test
-	void testToString() {
-		final MSEPaddingSync sync = MSEPaddingSync.newInstance(10);
-		this.log(sync.toString());
-		assertNotNull(sync.toString());
-	}
-	
-	@Test
-	void testSync() throws PacketSizeException {
-		final MSEPaddingSync sync = MSEPaddingSync.newInstance(3);
-		final ByteBuffer buffer = ByteBuffer.allocate(100);
-		buffer.putShort((short) 2).put("12".getBytes());
-		buffer.putShort((short) 100);
-		buffer.put("UU34".getBytes());
-		buffer.flip();
-		boolean success = sync.sync(buffer);
-		assertFalse(success);
-		final ByteBuffer append = ByteBuffer.allocate(98);
-		append.put("0".repeat(96).getBytes());
-		append.putShort((short) 0);
-		append.flip();
-		success = sync.sync(append);
-		assertTrue(success);
-//		sync.allPadding().forEach(bytes -> this.log("内容：" + new String(bytes)));
-	}
-	
-	@Test
-	void testCosted() throws NetException {
-		this.costed(100000, () -> {
-			try {
-				this.testSync();
-			} catch (PacketSizeException e) {
-				LOGGER.error("数据校验异常", e);
-			}
-		});
-	}
-	
+    @Test
+    void testToString() {
+        final MSEPaddingSync sync = MSEPaddingSync.newInstance(10);
+        this.log(sync.toString());
+        assertNotNull(sync.toString());
+    }
+    
+    @Test
+    void testSync() throws PacketSizeException {
+        final MSEPaddingSync sync = MSEPaddingSync.newInstance(3);
+        final ByteBuffer buffer = ByteBuffer.allocate(100);
+        buffer.putShort((short) 2).put("12".getBytes());
+        buffer.putShort((short) 100);
+        buffer.put("UU34".getBytes());
+        buffer.flip();
+        boolean success = sync.sync(buffer);
+        assertFalse(success);
+        final ByteBuffer append = ByteBuffer.allocate(98);
+        append.put("0".repeat(96).getBytes());
+        append.putShort((short) 0);
+        append.flip();
+        success = sync.sync(append);
+        assertTrue(success);
+//      sync.allPadding().forEach(bytes -> this.log("内容：" + new String(bytes)));
+    }
+    
+    @Test
+    void testCosted() throws NetException {
+        this.costed(100000, () -> {
+            try {
+                this.testSync();
+            } catch (PacketSizeException e) {
+                LOGGER.error("数据校验异常", e);
+            }
+        });
+    }
+    
 }
