ecd6ab024c4b9784708909ebdf97e7dcde6d9861
acgist <289547414@qq.com>
2023-07-31 07:25:01 +0800

    [*] 日常优化

--- snail/src/main/java/com/acgist/snail/net/torrent/codec/PeerCryptMessageCodec.java
+++ snail/src/main/java/com/acgist/snail/net/torrent/codec/PeerCryptMessageCodec.java
@@ -11,60 +11,60 @@ import com.acgist.snail.net.codec.MessageCodec;
 import com.acgist.snail.net.torrent.peer.PeerSubMessageHandler;
 
 /**
- * <p>Peer消息处理器：加密、解密</p>
+ * Peer消息处理器：加密、解密
  * 
  * @author acgist
  */
 public final class PeerCryptMessageCodec extends MessageCodec<ByteBuffer, ByteBuffer> {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(PeerCryptMessageCodec.class);
-	
-	/**
-	 * <p>MSE加密握手代理</p>
-	 */
-	private final MSECryptHandshakeHandler mseCryptHandshakeHandler;
-	
-	/**
-	 * @param peerUnpackMessageCodec Peer消息处理器
-	 * @param peerSubMessageHandler Peer消息代理
-	 */
-	public PeerCryptMessageCodec(PeerUnpackMessageCodec peerUnpackMessageCodec, PeerSubMessageHandler peerSubMessageHandler) {
-		super(peerUnpackMessageCodec);
-		this.mseCryptHandshakeHandler = MSECryptHandshakeHandler.newInstance(peerUnpackMessageCodec, peerSubMessageHandler);
-	}
-	
-	@Override
-	public ByteBuffer encode(ByteBuffer buffer) {
-		if(this.mseCryptHandshakeHandler.completed()) {
-			this.mseCryptHandshakeHandler.encrypt(buffer);
-		} else {
-			// TODO：判断是否或者还是并且
-			final boolean encrypt = this.mseCryptHandshakeHandler.needEncrypt() && CryptConfig.STRATEGY.getCrypt();
-			if(encrypt) {
-				// 需要加密
-				this.mseCryptHandshakeHandler.handshake();
-				this.mseCryptHandshakeHandler.lockHandshake();
-				this.mseCryptHandshakeHandler.encrypt(buffer);
-			} else {
-				// 不用加密：使用明文完成握手
-				this.mseCryptHandshakeHandler.plaintext();
-			}
-		}
-		return buffer;
-	}
-	
-	@Override
-	public void doDecode(ByteBuffer buffer, InetSocketAddress address) throws NetException {
-		if(this.mseCryptHandshakeHandler.available()) {
-			if(this.mseCryptHandshakeHandler.completed()) {
-				this.mseCryptHandshakeHandler.decrypt(buffer);
-				this.doNext(buffer, address);
-			} else {
-				this.mseCryptHandshakeHandler.handshake(buffer);
-			}
-		} else {
-			LOGGER.debug("Peer消息代理不可用：忽略消息解密");
-		}
-	}
-	
+    private static final Logger LOGGER = LoggerFactory.getLogger(PeerCryptMessageCodec.class);
+    
+    /**
+     * MSE加密握手代理
+     */
+    private final MSECryptHandshakeHandler mseCryptHandshakeHandler;
+    
+    /**
+     * @param peerUnpackMessageCodec Peer消息处理器
+     * @param peerSubMessageHandler  Peer消息代理
+     */
+    public PeerCryptMessageCodec(PeerUnpackMessageCodec peerUnpackMessageCodec, PeerSubMessageHandler peerSubMessageHandler) {
+        super(peerUnpackMessageCodec);
+        this.mseCryptHandshakeHandler = MSECryptHandshakeHandler.newInstance(peerUnpackMessageCodec, peerSubMessageHandler);
+    }
+    
+    @Override
+    public ByteBuffer encode(ByteBuffer buffer) {
+        if(this.mseCryptHandshakeHandler.completed()) {
+            this.mseCryptHandshakeHandler.encrypt(buffer);
+        } else {
+            // TODO：判断是否或者还是并且
+            final boolean encrypt = this.mseCryptHandshakeHandler.needEncrypt() && CryptConfig.STRATEGY.getCrypt();
+            if(encrypt) {
+                // 需要加密
+                this.mseCryptHandshakeHandler.handshake();
+                this.mseCryptHandshakeHandler.lockHandshake();
+                this.mseCryptHandshakeHandler.encrypt(buffer);
+            } else {
+                // 不用加密：使用明文完成握手
+                this.mseCryptHandshakeHandler.plaintext();
+            }
+        }
+        return buffer;
+    }
+    
+    @Override
+    public void doDecode(ByteBuffer buffer, InetSocketAddress address) throws NetException {
+        if(this.mseCryptHandshakeHandler.available()) {
+            if(this.mseCryptHandshakeHandler.completed()) {
+                this.mseCryptHandshakeHandler.decrypt(buffer);
+                this.doNext(buffer, address);
+            } else {
+                this.mseCryptHandshakeHandler.handshake(buffer);
+            }
+        } else {
+            LOGGER.debug("Peer消息代理不可用：忽略消息解密");
+        }
+    }
+    
 }
--- snail/src/main/java/com/acgist/snail/net/torrent/codec/PeerUnpackMessageCodec.java
+++ snail/src/main/java/com/acgist/snail/net/torrent/codec/PeerUnpackMessageCodec.java
@@ -10,99 +10,99 @@ import com.acgist.snail.net.codec.MessageCodec;
 import com.acgist.snail.net.torrent.peer.PeerSubMessageHandler;
 
 /**
- * <p>Peer消息处理器：拆包</p>
+ * Peer消息处理器：拆包
  * 
  * @author acgist
  */
 public final class PeerUnpackMessageCodec extends MessageCodec<ByteBuffer, ByteBuffer> {
 
-	/**
-	 * <p>消息缓存</p>
-	 */
-	private ByteBuffer buffer;
-	/**
-	 * <p>消息长度</p>
-	 */
-	private final ByteBuffer lengthStick;
-	/**
-	 * <p>Peer消息代理</p>
-	 */
-	private final PeerSubMessageHandler peerSubMessageHandler;
-	
-	/**
-	 * @param peerSubMessageHandler Peer消息代理
-	 */
-	public PeerUnpackMessageCodec(PeerSubMessageHandler peerSubMessageHandler) {
-		super(peerSubMessageHandler);
-		this.lengthStick = ByteBuffer.allocate(Integer.BYTES);
-		this.peerSubMessageHandler = peerSubMessageHandler;
-	}
-	
-	@Override
-	public void doDecode(ByteBuffer buffer, InetSocketAddress address) throws NetException {
-		// 消息数据长度
-		int length = 0;
-		while(true) {
-			if(this.buffer == null) {
-				if(this.peerSubMessageHandler.handshakeRecv()) {
-					while(this.lengthStick.hasRemaining() && buffer.hasRemaining()) {
-						this.lengthStick.put(buffer.get());
-					}
-					if(this.lengthStick.hasRemaining()) {
-						// 消息长度缺失跳出
-						break;
-					} else {
-						this.lengthStick.flip();
-						length = this.lengthStick.getInt();
-						this.lengthStick.compact();
-					}
-				} else {
-					// 握手消息长度
-					length = PeerConfig.HANDSHAKE_LENGTH;
-				}
-				// 心跳消息
-				if(length <= 0) {
-					this.peerSubMessageHandler.keepAlive();
-					if(buffer.hasRemaining()) {
-						// 还有消息：继续处理
-						continue;
-					} else {
-						// 没有消息：跳出循环
-						break;
-					}
-				}
-				PacketSizeException.verify(length);
-				this.buffer = ByteBuffer.allocate(length);
-			} else {
-				// 上次消息没有读取完成：计算剩余消息数据长度
-				length = this.buffer.capacity() - this.buffer.position();
-			}
-			final int remaining = buffer.remaining();
-			if(remaining > length) {
-				// 包含一条完整消息：继续读取、清空缓存
-				final byte[] bytes = new byte[length];
-				buffer.get(bytes);
-				this.buffer.put(bytes);
-				this.buffer.flip();
-				this.doNext(this.buffer, address);
-				this.buffer = null;
-			} else if(remaining == length) {
-				// 刚好一条完整消息：跳出循环、清空缓存
-				final byte[] bytes = new byte[length];
-				buffer.get(bytes);
-				this.buffer.put(bytes);
-				this.buffer.flip();
-				this.doNext(this.buffer, address);
-				this.buffer = null;
-				break;
-			} else if(remaining < length) {
-				// 不是一条完整消息：跳出循环、保留缓存
-				final byte[] bytes = new byte[remaining];
-				buffer.get(bytes);
-				this.buffer.put(bytes);
-				break;
-			}
-		}
-	}
+    /**
+     * 消息缓存
+     */
+    private ByteBuffer buffer;
+    /**
+     * 消息长度
+     */
+    private final ByteBuffer lengthStick;
+    /**
+     * Peer消息代理
+     */
+    private final PeerSubMessageHandler peerSubMessageHandler;
+    
+    /**
+     * @param peerSubMessageHandler Peer消息代理
+     */
+    public PeerUnpackMessageCodec(PeerSubMessageHandler peerSubMessageHandler) {
+        super(peerSubMessageHandler);
+        this.lengthStick           = ByteBuffer.allocate(Integer.BYTES);
+        this.peerSubMessageHandler = peerSubMessageHandler;
+    }
+    
+    @Override
+    public void doDecode(ByteBuffer buffer, InetSocketAddress address) throws NetException {
+        // 消息数据长度
+        int length = 0;
+        while(true) {
+            if(this.buffer == null) {
+                if(this.peerSubMessageHandler.handshakeRecv()) {
+                    while(this.lengthStick.hasRemaining() && buffer.hasRemaining()) {
+                        this.lengthStick.put(buffer.get());
+                    }
+                    if(this.lengthStick.hasRemaining()) {
+                        // 消息长度缺失跳出
+                        break;
+                    } else {
+                        this.lengthStick.flip();
+                        length = this.lengthStick.getInt();
+                        this.lengthStick.compact();
+                    }
+                } else {
+                    // 握手消息长度
+                    length = PeerConfig.HANDSHAKE_LENGTH;
+                }
+                // 心跳消息
+                if(length <= 0) {
+                    this.peerSubMessageHandler.keepAlive();
+                    if(buffer.hasRemaining()) {
+                        // 还有消息：继续处理
+                        continue;
+                    } else {
+                        // 没有消息：跳出循环
+                        break;
+                    }
+                }
+                PacketSizeException.verify(length);
+                this.buffer = ByteBuffer.allocate(length);
+            } else {
+                // 上次消息没有读取完成：计算剩余消息数据长度
+                length = this.buffer.capacity() - this.buffer.position();
+            }
+            final int remaining = buffer.remaining();
+            if(remaining > length) {
+                // 包含一条完整消息：继续读取、清空缓存
+                final byte[] bytes = new byte[length];
+                buffer.get(bytes);
+                this.buffer.put(bytes);
+                this.buffer.flip();
+                this.doNext(this.buffer, address);
+                this.buffer = null;
+            } else if(remaining == length) {
+                // 刚好一条完整消息：跳出循环、清空缓存
+                final byte[] bytes = new byte[length];
+                buffer.get(bytes);
+                this.buffer.put(bytes);
+                this.buffer.flip();
+                this.doNext(this.buffer, address);
+                this.buffer = null;
+                break;
+            } else if(remaining < length) {
+                // 不是一条完整消息：跳出循环、保留缓存
+                final byte[] bytes = new byte[remaining];
+                buffer.get(bytes);
+                this.buffer.put(bytes);
+                break;
+            }
+        }
+    }
 
 }
