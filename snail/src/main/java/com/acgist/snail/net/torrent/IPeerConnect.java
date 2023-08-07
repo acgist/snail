a276c7d77b9312838d84ab7dba381572004dd62d
acgist <289547414@qq.com>
2023-08-07 09:07:23 +0800

    [*] 日常优化

--- snail/src/main/java/com/acgist/snail/net/torrent/IPeerConnect.java
+++ snail/src/main/java/com/acgist/snail/net/torrent/IPeerConnect.java
@@ -1,35 +1,33 @@
 package com.acgist.snail.net.torrent;
 
 /**
- * <p>Peer连接接口</p>
+ * Peer连接接口
  * 
  * @author acgist
  */
 public interface IPeerConnect {
 
-	/**
-	 * <p>连接类型</p>
-	 * 
-	 * @author acgist
-	 */
-	public enum ConnectType {
+    /**
+     * 连接类型
+     * 
+     * @author acgist
+     */
+    public enum ConnectType {
 
-		/**
-		 * <p>UTP</p>
-		 */
-		UTP,
-		/**
-		 * <p>TCP</p>
-		 */
-		TCP;
-		
-	}
-	
-	/**
-	 * <p>获取连接类型</p>
-	 * 
-	 * @return 连接类型
-	 */
-	ConnectType connectType();
-	
+        /**
+         * UTP
+         */
+        UTP,
+        /**
+         * TCP
+         */
+        TCP;
+        
+    }
+    
+    /**
+     * @return 连接类型
+     */
+    ConnectType connectType();
+    
 }
