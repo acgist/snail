927b5c24724cb1a39f6dd4911ae25fa43ed26807
acgist <289547414@qq.com>
2023-08-10 07:25:46 +0800

    [*] 日常优化

--- snail/src/main/java/com/acgist/snail/net/torrent/peer/DhtExtensionMessageHandler.java
+++ snail/src/main/java/com/acgist/snail/net/torrent/peer/DhtExtensionMessageHandler.java
@@ -11,77 +11,77 @@ import com.acgist.snail.utils.NetUtils;
 import com.acgist.snail.utils.NumberUtils;
 
 /**
- * <p>DHT Extension</p>
- * <p>协议链接：http://www.bittorrent.org/beps/bep_0004.html</p>
+ * DHT Extension
+ * 协议链接：http://www.bittorrent.org/beps/bep_0004.html
  * 
  * @author acgist
  */
 public final class DhtExtensionMessageHandler implements IExtensionMessageHandler {
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(DhtExtensionMessageHandler.class);
-	
-	/**
-	 * <p>Peer信息</p>
-	 */
-	private final PeerSession peerSession;
-	/**
-	 * <p>BT任务信息</p>
-	 */
-	private final TorrentSession torrentSession;
-	/**
-	 * <p>Peer消息代理</p>
-	 */
-	private final PeerSubMessageHandler peerSubMessageHandler;
-	
-	/**
-	 * @param peerSession Peer信息
-	 * @param torrentSession BT任务信息
-	 * @param peerSubMessageHandler Peer消息代理
-	 */
-	private DhtExtensionMessageHandler(PeerSession peerSession, TorrentSession torrentSession, PeerSubMessageHandler peerSubMessageHandler) {
-		this.peerSession = peerSession;
-		this.torrentSession = torrentSession;
-		this.peerSubMessageHandler = peerSubMessageHandler;
-	}
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(DhtExtensionMessageHandler.class);
+    
+    /**
+     * Peer信息
+     */
+    private final PeerSession peerSession;
+    /**
+     * BT任务信息
+     */
+    private final TorrentSession torrentSession;
+    /**
+     * Peer消息代理
+     */
+    private final PeerSubMessageHandler peerSubMessageHandler;
+    
+    /**
+     * @param peerSession           Peer信息
+     * @param torrentSession        BT任务信息
+     * @param peerSubMessageHandler Peer消息代理
+     */
+    private DhtExtensionMessageHandler(PeerSession peerSession, TorrentSession torrentSession, PeerSubMessageHandler peerSubMessageHandler) {
+        this.peerSession           = peerSession;
+        this.torrentSession        = torrentSession;
+        this.peerSubMessageHandler = peerSubMessageHandler;
+    }
 
-	/**
-	 * <p>新建DHT扩展协议代理</p>
-	 * 
-	 * @param peerSession Peer信息
-	 * @param torrentSession BT任务信息
-	 * @param peerSubMessageHandler Peer消息代理
-	 * 
-	 * @return DHT扩展协议代理
-	 */
-	public static final DhtExtensionMessageHandler newInstance(PeerSession peerSession, TorrentSession torrentSession, PeerSubMessageHandler peerSubMessageHandler) {
-		return new DhtExtensionMessageHandler(peerSession, torrentSession, peerSubMessageHandler);
-	}
-	
-	@Override
-	public void onMessage(ByteBuffer buffer) {
-		this.port(buffer);
-	}
+    /**
+     * 新建DHT扩展协议代理
+     * 
+     * @param peerSession           Peer信息
+     * @param torrentSession        BT任务信息
+     * @param peerSubMessageHandler Peer消息代理
+     * 
+     * @return DHT扩展协议代理
+     */
+    public static final DhtExtensionMessageHandler newInstance(PeerSession peerSession, TorrentSession torrentSession, PeerSubMessageHandler peerSubMessageHandler) {
+        return new DhtExtensionMessageHandler(peerSession, torrentSession, peerSubMessageHandler);
+    }
+    
+    @Override
+    public void onMessage(ByteBuffer buffer) {
+        this.port(buffer);
+    }
 
-	/**
-	 * <p>发送DHT消息</p>
-	 */
-	public void port() {
-		LOGGER.debug("发送DHT消息");
-		final byte[] bytes = NumberUtils.shortToBytes(SystemConfig.getTorrentPortExtShort());
-		this.peerSubMessageHandler.pushMessage(PeerConfig.Type.DHT, bytes);
-	}
-	
-	/**
-	 * <p>处理DHT消息</p>
-	 * 
-	 * @param buffer 消息
-	 */
-	private void port(ByteBuffer buffer) {
-		final int port = NetUtils.portToInt(buffer.getShort());
-		final String host = this.peerSession.host();
-		LOGGER.debug("处理DHT消息：{}-{}", host, port);
-		this.peerSession.dhtPort(port);
-		this.torrentSession.newNode(host, port);
-	}
+    /**
+     * 发送DHT消息
+     */
+    public void port() {
+        LOGGER.debug("发送DHT消息");
+        final byte[] bytes = NumberUtils.shortToBytes(SystemConfig.getTorrentPortExtShort());
+        this.peerSubMessageHandler.pushMessage(PeerConfig.Type.DHT, bytes);
+    }
+    
+    /**
+     * 处理DHT消息
+     * 
+     * @param buffer 消息
+     */
+    private void port(ByteBuffer buffer) {
+        final int port    = NetUtils.portToInt(buffer.getShort());
+        final String host = this.peerSession.host();
+        LOGGER.debug("处理DHT消息：{} - {}", host, port);
+        this.peerSession.dhtPort(port);
+        this.torrentSession.newNode(host, port);
+    }
 
 }
--- snail/src/main/java/com/acgist/snail/net/torrent/peer/extension/DontHaveExtensionMessageHandler.java
+++ snail/src/main/java/com/acgist/snail/net/torrent/peer/extension/DontHaveExtensionMessageHandler.java
@@ -11,61 +11,61 @@ import com.acgist.snail.net.torrent.peer.PeerSession;
 import com.acgist.snail.utils.NumberUtils;
 
 /**
- * <p>The lt_donthave extension</p>
- * <p>协议链接：http://bittorrent.org/beps/bep_0054.html</p>
- * <p>不再含有某个Piece</p>
+ * The lt_donthave extension
+ * 协议链接：http://bittorrent.org/beps/bep_0054.html
+ * 不再含有某个Piece
  * 
  * @author acgist
  */
 public final class DontHaveExtensionMessageHandler extends ExtensionTypeMessageHandler {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(DontHaveExtensionMessageHandler.class);
-	
-	/**
-	 * @param peerSession Peer信息
-	 * @param extensionMessageHandler 扩展协议代理
-	 */
-	private DontHaveExtensionMessageHandler(PeerSession peerSession, ExtensionMessageHandler extensionMessageHandler) {
-		super(ExtensionType.LT_DONTHAVE, peerSession, extensionMessageHandler);
-	}
+    private static final Logger LOGGER = LoggerFactory.getLogger(DontHaveExtensionMessageHandler.class);
+    
+    /**
+     * @param peerSession             Peer信息
+     * @param extensionMessageHandler 扩展协议代理
+     */
+    private DontHaveExtensionMessageHandler(PeerSession peerSession, ExtensionMessageHandler extensionMessageHandler) {
+        super(ExtensionType.LT_DONTHAVE, peerSession, extensionMessageHandler);
+    }
 
-	/**
-	 * <p>新建dontHave扩展协议代理</p>
-	 * 
-	 * @param peerSession Peer信息
-	 * @param extensionMessageHandler 扩展协议代理
-	 * 
-	 * @return dontHave扩展协议代理
-	 */
-	public static final DontHaveExtensionMessageHandler newInstance(PeerSession peerSession, ExtensionMessageHandler extensionMessageHandler) {
-		return new DontHaveExtensionMessageHandler(peerSession, extensionMessageHandler);
-	}
-	
-	@Override
-	protected void doMessage(ByteBuffer buffer) {
-		this.dontHave(buffer);
-	}
-	
-	/**
-	 * <p>发送dontHave消息</p>
-	 * 
-	 * @param index Piece索引
-	 */
-	public void dontHave(int index) {
-		LOGGER.debug("发送dontHave消息：{}", index);
-		final byte[] bytes = NumberUtils.intToBytes(index);
-		this.pushMessage(bytes);
-	}
-	
-	/**
-	 * <p>处理dontHave消息</p>
-	 * 
-	 * @param buffer 消息
-	 */
-	private void dontHave(ByteBuffer buffer) {
-		final int index = buffer.getInt();
-		LOGGER.debug("处理dontHave消息：{}", index);
-		this.peerSession.pieceOff(index);
-	}
+    /**
+     * 新建dontHave扩展协议代理
+     * 
+     * @param peerSession             Peer信息
+     * @param extensionMessageHandler 扩展协议代理
+     * 
+     * @return dontHave扩展协议代理
+     */
+    public static final DontHaveExtensionMessageHandler newInstance(PeerSession peerSession, ExtensionMessageHandler extensionMessageHandler) {
+        return new DontHaveExtensionMessageHandler(peerSession, extensionMessageHandler);
+    }
+    
+    @Override
+    protected void doMessage(ByteBuffer buffer) {
+        this.dontHave(buffer);
+    }
+    
+    /**
+     * 发送dontHave消息
+     * 
+     * @param index Piece索引
+     */
+    public void dontHave(int index) {
+        LOGGER.debug("发送dontHave消息：{}", index);
+        final byte[] bytes = NumberUtils.intToBytes(index);
+        this.pushMessage(bytes);
+    }
+    
+    /**
+     * 处理dontHave消息
+     * 
+     * @param buffer 消息
+     */
+    private void dontHave(ByteBuffer buffer) {
+        final int index = buffer.getInt();
+        LOGGER.debug("处理dontHave消息：{}", index);
+        this.peerSession.pieceOff(index);
+    }
 
 }
