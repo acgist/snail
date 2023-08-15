5d4c16895a278650c5bf211414d0ccaa081584ea
acgist <289547414@qq.com>
2023-08-16 07:31:33 +0800

    [*] 日常优化

--- snail/src/test/java/com/acgist/snail/net/torrent/peer/PeerConnectSessionTest.java
+++ snail/src/test/java/com/acgist/snail/net/torrent/peer/PeerConnectSessionTest.java
@@ -12,34 +12,34 @@ import com.acgist.snail.utils.ThreadUtils;
 
 class PeerConnectSessionTest extends Performance {
 
-	@Test
-	void testPeerConnectSession() {
-		final PeerConnectSession session = new PeerConnectSession();
-		assertTrue(session.isAmChoked());
-		assertTrue(session.isPeerChoked());
-		assertFalse(session.isAmInterested());
-		assertFalse(session.isPeerInterested());
-		session.amUnchoked();
-		session.peerUnchoked();
-		session.amInterested();
-		session.peerInterested();
-		assertFalse(session.isAmChoked());
-		assertFalse(session.isPeerChoked());
-		assertTrue(session.isAmInterested());
-		assertTrue(session.isPeerInterested());
-		session.upload(1024);
-		session.download(1024 * 2);
-		assertEquals(DownloadConfig.getUploadBufferByte(), session.uploadMark());
-		assertEquals(DownloadConfig.getDownloadBufferByte(), session.downloadMark());
-		ThreadUtils.sleep(60000L);
-		assertEquals(1024, session.uploadMark());
-		assertEquals(1024 * 2, session.downloadMark());
-		ThreadUtils.sleep(30000L);
-		assertEquals(1024, session.uploadMark());
-		assertEquals(1024 * 2, session.downloadMark());
-		ThreadUtils.sleep(30000L);
-		assertEquals(0, session.uploadMark());
-		assertEquals(0, session.downloadMark());
-	}
-	
+    @Test
+    void testPeerConnectSession() {
+        final PeerConnectSession session = new PeerConnectSession();
+        assertTrue(session.isAmChoked());
+        assertTrue(session.isPeerChoked());
+        assertFalse(session.isAmInterested());
+        assertFalse(session.isPeerInterested());
+        session.amUnchoked();
+        session.peerUnchoked();
+        session.amInterested();
+        session.peerInterested();
+        assertFalse(session.isAmChoked());
+        assertFalse(session.isPeerChoked());
+        assertTrue(session.isAmInterested());
+        assertTrue(session.isPeerInterested());
+        session.upload(1024);
+        session.download(1024 * 2);
+        assertEquals(DownloadConfig.getUploadBufferByte(), session.uploadMark());
+        assertEquals(DownloadConfig.getDownloadBufferByte(), session.downloadMark());
+        ThreadUtils.sleep(60000L);
+        assertEquals(1024, session.uploadMark());
+        assertEquals(1024 * 2, session.downloadMark());
+        ThreadUtils.sleep(30000L);
+        assertEquals(1024, session.uploadMark());
+        assertEquals(1024 * 2, session.downloadMark());
+        ThreadUtils.sleep(30000L);
+        assertEquals(0, session.uploadMark());
+        assertEquals(0, session.downloadMark());
+    }
+    
 }
--- snail/src/test/java/com/acgist/snail/net/torrent/peer/PeerContextTest.java
+++ snail/src/test/java/com/acgist/snail/net/torrent/peer/PeerContextTest.java
@@ -10,14 +10,14 @@ import com.acgist.snail.utils.Performance;
 
 class PeerContextTest extends Performance {
 
-	@Test
-	void testNewPeerSession() {
-		final String hash = "1".repeat(20);
-		this.costed(100000, 100, () -> {
-			PeerContext.getInstance().newPeerSession(hash, StatisticsContext.getInstance().getStatistics(), "192.168.1.100", 1000, Source.CONNECT);
-		});
-		this.log(PeerContext.getInstance().listPeerSession(hash).size());
-		assertEquals(1, PeerContext.getInstance().listPeerSession(hash).size());
-	}
-	
+    @Test
+    void testNewPeerSession() {
+        final String hash = "1".repeat(20);
+        this.costed(100000, 100, () -> {
+            PeerContext.getInstance().newPeerSession(hash, StatisticsContext.getInstance().getStatistics(), "192.168.1.100", 1000, Source.CONNECT);
+        });
+        this.log(PeerContext.getInstance().listPeerSession(hash).size());
+        assertEquals(1, PeerContext.getInstance().listPeerSession(hash).size());
+    }
+    
 }
