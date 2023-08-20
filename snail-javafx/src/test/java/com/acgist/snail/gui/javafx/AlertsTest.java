b6f90752b71ca7976072159745170d7a0c4b0ba5
acgist <289547414@qq.com>
2023-08-21 07:53:42 +0800

    [*] 日常优化

--- snail-javafx/src/test/java/com/acgist/snail/gui/javafx/AlertsTest.java
+++ snail-javafx/src/test/java/com/acgist/snail/gui/javafx/AlertsTest.java
@@ -14,25 +14,25 @@ import javafx.scene.control.ButtonType;
 
 class AlertsTest extends Performance {
 
-	@Test
-	void testAlerts() throws InterruptedException {
-		final Object lock = new Object();
-		Platform.startup(() -> {});
-		Platform.runLater(() -> {
-			Alerts.info("测试", "测试信息");
-			Alerts.warn("警告", "警告信息");
-			Optional<ButtonType> optional;
-			do {
-				optional = Alerts.build("确认", "是否删除？", MessageType.CONFIRM);
-			} while (optional.isPresent() && optional.get() != ButtonType.OK);
-			synchronized (lock) {
-				lock.notify();
-			}
-		});
-		synchronized (lock) {
-			lock.wait();
-		}
-		assertNotNull(lock);
-	}
-	
+    @Test
+    void testAlerts() throws InterruptedException {
+        final Object lock = new Object();
+        Platform.startup(() -> {});
+        Platform.runLater(() -> {
+            Alerts.info("测试", "测试信息");
+            Alerts.warn("警告", "警告信息");
+            Optional<ButtonType> optional;
+            do {
+                optional = Alerts.build("确认", "是否删除？", MessageType.CONFIRM);
+            } while (optional.isPresent() && optional.get() != ButtonType.OK);
+            synchronized (lock) {
+                lock.notify();
+            }
+        });
+        synchronized (lock) {
+            lock.wait();
+        }
+        assertNotNull(lock);
+    }
+    
 }
