b51bcbd9566a01647e3a46996b9b11165aab855a
acgist <289547414@qq.com>
2023-08-27 08:24:59 +0800

    [*] 日常优化

--- snail-javafx/src/main/java/com/acgist/snail/gui/javafx/window/statistics/StatisticsWindow.java
+++ snail-javafx/src/main/java/com/acgist/snail/gui/javafx/window/statistics/StatisticsWindow.java
@@ -6,36 +6,36 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 /**
- * <p>统计窗口</p>
+ * 统计窗口
  * 
  * @author acgist
  */
 public final class StatisticsWindow extends Window<StatisticsController> {
-	
-	private static final StatisticsWindow INSTANCE;
-	
-	public static final StatisticsWindow getInstance() {
-		return INSTANCE;
-	}
-	
-	static {
-		INSTANCE = new StatisticsWindow();
-	}
-	
-	private StatisticsWindow() {
-		super("统计", 800, 640, Modality.APPLICATION_MODAL, "/fxml/statistics.fxml");
-	}
-	
-	@Override
-	public void start(Stage stage) throws Exception {
-		this.dialogWindow();
-		this.hiddenRelease();
-	}
-	
-	@Override
-	public void show() {
-		this.controller.statistics();
-		super.show();
-	}
-	
+    
+    private static final StatisticsWindow INSTANCE;
+    
+    public static final StatisticsWindow getInstance() {
+        return INSTANCE;
+    }
+    
+    static {
+        INSTANCE = new StatisticsWindow();
+    }
+    
+    private StatisticsWindow() {
+        super("统计", 800, 640, Modality.APPLICATION_MODAL, "/fxml/statistics.fxml");
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.dialogWindow();
+        this.hiddenRelease();
+    }
+    
+    @Override
+    public void show() {
+        this.controller.statistics();
+        super.show();
+    }
+    
 }
--- snail-javafx/src/main/java/com/acgist/snail/gui/javafx/window/torrent/TorrentController.java
+++ snail-javafx/src/main/java/com/acgist/snail/gui/javafx/window/torrent/TorrentController.java
@@ -1,6 +1,7 @@
 package com.acgist.snail.gui.javafx.window.torrent;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import com.acgist.snail.context.ITaskSession;
@@ -14,6 +15,8 @@ import com.acgist.snail.net.DownloadException;
 import com.acgist.snail.net.torrent.Torrent;
 import com.acgist.snail.net.torrent.TorrentContext;
 import com.acgist.snail.net.torrent.TorrentFile;
+import com.acgist.snail.net.torrent.TorrentInfo;
+import com.acgist.snail.net.torrent.TorrentSession;
 import com.acgist.snail.protocol.Protocol.Type;
 
 import javafx.event.ActionEvent;
@@ -26,139 +29,142 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 /**
- * <p>编辑任务窗口控制器</p>
+ * 编辑任务窗口控制器
  * 
  * @author acgist
  */
 public final class TorrentController extends Controller {
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(TorrentController.class);
-	
-	/**
-	 * <p>下载按钮高度：{@value}</p>
-	 */
-	private static final double DOWNLOAD_BUTTON_HEIGHT = 40D;
-	
-	@FXML
-	private FlowPane root;
-	@FXML
-	private VBox treeBox;
-	@FXML
-	private VBox downloadBox;
-	@FXML
-	private Button download;
-	
-	/**
-	 * <p>任务信息</p>
-	 */
-	private ITaskSession taskSession;
-	/**
-	 * <p>BT任务下载文件选择器</p>
-	 */
-	private TorrentSelector torrentSelector;
-	
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		// 设置属性
-		this.downloadBox.prefWidthProperty().bind(this.root.widthProperty());
-		this.downloadBox.prefHeightProperty().setValue(DOWNLOAD_BUTTON_HEIGHT);
-		this.treeBox.prefWidthProperty().bind(this.root.widthProperty());
-		this.treeBox.prefHeightProperty().bind(this.root.heightProperty().subtract(DOWNLOAD_BUTTON_HEIGHT));
-		// 绑定事件
-		this.download.setOnAction(this.downloadEvent);
-	}
-	
-	@Override
-	public void release() {
-		super.release();
-		this.taskSession = null;
-		this.torrentSelector = null;
-		this.treeBox.getChildren().clear();
-	}
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(TorrentController.class);
+    
+    /**
+     * 下载按钮高度：{@value}
+     */
+    private static final double DOWNLOAD_BUTTON_HEIGHT = 40D;
+    
+    @FXML
+    private FlowPane root;
+    @FXML
+    private VBox treeBox;
+    @FXML
+    private VBox downloadBox;
+    @FXML
+    private Button download;
+    
+    /**
+     * 任务信息
+     */
+    private ITaskSession taskSession;
+    /**
+     * BT任务下载文件选择器
+     */
+    private TorrentSelector torrentSelector;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // 设置属性
+        this.downloadBox.prefWidthProperty().bind(this.root.widthProperty());
+        this.downloadBox.prefHeightProperty().setValue(DOWNLOAD_BUTTON_HEIGHT);
+        this.treeBox.prefWidthProperty().bind(this.root.widthProperty());
+        this.treeBox.prefHeightProperty().bind(this.root.heightProperty().subtract(DOWNLOAD_BUTTON_HEIGHT));
+        // 绑定事件
+        this.download.setOnAction(this.downloadEvent);
+    }
+    
+    @Override
+    public void release() {
+        super.release();
+        this.taskSession     = null;
+        this.torrentSelector = null;
+        this.treeBox.getChildren().clear();
+    }
 
-	/**
-	 * <p>新建文件选择树形菜单</p>
-	 * 
-	 * @param taskSession 任务信息
-	 */
-	public void buildTree(ITaskSession taskSession) {
-		Torrent torrent = null;
-		final TreeView<HBox> tree = this.buildTree();
-		try {
-			torrent = TorrentContext.getInstance().newTorrentSession(taskSession.getTorrent()).torrent();
-		} catch (DownloadException e) {
-			LOGGER.error("种子文件解析异常", e);
-			Alerts.warn("下载失败", e.getMessage());
-		}
-		if(torrent != null) {
-			this.taskSession = taskSession;
-			this.torrentSelector = TorrentSelector.newInstance(torrent.name(), this.download, tree);
-			torrent.getInfo().files().stream()
-				.filter(TorrentFile::notPaddingFile)
-				.forEach(this.torrentSelector::build);
-			this.torrentSelector.select(taskSession);
-		}
-	}
-	
-	/**
-	 * <p>新建树形菜单</p>
-	 * 
-	 * @return 树形菜单
-	 */
-	private TreeView<HBox> buildTree() {
-		final TreeView<HBox> tree = new TreeView<>();
-		tree.setId("tree");
-		tree.prefWidthProperty().bind(this.root.widthProperty());
-		tree.prefHeightProperty().bind(this.treeBox.heightProperty());
-		this.treeBox.getChildren().clear();
-		this.treeBox.getChildren().add(tree);
-		return tree;
-	}
-	
-	/**
-	 * <p>下载按钮事件</p>
-	 */
-	private EventHandler<ActionEvent> downloadEvent = event -> {
-		final var list = this.torrentSelector.selectedFilePath();
-		if(list.isEmpty()) {
-			Alerts.warn("下载失败", "请选择下载文件");
-			return;
-		}
-		this.taskSession.setSize(this.torrentSelector.selectedFileSize());
-		this.taskSession.setDescription(DescriptionWrapper.newEncoder(list).serialize());
-		if(this.taskSession.getId() != null) {
-			// 已经保存实体：修改任务
-			try {
-				this.updateTaskSession();
-			} catch (DownloadException e) {
-				LOGGER.error("更新下载任务异常", e);
-				Alerts.warn("下载失败", e.getMessage());
-			}
-		}
-		// 其他情况：新建任务自动刷新列表
-		TorrentWindow.getInstance().hide();
-	};
-	
-	/**
-	 * <p>更新下载任务</p>
-	 * 
-	 * @throws DownloadException 下载异常
-	 */
-	private void updateTaskSession() throws DownloadException {
-		final boolean magnetToTorrent = this.taskSession.getType() == Type.MAGNET;
-		if(magnetToTorrent) {
-			// 磁力链接任务转为BT任务
-			this.taskSession.magnetToTorrent();
-			// 切换下载器并且重新下载
-			this.taskSession.restart();
-		} else {
-			// 刷新任务
-			this.taskSession.refresh();
-		}
-		// 更新任务
-		this.taskSession.update();
-		// 刷新任务状态
-		TaskDisplay.getInstance().refreshTaskStatus();
-	}
-	
+    /**
+     * 新建文件选择树形菜单
+     * 
+     * @param taskSession 任务信息
+     */
+    public void buildTree(ITaskSession taskSession) {
+        Torrent torrent           = null;
+        final TreeView<HBox> tree = this.buildTree();
+        try {
+            final TorrentSession torrentSession = TorrentContext.getInstance().newTorrentSession(taskSession.getTorrent());
+            torrent = torrentSession.torrent();
+        } catch (DownloadException e) {
+            LOGGER.error("种子文件解析异常", e);
+            Alerts.warn("下载失败", e.getMessage());
+        }
+        if(torrent != null) {
+            this.taskSession     = taskSession;
+            this.torrentSelector = TorrentSelector.newInstance(torrent.name(), this.download, tree);
+            final TorrentInfo torrentInfo = torrent.getInfo();
+            final List<TorrentFile> files = torrentInfo.files();
+            files.stream()
+                .filter(TorrentFile::notPaddingFile)
+                .forEach(this.torrentSelector::build);
+            this.torrentSelector.select(taskSession);
+        }
+    }
+    
+    /**
+     * 新建树形菜单
+     * 
+     * @return 树形菜单
+     */
+    private TreeView<HBox> buildTree() {
+        final TreeView<HBox> tree = new TreeView<>();
+        tree.setId("tree");
+        tree.prefWidthProperty().bind(this.root.widthProperty());
+        tree.prefHeightProperty().bind(this.treeBox.heightProperty());
+        this.treeBox.getChildren().clear();
+        this.treeBox.getChildren().add(tree);
+        return tree;
+    }
+    
+    /**
+     * 下载按钮事件
+     */
+    private EventHandler<ActionEvent> downloadEvent = event -> {
+        final List<String> list = this.torrentSelector.selectedFilePath();
+        if(list.isEmpty()) {
+            Alerts.warn("下载失败", "请选择下载文件");
+            return;
+        }
+        this.taskSession.setSize(this.torrentSelector.selectedFileSize());
+        this.taskSession.setDescription(DescriptionWrapper.newEncoder(list).serialize());
+        if(this.taskSession.getId() != null) {
+            // 已经保存实体：修改任务
+            try {
+                this.updateTaskSession();
+            } catch (DownloadException e) {
+                LOGGER.error("更新下载任务异常", e);
+                Alerts.warn("下载失败", e.getMessage());
+            }
+        }
+        // 其他情况：新建任务自动刷新列表
+        TorrentWindow.getInstance().hide();
+    };
+    
+    /**
+     * 更新下载任务
+     * 
+     * @throws DownloadException 下载异常
+     */
+    private void updateTaskSession() throws DownloadException {
+        final boolean magnetToTorrent = this.taskSession.getType() == Type.MAGNET;
+        if(magnetToTorrent) {
+            // 磁力链接任务转为BT任务
+            this.taskSession.magnetToTorrent();
+            // 切换下载器并且重新下载
+            this.taskSession.restart();
+        } else {
+            // 刷新任务
+            this.taskSession.refresh();
+        }
+        // 更新任务
+        this.taskSession.update();
+        // 刷新任务状态
+        TaskDisplay.getInstance().refreshTaskStatus();
+    }
+    
 }
