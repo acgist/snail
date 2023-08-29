36e7bc3cdd699802cefbc5d0f9654e204a93627e
acgist <289547414@qq.com>
2023-08-30 07:11:56 +0800

    [*] 日常优化

--- snail-javafx/src/main/java/com/acgist/snail/gui/javafx/window/setting/SettingWindow.java
+++ snail-javafx/src/main/java/com/acgist/snail/gui/javafx/window/setting/SettingWindow.java
@@ -6,29 +6,29 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 /**
- * <p>设置窗口</p>
+ * 设置窗口
  * 
  * @author acgist
  */
 public final class SettingWindow extends Window<SettingController> {
 
-	private static final SettingWindow INSTANCE;
-	
-	public static final SettingWindow getInstance() {
-		return INSTANCE;
-	}
-	
-	static {
-		INSTANCE = new SettingWindow();
-	}
-	
-	private SettingWindow() {
-		super("设置", 600, 400, Modality.APPLICATION_MODAL, "/fxml/setting.fxml");
-	}
-	
-	@Override
-	public void start(Stage stage) throws Exception {
-		this.dialogWindow();
-	}
-	
+    private static final SettingWindow INSTANCE;
+    
+    public static final SettingWindow getInstance() {
+        return INSTANCE;
+    }
+    
+    static {
+        INSTANCE = new SettingWindow();
+    }
+    
+    private SettingWindow() {
+        super("设置", 600, 400, Modality.APPLICATION_MODAL, "/fxml/setting.fxml");
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.dialogWindow();
+    }
+    
 }
