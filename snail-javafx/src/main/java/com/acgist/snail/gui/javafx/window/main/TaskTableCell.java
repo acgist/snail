b8aea5ce8ed14a14c264e6fd7dcae35710a534eb
acgist <289547414@qq.com>
2023-08-31 07:55:22 +0800

    [*] 日常优化

--- snail-javafx/src/main/java/com/acgist/snail/gui/javafx/window/main/TaskTableCell.java
+++ snail-javafx/src/main/java/com/acgist/snail/gui/javafx/window/main/TaskTableCell.java
@@ -10,59 +10,65 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableCell;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
+import javafx.stage.Screen;
 
 /**
- * <p>任务单元格</p>
+ * 任务单元格
  * 
  * @author acgist
  */
 public final class TaskTableCell extends TableCell<ITaskSession, String> {
 
-	/**
-	 * <p>对齐方式</p>
-	 */
-	private final Pos pos;
-	/**
-	 * <p>是否显示Icon</p>
-	 */
-	private final boolean icon;
-	/**
-	 * <p>是否显示Tooltip</p>
-	 */
-	private final boolean tooltip;
-	
-	/**
-	 * @param pos 对齐方式
-	 * @param icon 是否显示Icon
-	 * @param tooltip 是否显示Tooltip
-	 */
-	public TaskTableCell(Pos pos, boolean icon, boolean tooltip) {
-		this.pos = pos;
-		this.icon = icon;
-		this.tooltip = tooltip;
-	}
-	
-	@Override
-	public void updateItem(String value, boolean empty) {
-		super.updateItem(value, empty);
-		final HBox box = new HBox();
-		final ITaskSession taskSession = this.getTableRow().getItem();
-		if(taskSession != null) {
-			final Text name = new Text(value);
-			if(this.pos != null) {
-				box.setAlignment(this.pos);
-			}
-			if(this.icon) {
-				final FileType fileType = taskSession.getFileType();
-				final Label fileLabel = Fonts.fileTypeIconLabel(fileType);
-				box.getChildren().add(fileLabel);
-			}
-			if(this.tooltip) {
-				this.setTooltip(Tooltips.newTooltip(value));
-			}
-			box.getChildren().add(name);
-		}
-		this.setGraphic(box);
-	}
-	
+    /**
+     * 对齐方式
+     */
+    private final Pos pos;
+    /**
+     * 是否显示Icon
+     */
+    private final boolean icon;
+    /**
+     * 是否显示Tooltip
+     */
+    private final boolean tooltip;
+    
+    /**
+     * @param pos     对齐方式
+     * @param icon    是否显示Icon
+     * @param tooltip 是否显示Tooltip
+     */
+    public TaskTableCell(Pos pos, boolean icon, boolean tooltip) {
+        this.pos     = pos;
+        this.icon    = icon;
+        this.tooltip = tooltip;
+    }
+    
+    @Override
+    public void updateItem(String value, boolean empty) {
+        super.updateItem(value, empty);
+        final HBox box = new HBox();
+        final ITaskSession taskSession = this.getTableRow().getItem();
+        if(taskSession != null) {
+            if(this.pos != null) {
+                box.setAlignment(this.pos);
+            }
+            if(this.icon) {
+                final FileType fileType = taskSession.getFileType();
+                final Label fileLabel   = Fonts.fileTypeIconLabel(fileType);
+                final double scaleX = Screen.getPrimary().getOutputScaleX();
+                final double scaleY = Screen.getPrimary().getOutputScaleY();
+                if(scaleX != 1.0D && scaleY != 1.0D) {
+                    fileLabel.setStyle("-fx-font-size:14px;");
+                }
+                box.getChildren().add(fileLabel);
+            }
+            if(this.tooltip) {
+                this.setTooltip(Tooltips.newTooltip(value));
+            }
+            final Text name = new Text(value);
+            box.getChildren().add(name);
+        }
+        this.setGraphic(box);
+    }
+    
 }
--- snail-javafx/src/main/java/com/acgist/snail/gui/javafx/window/setting/SettingController.java
+++ snail-javafx/src/main/java/com/acgist/snail/gui/javafx/window/setting/SettingController.java
@@ -25,215 +25,217 @@ import javafx.scene.layout.FlowPane;
 import javafx.util.StringConverter;
 
 /**
- * <p>设置窗口控制器</p>
+ * 设置窗口控制器
  * 
  * @author acgist
  */
 public final class SettingController extends Controller {
-	
-	/**
-	 * <p>下载速度滑块滑动大小：{@value}</p>
-	 */
-	private static final int MIN_STEP_SIZE = 512;
-	
-	@FXML
-	private FlowPane root;
-	@FXML
-	private ScrollPane scrollPane;
-	@FXML
-	private Label pathValue;
-	@FXML
-	private Slider size;
-	@FXML
-	private Slider buffer;
-	@FXML
-	private Slider memoryBuffer;
-	@FXML
-	private CheckBox notice;
-	@FXML
-	private CheckBox delete;
-	
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		// 绑定宽高
-		this.scrollPane.prefWidthProperty().bind(this.root.widthProperty());
-		this.scrollPane.prefHeightProperty().bind(this.root.heightProperty());
-		// 初始化
-		this.initSetting();
-		this.initControl();
-	}
+    
+    /**
+     * 下载速度滑块滑动大小：{@value}
+     */
+    private static final int MIN_STEP_SIZE = 512;
+    
+    @FXML
+    private FlowPane root;
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private Label pathValue;
+    @FXML
+    private Slider size;
+    @FXML
+    private Slider buffer;
+    @FXML
+    private Slider memoryBuffer;
+    @FXML
+    private CheckBox notice;
+    @FXML
+    private CheckBox delete;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // 绑定宽高
+        this.scrollPane.prefWidthProperty().bind(this.root.widthProperty());
+        this.scrollPane.prefHeightProperty().bind(this.root.heightProperty());
+        // 初始化
+        this.initSetting();
+        this.initControl();
+    }
 
-	/**
-	 * <p>下载目录</p>
-	 * 
-	 * @param event 事件
-	 */
-	@FXML
-	public void handlePathAction(ActionEvent event) {
-		final File file = Choosers.chooseDirectory(SettingWindow.getInstance().getStage(), "下载目录");
-		if (file != null) {
-			final String path = file.getAbsolutePath();
-			DownloadConfig.setPath(path);
-			this.pathValue(DownloadConfig.getPath());
-		}
-	}
+    /**
+     * 下载目录
+     * 
+     * @param event 事件
+     */
+    @FXML
+    public void handlePathAction(ActionEvent event) {
+        final File file = Choosers.chooseDirectory(SettingWindow.getInstance().getStage(), "下载目录");
+        if (file != null) {
+            final String path = file.getAbsolutePath();
+            DownloadConfig.setPath(path);
+            this.pathValue(DownloadConfig.getPath());
+        }
+    }
 
-	/**
-	 * <p>消息提示</p>
-	 * 
-	 * @param event 事件
-	 */
-	@FXML
-	public void handleNoticeAction(ActionEvent event) {
-		DownloadConfig.setNotice(this.notice.isSelected());
-	}
-	
-	/**
-	 * <p>删除文件</p>
-	 * 
-	 * @param event 事件
-	 */
-	@FXML
-	public void handleDeleteAction(ActionEvent event) {
-		DownloadConfig.setDelete(this.delete.isSelected());
-	}
-	
-	/**
-	 * <p>初始化配置</p>
-	 */
-	private void initSetting() {
-		this.pathValue(DownloadConfig.getPath());
-		this.size.setValue(DownloadConfig.getSize());
-		this.buffer.setValue(DownloadConfig.getBuffer());
-		this.memoryBuffer.setValue(DownloadConfig.getMemoryBuffer());
-		this.notice.setSelected(DownloadConfig.getNotice());
-		this.delete.setSelected(DownloadConfig.getDelete());
-	}
-	
-	/**
-	 * <p>初始化控件</p>
-	 */
-	private void initControl() {
-		// 初始化下载目录
-		this.pathValue.setCursor(Cursor.HAND);
-		this.pathValue.setOnMouseReleased(this.pathReleaseAction);
-		// 初始化任务数量
-		this.size.valueProperty().addListener(this.sizeListener);
-		this.size.setOnMouseReleased(this.sizeReleaseAction);
-		// 初始化下载速度
-		this.buffer.valueProperty().addListener(this.bufferListener);
-		this.buffer.setOnMouseReleased(this.bufferReleaseAction);
-		this.buffer.setLabelFormatter(this.bufferFormatter);
-		// 初始化磁盘缓存
-		this.memoryBuffer.valueProperty().addListener(this.memoryBufferListener);
-		this.memoryBuffer.setOnMouseReleased(this.memoryBufferReleaseAction);
-		this.memoryBuffer.setLabelFormatter(this.memoryBufferFormatter);
-	}
-	
-	/**
-	 * <p>下载目录点击事件</p>
-	 */
-	private EventHandler<MouseEvent> pathReleaseAction = event -> Desktops.open(new File(DownloadConfig.getPath()));
+    /**
+     * 消息提示
+     * 
+     * @param event 事件
+     */
+    @FXML
+    public void handleNoticeAction(ActionEvent event) {
+        DownloadConfig.setNotice(this.notice.isSelected());
+    }
+    
+    /**
+     * 删除文件
+     * 
+     * @param event 事件
+     */
+    @FXML
+    public void handleDeleteAction(ActionEvent event) {
+        DownloadConfig.setDelete(this.delete.isSelected());
+    }
+    
+    /**
+     * 初始化配置
+     */
+    private void initSetting() {
+        this.pathValue(DownloadConfig.getPath());
+        this.size.setValue(DownloadConfig.getSize());
+        this.buffer.setValue(DownloadConfig.getBuffer());
+        this.memoryBuffer.setValue(DownloadConfig.getMemoryBuffer());
+        this.notice.setSelected(DownloadConfig.getNotice());
+        this.delete.setSelected(DownloadConfig.getDelete());
+    }
+    
+    /**
+     * 初始化控件
+     */
+    private void initControl() {
+        // 初始化下载目录
+        this.pathValue.setCursor(Cursor.HAND);
+        this.pathValue.setOnMouseReleased(this.pathReleaseAction);
+        // 初始化任务数量
+        this.size.valueProperty().addListener(this.sizeListener);
+        this.size.setOnMouseReleased(this.sizeReleaseAction);
+        // 初始化下载速度
+        this.buffer.valueProperty().addListener(this.bufferListener);
+        this.buffer.setOnMouseReleased(this.bufferReleaseAction);
+        this.buffer.setLabelFormatter(this.bufferFormatter);
+        // 初始化磁盘缓存
+        this.memoryBuffer.valueProperty().addListener(this.memoryBufferListener);
+        this.memoryBuffer.setOnMouseReleased(this.memoryBufferReleaseAction);
+        this.memoryBuffer.setLabelFormatter(this.memoryBufferFormatter);
+    }
+    
+    /**
+     * 下载目录点击事件
+     */
+    private EventHandler<MouseEvent> pathReleaseAction = event -> Desktops.open(new File(DownloadConfig.getPath()));
 
-	/**
-	 * <p>下载任务数量监听</p>
-	 */
-	private ChangeListener<? super Number> sizeListener = (obs, oldVal, newVal) -> {
-		// 设置整数任务
-		final int value = newVal.intValue();
-		// 至少下载一个任务
-		this.size.setValue(value <= 0 ? 1 : value);
-	};
-	
-	/**
-	 * <p>下载任务数量保存</p>
-	 */
-	private EventHandler<MouseEvent> sizeReleaseAction = event -> {
-		final Double value = this.size.getValue();
-		DownloadConfig.setSize(value.intValue());
-	};
-	
-	/**
-	 * <p>下载速度监听</p>
-	 */
-	private ChangeListener<? super Number> bufferListener = (obs, oldVal, newVal) -> {
-		int value = newVal.intValue();
-		if(value < SystemConfig.MIN_DOWNLOAD_BUFFER_KB) {
-			// 最小下载速度
-			value = SystemConfig.MIN_DOWNLOAD_BUFFER_KB;
-		} else if(value > MIN_STEP_SIZE) {
-			// 超过滑块大小时设置为滑块大小的整数倍
-			value = value / MIN_STEP_SIZE * MIN_STEP_SIZE;
-		}
-		this.buffer.setValue(value);
-	};
-	
-	/**
-	 * <p>下载速度保存</p>
-	 */
-	private EventHandler<MouseEvent> bufferReleaseAction = event -> {
-		final Double value = this.buffer.getValue();
-		DownloadConfig.setBuffer(value.intValue());
-	};
-	
-	/**
-	 * <p>下载速度格式</p>
-	 */
-	private StringConverter<Double> bufferFormatter = new StringConverter<Double>() {
-		
-		@Override
-		public String toString(Double value) {
-			return (value.intValue() / SystemConfig.DATA_SCALE) + "M";
-		}
-		
-		@Override
-		public Double fromString(String label) {
-			return Double.valueOf(label.substring(0, label.length() - 1)) * SystemConfig.DATA_SCALE;
-		}
-		
-	};
-	
-	/**
-	 * <p>磁盘缓存监听</p>
-	 */
-	private ChangeListener<? super Number> memoryBufferListener = (obs, oldVal, newVal) -> {
-		final int value = newVal.intValue();
-		this.memoryBuffer.setValue(value);
-	};
-	
-	/**
-	 * <p>磁盘缓存保存</p>
-	 */
-	private EventHandler<MouseEvent> memoryBufferReleaseAction = event -> {
-		final Double value = this.memoryBuffer.getValue();
-		DownloadConfig.setMemoryBuffer(value.intValue());
-	};
-	
-	/**
-	 * <p>磁盘缓存格式</p>
-	 */
-	private StringConverter<Double> memoryBufferFormatter = new StringConverter<Double>() {
-		
-		@Override
-		public String toString(Double value) {
-			return value.intValue() + "M";
-		}
-		
-		@Override
-		public Double fromString(String label) {
-			return Double.valueOf(label.substring(0, label.length() - 1));
-		}
-		
-	};
-	
-	/**
-	 * <p>设置下载路径</p>
-	 * 
-	 * @param path 下载路径
-	 */
-	private void pathValue(String path) {
-		this.pathValue.setText(path);
-		this.pathValue.setTooltip(Tooltips.newTooltip(path));
-	}
-	
+    /**
+     * 下载任务数量监听
+     */
+    private ChangeListener<? super Number> sizeListener = (obs, oldVal, newVal) -> {
+        // 设置整数任务
+        final int value = newVal.intValue();
+        // 至少下载一个任务
+        this.size.setValue(value <= 0 ? 1 : value);
+    };
+    
+    /**
+     * 下载任务数量保存
+     */
+    private EventHandler<MouseEvent> sizeReleaseAction = event -> {
+        final Double value = this.size.getValue();
+        DownloadConfig.setSize(value.intValue());
+    };
+    
+    /**
+     * 下载速度监听
+     */
+    private ChangeListener<? super Number> bufferListener = (obs, oldVal, newVal) -> {
+        int value = newVal.intValue();
+        if(value < SystemConfig.MIN_DOWNLOAD_BUFFER_KB) {
+            // 最小下载速度
+            value = SystemConfig.MIN_DOWNLOAD_BUFFER_KB;
+        } else if(value > MIN_STEP_SIZE) {
+            // 超过滑块大小时设置为滑块大小的整数倍
+            value = value / MIN_STEP_SIZE * MIN_STEP_SIZE;
+        } else {
+            // 其他情况
+        }
+        this.buffer.setValue(value);
+    };
+    
+    /**
+     * 下载速度保存
+     */
+    private EventHandler<MouseEvent> bufferReleaseAction = event -> {
+        final Double value = this.buffer.getValue();
+        DownloadConfig.setBuffer(value.intValue());
+    };
+    
+    /**
+     * 下载速度格式
+     */
+    private StringConverter<Double> bufferFormatter = new StringConverter<Double>() {
+        
+        @Override
+        public String toString(Double value) {
+            return (value.intValue() / SystemConfig.DATA_SCALE) + "M";
+        }
+        
+        @Override
+        public Double fromString(String label) {
+            return Double.valueOf(label.substring(0, label.length() - 1)) * SystemConfig.DATA_SCALE;
+        }
+        
+    };
+    
+    /**
+     * 磁盘缓存监听
+     */
+    private ChangeListener<? super Number> memoryBufferListener = (obs, oldVal, newVal) -> {
+        final int value = newVal.intValue();
+        this.memoryBuffer.setValue(value);
+    };
+    
+    /**
+     * 磁盘缓存保存
+     */
+    private EventHandler<MouseEvent> memoryBufferReleaseAction = event -> {
+        final Double value = this.memoryBuffer.getValue();
+        DownloadConfig.setMemoryBuffer(value.intValue());
+    };
+    
+    /**
+     * 磁盘缓存格式
+     */
+    private StringConverter<Double> memoryBufferFormatter = new StringConverter<Double>() {
+        
+        @Override
+        public String toString(Double value) {
+            return value.intValue() + "M";
+        }
+        
+        @Override
+        public Double fromString(String label) {
+            return Double.valueOf(label.substring(0, label.length() - 1));
+        }
+        
+    };
+    
+    /**
+     * 设置下载路径
+     * 
+     * @param path 下载路径
+     */
+    private void pathValue(String path) {
+        this.pathValue.setText(path);
+        this.pathValue.setTooltip(Tooltips.newTooltip(path));
+    }
+    
 }
