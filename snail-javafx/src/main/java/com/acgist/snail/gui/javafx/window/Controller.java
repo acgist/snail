1db99cb63b3d222c15bb9b6d4faea3a27b8086cd
acgist <289547414@qq.com>
2023-09-02 09:05:58 +0800

    [*] 日常优化

--- snail-javafx/src/main/java/com/acgist/snail/gui/javafx/window/Controller.java
+++ snail-javafx/src/main/java/com/acgist/snail/gui/javafx/window/Controller.java
@@ -1,6 +1,7 @@
 package com.acgist.snail.gui.javafx.window;
 
 import java.io.File;
+import java.util.List;
 
 import com.acgist.snail.logger.Logger;
 import com.acgist.snail.logger.LoggerFactory;
@@ -11,55 +12,55 @@ import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
 
 /**
- * <p>窗口控制器</p>
+ * 窗口控制器
  * 
  * @author acgist
  */
 public abstract class Controller implements Initializable {
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(Controller.class);
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(Controller.class);
 
-	/**
-	 * <p>双击次数：{@value}</p>
-	 */
-	protected static final int DOUBLE_CLICK_COUNT = 2;
-	
-	/**
-	 * <p>获取拖入面板数据</p>
-	 * <p>支持数据：链接、文本、文件（路径）</p>
-	 * 
-	 * @param event 拖拽事件
-	 * 
-	 * @return 链接、文本、文件（路径）
-	 */
-	protected final String dragboard(DragEvent event) {
-		final Dragboard dragboard = event.getDragboard();
-		if (dragboard.hasFiles()) {
-			// 优先判断文件
-			final var files = dragboard.getFiles();
-			if (CollectionUtils.isNotEmpty(files)) {
-				for (File file : files) {
-					if(file.isFile()) {
-						return file.getAbsolutePath();
-					}
-				}
-			}
-		} else if (dragboard.hasUrl()) {
-			return dragboard.getUrl();
-		} else if (dragboard.hasString()) {
-			return dragboard.getString();
-		}
-		if(LOGGER.isInfoEnabled()) {
-			LOGGER.info("不支持的拖拽对象：{}", dragboard.getContentTypes());
-		}
-		return null;
-	}
+    /**
+     * 双击次数：{@value}
+     */
+    protected static final int DOUBLE_CLICK_COUNT = 2;
+    
+    /**
+     * 拖入面板数据
+     * 支持数据：链接、文本、文件（路径）
+     * 
+     * @param event 拖拽事件
+     * 
+     * @return 链接、文本、文件（路径）
+     */
+    protected final String dragboard(DragEvent event) {
+        final Dragboard dragboard = event.getDragboard();
+        if (dragboard.hasFiles()) {
+            // 优先判断文件
+            final List<File> files = dragboard.getFiles();
+            if (CollectionUtils.isNotEmpty(files)) {
+                for (File file : files) {
+                    if(file.isFile()) {
+                        return file.getAbsolutePath();
+                    }
+                }
+            }
+        } else if (dragboard.hasUrl()) {
+            return dragboard.getUrl();
+        } else if (dragboard.hasString()) {
+            return dragboard.getString();
+        }
+        if(LOGGER.isInfoEnabled()) {
+            LOGGER.info("不支持的拖拽对象：{}", dragboard.getContentTypes());
+        }
+        return null;
+    }
 
-	/**
-	 * <p>释放资源</p>
-	 */
-	public void release() {
-		LOGGER.debug("释放控制器资源");
-	}
-	
+    /**
+     * 释放控制器资源
+     */
+    public void release() {
+        LOGGER.debug("释放控制器资源");
+    }
+    
 }
