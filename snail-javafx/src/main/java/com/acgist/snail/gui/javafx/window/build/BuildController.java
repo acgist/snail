0b9b449e4d097e18ba813afb5e38e9b5184bed70
acgist <289547414@qq.com>
2023-09-03 08:04:06 +0800

    [*] 日常优化

--- snail-javafx/src/main/java/com/acgist/snail/gui/javafx/window/build/BuildController.java
+++ snail-javafx/src/main/java/com/acgist/snail/gui/javafx/window/build/BuildController.java
@@ -26,145 +26,146 @@ import javafx.scene.input.TransferMode;
 import javafx.scene.layout.FlowPane;
 
 /**
- * <p>新建窗口控制器</p>
+ * 新建窗口控制器
  * 
  * @author acgist
  */
 public final class BuildController extends Controller {
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(BuildController.class);
-	
-	@FXML
-	private FlowPane root;
-	@FXML
-	private TextField urlValue;
-	
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		this.root.setOnDragOver(this.dragOverAction);
-		this.root.setOnDragDropped(this.dragDroppedAction);
-		this.urlValue.setPromptText("输入下载链接、磁力链接或选择种子文件");
-	}
-	
-	/**
-	 * <p>选择种子按钮</p>
-	 * 
-	 * @param event 事件
-	 */
-	@FXML
-	public void handleTorrentAction(ActionEvent event) {
-		final List<File> files = Choosers.chooseMultipleFile(BuildWindow.getInstance().getStage(), "选择种子", "种子文件", "*.torrent");
-		if (CollectionUtils.isNotEmpty(files)) {
-			this.setUrl(files.stream().map(File::getAbsolutePath).collect(Collectors.joining(SymbolConfig.Symbol.SEMICOLON.toString())));
-		}
-	}
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(BuildController.class);
+    
+    @FXML
+    private FlowPane root;
+    @FXML
+    private TextField urlValue;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.root.setOnDragOver(this.dragOverAction);
+        this.root.setOnDragDropped(this.dragDroppedAction);
+        this.urlValue.setPromptText("输入下载链接、磁力链接或选择种子文件");
+    }
+    
+    /**
+     * 选择种子按钮
+     * 
+     * @param event 事件
+     */
+    @FXML
+    public void handleTorrentAction(ActionEvent event) {
+        final List<File> files = Choosers.chooseMultipleFile(BuildWindow.getInstance().getStage(), "选择种子", "种子文件", "*.torrent");
+        if (CollectionUtils.isNotEmpty(files)) {
+            this.setUrl(files.stream().map(File::getAbsolutePath).collect(Collectors.joining(SymbolConfig.Symbol.SEMICOLON.toString())));
+        }
+    }
 
-	/**
-	 * <p>确认下载按钮</p>
-	 * 
-	 * @param event 事件
-	 */
-	@FXML
-	public void handleBuildAction(ActionEvent event) {
-		final String url = this.urlValue.getText();
-		if(StringUtils.isEmpty(url)) {
-			Alerts.warn("下载失败", "输入下载链接");
-			return;
-		}
-		// 使用分号分隔：如果本地文件目录存在空格不能使用
-		final String[] urls = url.split(SymbolConfig.Symbol.SEMICOLON.toString());
-		boolean success = false;
-		for (String value : urls) {
-			if(StringUtils.isEmpty(value)) {
-				continue;
-			}
-			success = this.download(value) || success;
-		}
-		if(success) {
-			BuildWindow.getInstance().hide();
-		}
-	}
-	
-	/**
-	 * <p>下载链接</p>
-	 * 
-	 * @param url 链接地址
-	 * 
-	 * @return 是否成功
-	 */
-	private boolean download(String url) {
-		boolean success = true;
-		try {
-			TaskContext.getInstance().download(url);
-		} catch (Exception e) {
-			LOGGER.error("新建下载任务异常：{}", url, e);
-			Alerts.warn("下载失败", e.getMessage());
-			success = false;
-		}
-		return success;
-	}
-	
-	/**
-	 * <p>取消下载按钮</p>
-	 * 
-	 * @param event 事件
-	 */
-	@FXML
-	public void handleCancelAction(ActionEvent event) {
-		BuildWindow.getInstance().hide();
-	}
-	
-	/**
-	 * <p>设置下载链接</p>
-	 * 
-	 * @param url 下载链接
-	 */
-	public void setUrl(String url) {
-		if(url == null) {
-			url = "";
-		}
-		this.urlValue.setText(url.strip());
-	}
-	
-	/**
-	 * <p>清空下载链接</p>
-	 */
-	public void cleanUrl() {
-		this.setUrl(null);
-	}
-	
-	/**
-	 * <p>设置焦点</p>
-	 */
-	public void setFocus() {
-		this.urlValue.requestFocus();
-	}
-	
-	/**
-	 * <p>拖入文件事件（显示）</p>
-	 */
-	private EventHandler<DragEvent> dragOverAction = event -> {
-		if (event.getGestureSource() != this.root) {
-			final String url = this.dragboard(event);
-			if(ProtocolContext.getInstance().support(url)) {
-				event.acceptTransferModes(TransferMode.COPY);
-			} else {
-				event.acceptTransferModes(TransferMode.NONE);
-			}
-		}
-		event.consume();
-	};
-	
-	/**
-	 * <p>拖入文件事件（加载）</p>
-	 */
-	private EventHandler<DragEvent> dragDroppedAction = event -> {
-		final String url = this.dragboard(event);
-		if(StringUtils.isNotEmpty(url)) {
-			this.setUrl(url);
-		}
-		event.setDropCompleted(true);
-		event.consume();
-	};
-	
+    /**
+     * 确认下载按钮
+     * 
+     * @param event 事件
+     */
+    @FXML
+    public void handleBuildAction(ActionEvent event) {
+        final String url = this.urlValue.getText();
+        if(StringUtils.isEmpty(url)) {
+            Alerts.warn("下载失败", "输入下载链接");
+            return;
+        }
+        // 使用分号分隔：如果本地文件目录存在空格不能使用
+        final String[] urls = url.split(SymbolConfig.Symbol.SEMICOLON.toString());
+        boolean success = false;
+        for (String value : urls) {
+            value = value.strip();
+            if(StringUtils.isEmpty(value)) {
+                continue;
+            }
+            success = this.download(value) || success;
+        }
+        if(success) {
+            BuildWindow.getInstance().hide();
+        }
+    }
+    
+    /**
+     * 下载链接
+     * 
+     * @param url 链接地址
+     * 
+     * @return 是否成功
+     */
+    private boolean download(String url) {
+        boolean success = true;
+        try {
+            TaskContext.getInstance().download(url);
+        } catch (Exception e) {
+            LOGGER.error("新建下载任务异常：{}", url, e);
+            Alerts.warn("下载失败", e.getMessage());
+            success = false;
+        }
+        return success;
+    }
+    
+    /**
+     * 取消下载按钮
+     * 
+     * @param event 事件
+     */
+    @FXML
+    public void handleCancelAction(ActionEvent event) {
+        BuildWindow.getInstance().hide();
+    }
+    
+    /**
+     * 设置下载链接
+     * 
+     * @param url 下载链接
+     */
+    public void setUrl(String url) {
+        if(url == null) {
+            url = "";
+        }
+        this.urlValue.setText(url.strip());
+    }
+    
+    /**
+     * 清空下载链接
+     */
+    public void cleanUrl() {
+        this.setUrl(null);
+    }
+    
+    /**
+     * 设置焦点
+     */
+    public void setFocus() {
+        this.urlValue.requestFocus();
+    }
+    
+    /**
+     * 拖入文件事件（显示）
+     */
+    private EventHandler<DragEvent> dragOverAction = event -> {
+        if (event.getGestureSource() != this.root) {
+            final String url = this.dragboard(event);
+            if(ProtocolContext.getInstance().support(url)) {
+                event.acceptTransferModes(TransferMode.COPY);
+            } else {
+                event.acceptTransferModes(TransferMode.NONE);
+            }
+        }
+        event.consume();
+    };
+    
+    /**
+     * 拖入文件事件（加载）
+     */
+    private EventHandler<DragEvent> dragDroppedAction = event -> {
+        final String url = this.dragboard(event);
+        if(StringUtils.isNotEmpty(url)) {
+            this.setUrl(url);
+        }
+        event.setDropCompleted(true);
+        event.consume();
+    };
+    
 }
--- snail-javafx/src/main/java/com/acgist/snail/gui/javafx/window/build/BuildWindow.java
+++ snail-javafx/src/main/java/com/acgist/snail/gui/javafx/window/build/BuildWindow.java
@@ -6,51 +6,51 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 /**
- * <p>新建窗口</p>
+ * 新建窗口
  * 
  * @author acgist
  */
 public final class BuildWindow extends Window<BuildController> {
 
-	private static final BuildWindow INSTANCE;
-	
-	public static final BuildWindow getInstance() {
-		return INSTANCE;
-	}
-	
-	static {
-		INSTANCE = new BuildWindow();
-	}
-	
-	private BuildWindow() {
-		super("新建下载", 600, 300, Modality.APPLICATION_MODAL, "/fxml/build.fxml");
-	}
-	
-	@Override
-	public void start(Stage stage) throws Exception {
-		this.dialogWindow();
-	}
-	
-	/**
-	 * <p>显示新建窗口并设置下载链接</p>
-	 * 
-	 * @param url 下载链接
-	 */
-	public void show(String url) {
-		this.controller.setUrl(url);
-		super.show();
-	}
-	
-	@Override
-	public void show() {
-		super.show();
-		this.controller.setFocus();
-	}
-	
-	@Override
-	public void hide() {
-		super.hide();
-		this.controller.cleanUrl();
-	}
-	
+    private static final BuildWindow INSTANCE;
+    
+    public static final BuildWindow getInstance() {
+        return INSTANCE;
+    }
+    
+    static {
+        INSTANCE = new BuildWindow();
+    }
+    
+    private BuildWindow() {
+        super("新建下载", 600, 300, Modality.APPLICATION_MODAL, "/fxml/build.fxml");
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.dialogWindow();
+    }
+    
+    /**
+     * 显示新建窗口并设置下载链接
+     * 
+     * @param url 下载链接
+     */
+    public void show(String url) {
+        this.controller.setUrl(url);
+        super.show();
+    }
+    
+    @Override
+    public void show() {
+        super.show();
+        this.controller.setFocus();
+    }
+    
+    @Override
+    public void hide() {
+        super.hide();
+        this.controller.cleanUrl();
+    }
+    
 }
