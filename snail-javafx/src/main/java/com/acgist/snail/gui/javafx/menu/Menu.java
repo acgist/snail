4267f76f3d79a2b16f998375a2a4b7d4c0d08ed3
acgist <289547414@qq.com>
2023-09-09 10:02:49 +0800

    [*]日常优化

--- snail-javafx/src/main/java/com/acgist/snail/gui/javafx/menu/Menu.java
+++ snail-javafx/src/main/java/com/acgist/snail/gui/javafx/menu/Menu.java
@@ -9,43 +9,43 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.SeparatorMenuItem;
 
 /**
- * <p>菜单</p>
+ * 菜单
  * 
  * @author acgist
  */
 public abstract class Menu extends ContextMenu {
 
-	/**
-	 * <p>添加分隔</p>
-	 */
-	protected void buildSeparator() {
-		this.getItems().add(new SeparatorMenuItem());
-	}
-	
-	/**
-	 * <p>新建菜单</p>
-	 * 
-	 * @param value 名称
-	 * @param icon 图标
-	 * @param action 事件
-	 * 
-	 * @return 菜单
-	 */
-	protected MenuItem buildMenuItem(String value, SnailIcon icon, EventHandler<ActionEvent> action) {
-		final MenuItem menuItem;
-		if(icon == null) {
-			menuItem = new MenuItem(value);
-		} else {
-			menuItem = new MenuItem(value, icon.iconLabel());
-		}
-		this.getItems().add(menuItem);
-		menuItem.setOnAction(action);
-		return menuItem;
-	}
-	
-	/**
-	 * <p>新建所有菜单</p>
-	 */
-	protected abstract void buildMenus();
-	
+    /**
+     * 添加分隔
+     */
+    protected void buildSeparator() {
+        this.getItems().add(new SeparatorMenuItem());
+    }
+    
+    /**
+     * 新建菜单
+     * 
+     * @param value  名称
+     * @param icon   图标
+     * @param action 事件
+     * 
+     * @return 菜单
+     */
+    protected MenuItem buildMenuItem(String value, SnailIcon icon, EventHandler<ActionEvent> action) {
+        final MenuItem menuItem;
+        if(icon == null) {
+            menuItem = new MenuItem(value);
+        } else {
+            menuItem = new MenuItem(value, icon.iconLabel());
+        }
+        this.getItems().add(menuItem);
+        menuItem.setOnAction(action);
+        return menuItem;
+    }
+    
+    /**
+     * 新建所有菜单
+     */
+    protected abstract void buildMenus();
+    
 }
