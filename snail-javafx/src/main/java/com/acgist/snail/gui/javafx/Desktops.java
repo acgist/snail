0d6ef11b7b3293700fb54836b11dba7d8d6aae2e
acgist <289547414@qq.com>
2023-09-13 08:10:53 +0800

    [*] 日常优化

--- snail-javafx/src/main/java/com/acgist/snail/gui/javafx/Desktops.java
+++ snail-javafx/src/main/java/com/acgist/snail/gui/javafx/Desktops.java
@@ -13,75 +13,73 @@ import com.acgist.snail.logger.LoggerFactory;
 import com.acgist.snail.utils.StringUtils;
 
 /**
- * <p>桌面助手</p>
+ * 桌面助手
  * 
  * @author acgist
  */
 public final class Desktops {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(Desktops.class);
-	
-	private Desktops() {
-	}
-	
-	/**
-	 * <p>打开文件</p>
-	 * <p>使用资源管理器打开文件</p>
-	 * 
-	 * @param file 文件
-	 */
-	public static final void open(final File file) {
-		if(
-			file != null &&
-			file.exists() &&
-			Desktops.support(Action.OPEN)
-		) {
-			SwingUtilities.invokeLater(() -> {
-				try {
-					Desktop.getDesktop().open(file);
-				} catch (IOException e) {
-					LOGGER.error("打开文件异常：{}", file, e);
-				}
-			});
-		} else {
-			LOGGER.warn("打开文件失败：{}", file);
-		}
-	}
-	
-	/**
-	 * <p>打开网页链接</p>
-	 * <p>使用默认浏览器打开网页链接</p>
-	 * 
-	 * @param url 网页链接
-	 */
-	public static final void browse(final String url) {
-		if(
-			StringUtils.isNotEmpty(url) &&
-			Desktops.support(Action.BROWSE)
-		) {
-			SwingUtilities.invokeLater(() -> {
-				try {
-					Desktop.getDesktop().browse(URI.create(url));
-				} catch (IOException e) {
-					LOGGER.error("打开网页链接异常：{}", url, e);
-				}
-			});
-		} else {
-			LOGGER.warn("打开网页链接失败：{}", url);
-		}
-	}
-	
-	/**
-	 * <p>判断是否支持动作</p>
-	 * 
-	 * @param action 动作
-	 * 
-	 * @return 是否支持
-	 */
-	public static final boolean support(final Action action) {
-		return
-			Desktop.isDesktopSupported() &&
-			Desktop.getDesktop().isSupported(action);
-	}
-	
+    private static final Logger LOGGER = LoggerFactory.getLogger(Desktops.class);
+    
+    private Desktops() {
+    }
+    
+    /**
+     * 打开文件
+     * 使用资源管理器打开文件
+     * 
+     * @param file 文件
+     */
+    public static final void open(final File file) {
+        if(
+            file != null  &&
+            file.exists() &&
+            Desktops.support(Action.OPEN)
+        ) {
+            SwingUtilities.invokeLater(() -> {
+                try {
+                    Desktop.getDesktop().open(file);
+                } catch (IOException e) {
+                    LOGGER.error("打开文件异常：{}", file, e);
+                }
+            });
+        } else {
+            LOGGER.warn("打开文件失败：{}", file);
+        }
+    }
+    
+    /**
+     * 打开网页链接
+     * 使用默认浏览器打开网页链接
+     * 
+     * @param url 网页链接
+     */
+    public static final void browse(final String url) {
+        if(
+            StringUtils.isNotEmpty(url) &&
+            Desktops.support(Action.BROWSE)
+        ) {
+            SwingUtilities.invokeLater(() -> {
+                try {
+                    Desktop.getDesktop().browse(URI.create(url));
+                } catch (IOException e) {
+                    LOGGER.error("打开网页链接异常：{}", url, e);
+                }
+            });
+        } else {
+            LOGGER.warn("打开网页链接失败：{}", url);
+        }
+    }
+    
+    /**
+     * 判断是否支持动作
+     * 
+     * @param action 动作
+     * 
+     * @return 是否支持
+     */
+    public static final boolean support(final Action action) {
+        return Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(action);
+    }
+    
 }
