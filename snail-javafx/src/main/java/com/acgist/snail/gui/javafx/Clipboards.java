4e907d1169c5e20acb2a7c9f688b5ec790db1031
acgist <289547414@qq.com>
2023-09-14 08:13:03 +0800

    [*] 日常优化

--- snail-javafx/src/main/java/com/acgist/snail/gui/javafx/Clipboards.java
+++ snail-javafx/src/main/java/com/acgist/snail/gui/javafx/Clipboards.java
@@ -4,34 +4,32 @@ import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 
 /**
- * <p>剪切板助手</p>
+ * 剪切板助手
  * 
  * @author acgist
  */
 public final class Clipboards {
 
-	private Clipboards() {
-	}
-	
-	/**
-	 * <p>拷贝文本内容到剪切板</p>
-	 * 
-	 * @param value 文本内容
-	 */
-	public static final void copy(final String value) {
-		final Clipboard clipboard = Clipboard.getSystemClipboard();
-		final ClipboardContent content = new ClipboardContent();
-		content.putString(value);
-		clipboard.setContent(content);
-	}
-	
-	/**
-	 * <p>获取剪切板文本内容</p>
-	 * 
-	 * @return 文本内容
-	 */
-	public static final String get() {
-		return Clipboard.getSystemClipboard().getString();
-	}
-	
+    private Clipboards() {
+    }
+    
+    /**
+     * 拷贝文本内容到剪切板
+     * 
+     * @param value 文本内容
+     */
+    public static final void copy(final String value) {
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        final ClipboardContent content = new ClipboardContent();
+        content.putString(value);
+        clipboard.setContent(content);
+    }
+    
+    /**
+     * @return 剪切板文本内容
+     */
+    public static final String get() {
+        return Clipboard.getSystemClipboard().getString();
+    }
+    
 }
