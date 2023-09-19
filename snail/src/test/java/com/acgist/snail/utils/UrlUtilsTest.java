b5e3eef5b9660cbaa15881f8e66723e40dd048f3
acgist <289547414@qq.com>
2023-09-20 07:33:10 +0800

    [*] 日常优化

--- snail/src/test/java/com/acgist/snail/utils/UrlUtilsTest.java
+++ snail/src/test/java/com/acgist/snail/utils/UrlUtilsTest.java
@@ -10,36 +10,36 @@ import org.junit.jupiter.api.Test;
 
 class UrlUtilsTest extends Performance {
 
-	@Test
-	void testRedirect() {
-		String path = "https://www.acgist.com/index.html";
-		String pathA = "https://www.acgist.com/a/index.html";
-		String pathAB = "https://www.acgist.com/a/b/index.html";
-		String baidu = "https://www.baidu.com";
-		assertEquals(UrlUtils.redirect("https://www.acgist.com", "index.html"), path);
-		assertEquals(UrlUtils.redirect("https://www.acgist.com/", "index.html"), path);
-		assertEquals(UrlUtils.redirect("https://www.acgist.com", "/index.html"), path);
-		assertEquals(UrlUtils.redirect("https://www.acgist.com/", "/index.html"), path);
-		assertEquals(UrlUtils.redirect("https://www.acgist.com/a", "index.html"), path);
-		assertEquals(UrlUtils.redirect("https://www.acgist.com/a/", "index.html"), pathA);
-		assertEquals(UrlUtils.redirect("https://www.acgist.com/a", "/index.html"), path);
-		assertEquals(UrlUtils.redirect("https://www.acgist.com/a/", "/index.html"), path);
-		assertEquals(UrlUtils.redirect("https://www.acgist.com/a/b", "index.html"), pathA);
-		assertEquals(UrlUtils.redirect("https://www.acgist.com/a/b/", "index.html"), pathAB);
-		assertEquals(UrlUtils.redirect("https://www.acgist.com/a/b", "/index.html"), path);
-		assertEquals(UrlUtils.redirect("https://www.acgist.com/a/b/", "/index.html"), path);
-		assertEquals(UrlUtils.redirect("https://www.acgist.com/a/b/", " /index.html "), path);
-		assertEquals(UrlUtils.redirect("https://www.acgist.com/a/b/", "\"/index.html\""), path);
-		assertEquals(UrlUtils.redirect("https://www.acgist.com/a/b/", " \"/index.html\" "), path);
-		assertEquals(UrlUtils.redirect(path, baidu), baidu);
-	}
+    @Test
+    void testRedirect() {
+        String path   = "https://www.acgist.com/index.html";
+        String pathA  = "https://www.acgist.com/a/index.html";
+        String pathAB = "https://www.acgist.com/a/b/index.html";
+        String baidu  = "https://www.baidu.com";
+        assertEquals(UrlUtils.redirect("https://www.acgist.com",      "index.html"),        path);
+        assertEquals(UrlUtils.redirect("https://www.acgist.com/",     "index.html"),        path);
+        assertEquals(UrlUtils.redirect("https://www.acgist.com",      "/index.html"),       path);
+        assertEquals(UrlUtils.redirect("https://www.acgist.com/",     "/index.html"),       path);
+        assertEquals(UrlUtils.redirect("https://www.acgist.com/a",    "index.html"),        path);
+        assertEquals(UrlUtils.redirect("https://www.acgist.com/a/",   "index.html"),        pathA);
+        assertEquals(UrlUtils.redirect("https://www.acgist.com/a",    "/index.html"),       path);
+        assertEquals(UrlUtils.redirect("https://www.acgist.com/a/",   "/index.html"),       path);
+        assertEquals(UrlUtils.redirect("https://www.acgist.com/a/b",  "index.html"),        pathA);
+        assertEquals(UrlUtils.redirect("https://www.acgist.com/a/b/", "index.html"),        pathAB);
+        assertEquals(UrlUtils.redirect("https://www.acgist.com/a/b",  "/index.html"),       path);
+        assertEquals(UrlUtils.redirect("https://www.acgist.com/a/b/", "/index.html"),       path);
+        assertEquals(UrlUtils.redirect("https://www.acgist.com/a/b/", " /index.html "),     path);
+        assertEquals(UrlUtils.redirect("https://www.acgist.com/a/b/", "\"/index.html\""),   path);
+        assertEquals(UrlUtils.redirect("https://www.acgist.com/a/b/", " \"/index.html\" "), path);
+        assertEquals(UrlUtils.redirect(path, baidu), baidu);
+    }
 
-	@Test
-	void testEncodeDecode() throws UnsupportedEncodingException {
-		assertEquals("%2B+%2B", URLEncoder.encode("+ +", "UTF-8"));
-		assertEquals("+ +", URLDecoder.decode("%2B+%2B", "UTF-8"));
-		assertEquals("%2B%20%2B", UrlUtils.encode("+ +"));
-		assertEquals("+ +", UrlUtils.decode("%2B+%2B"));
-	}
-	
+    @Test
+    void testEncodeDecode() throws UnsupportedEncodingException {
+        assertEquals("%2B+%2B", URLEncoder.encode("+ +", "UTF-8"));
+        assertEquals("+ +", URLDecoder.decode("%2B+%2B", "UTF-8"));
+        assertEquals("%2B%20%2B", UrlUtils.encode("+ +"));
+        assertEquals("+ +", UrlUtils.decode("%2B+%2B"));
+    }
+    
 }
