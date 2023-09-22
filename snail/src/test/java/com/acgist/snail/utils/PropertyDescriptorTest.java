c5dbb43cefba66ad356506b80b7de68d6f6bca2e
acgist <289547414@qq.com>
2023-09-22 08:19:21 +0800

    [*] 日常优化

--- snail/src/test/java/com/acgist/snail/utils/PropertyDescriptorTest.java
+++ snail/src/test/java/com/acgist/snail/utils/PropertyDescriptorTest.java
@@ -16,73 +16,73 @@ import com.acgist.snail.context.entity.TaskEntity;
 
 class PropertyDescriptorTest extends Performance {
 
-	@Test
-	void testGetter() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		final String id = "1234";
-		final TaskEntity task = new TaskEntity();
-		task.setId(id);
-		assertEquals(id, PropertyDescriptor.newInstance(task).get("id"));
-	}
-	
-	@Test
-	void testSetter() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		final String id = "1234";
-		final TaskEntity task = new TaskEntity();
-		assertNull(task.getId());
-		PropertyDescriptor.newInstance(task).set("id", id);
-		assertEquals(id, task.getId());
-	}
-	
-	@Test
-	void testEnumProperty() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		boolean find = false;
-		final TaskEntity task = new TaskEntity();
-		task.setStatus(Status.AWAIT);
-		final PropertyDescriptor descriptor = PropertyDescriptor.newInstance(task);
-		if(descriptor.getPropertyType("status").isEnum()) {
-			final var enums = descriptor.getPropertyType("status").getEnumConstants();
-			for (Object object : enums) {
-				if(object == descriptor.get("status")) {
-					find = true;
-					break;
-				}
-			}
-		}
-		assertTrue(find);
-	}
+    @Test
+    void testGetter() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        final String id = "1234";
+        final TaskEntity task = new TaskEntity();
+        task.setId(id);
+        assertEquals(id, PropertyDescriptor.newInstance(task).get("id"));
+    }
+    
+    @Test
+    void testSetter() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        final String id = "1234";
+        final TaskEntity task = new TaskEntity();
+        assertNull(task.getId());
+        PropertyDescriptor.newInstance(task).set("id", id);
+        assertEquals(id, task.getId());
+    }
+    
+    @Test
+    void testEnumProperty() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        boolean find = false;
+        final TaskEntity task = new TaskEntity();
+        task.setStatus(Status.AWAIT);
+        final PropertyDescriptor descriptor = PropertyDescriptor.newInstance(task);
+        if(descriptor.getPropertyType("status").isEnum()) {
+            final Object[] enums = descriptor.getPropertyType("status").getEnumConstants();
+            for (final Object object : enums) {
+                if(object == descriptor.get("status")) {
+                    find = true;
+                    break;
+                }
+            }
+        }
+        assertTrue(find);
+    }
 
-	@Test
-	void testWeakMap() {
-		TaskEntity task = new TaskEntity();
-		final Map<TaskEntity, Map<Integer, Integer>> taskMap = new WeakHashMap<>();
-		taskMap.put(task, Map.of(1, 2));
-		System.out.println(taskMap);
-		task = null;
-		System.gc();
-		System.out.println(taskMap);
-		Class<TaskEntity> clazz = TaskEntity.class;
-		final Map<Class<?>, Map<Integer, Integer>> clazzMap = new WeakHashMap<>();
-		clazzMap.put(clazz, Map.of(1, 2));
-		System.out.println(clazzMap);
-		clazz = null;
-		System.gc();
-		System.out.println(clazzMap);
-	}
-	
-	@Test
-	void testCosted() {
-		final TaskEntity task = new TaskEntity();
-		final var descriptor = PropertyDescriptor.newInstance(task);
-		this.costed(100000, () -> descriptor.get("id"));
-		this.costed(100000, () -> descriptor.set("id", "1234"));
-		assertNotNull(task);
-		this.costed(1000, () -> {
-			final var costed = PropertyDescriptor.newInstance(new TaskEntity());
-			costed.get("id");
-			costed.set("id", "1234");
-//			System.gc();
-		});
-		System.gc();
-	}
-	
+    @Test
+    void testWeakMap() {
+        TaskEntity task = new TaskEntity();
+        final Map<TaskEntity, Map<Integer, Integer>> taskMap = new WeakHashMap<>();
+        taskMap.put(task, Map.of(1, 2));
+        this.log("{}", taskMap);
+        task = null;
+        System.gc();
+        this.log("{}", taskMap);
+        Class<TaskEntity> clazz = TaskEntity.class;
+        final Map<Class<?>, Map<Integer, Integer>> clazzMap = new WeakHashMap<>();
+        clazzMap.put(clazz, Map.of(1, 2));
+        this.log("{}", clazzMap);
+        clazz = null;
+        System.gc();
+        this.log("{}", clazzMap);
+    }
+    
+    @Test
+    void testCosted() {
+        final TaskEntity task = new TaskEntity();
+        final PropertyDescriptor descriptor = PropertyDescriptor.newInstance(task);
+        this.costed(100000, () -> descriptor.get("id"));
+        this.costed(100000, () -> descriptor.set("id", "1234"));
+        assertNotNull(task);
+        this.costed(1000, () -> {
+            final PropertyDescriptor costed = PropertyDescriptor.newInstance(new TaskEntity());
+            costed.get("id");
+            costed.set("id", "1234");
+//          System.gc();
+        });
+        System.gc();
+    }
+    
 }
