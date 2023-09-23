e1728b1ac2bb16adc127a9ec00c5abddd151bf5a
acgist <289547414@qq.com>
2023-09-23 11:04:27 +0800

    [*] 日常优化

--- snail/src/test/java/com/acgist/snail/utils/PerformanceTest.java
+++ snail/src/test/java/com/acgist/snail/utils/PerformanceTest.java
@@ -1,19 +1,23 @@
 package com.acgist.snail.utils;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 import org.junit.jupiter.api.Test;
 
 class PerformanceTest extends Performance {
 
-	@Test
-	void testLog() {
-		this.log(null);
-		this.log("1");
-		this.log(null, "1");
-		this.log(null, null, null);
-		this.log(null, new Object[] { null });
-		this.log(null, new Object[] { null, null });
-		this.log(null, new Object[] {});
-		this.log(null, "1", "2", 3);
-	}
-	
+    @Test
+    void testLog() {
+        assertDoesNotThrow(() -> {
+            this.log(null);
+            this.log("1");
+            this.log("{}", "1");
+            this.log("{}", null, null);
+            this.log("{}", new Object[] { null });
+            this.log("{}", new Object[] { null, null });
+            this.log("{}", new Object[] {});
+            this.log("{}", "1", "2", 3);
+        });
+    }
+    
 }
