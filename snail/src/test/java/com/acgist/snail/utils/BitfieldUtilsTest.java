70104a388e44eca91484a5f560f070299a205c5b
acgist <289547414@qq.com>
2023-10-10 08:01:04 +0800

    [*] 日常优化

--- snail/src/test/java/com/acgist/snail/utils/BitfieldUtilsTest.java
+++ snail/src/test/java/com/acgist/snail/utils/BitfieldUtilsTest.java
@@ -9,17 +9,17 @@ import org.junit.jupiter.api.Test;
 
 class BitfieldUtilsTest extends Performance {
 
-	@Test
-	void testBitfieldUtils() {
-		BitSet bitSet = new BitSet();
-		bitSet.set(0);
-		assertEquals((byte) 0B1000_0000, BitfieldUtils.toBytes(8, bitSet)[0]);
-		bitSet = new BitSet();
-		bitSet.set(7);
-		assertEquals((byte) 0B0000_0001, BitfieldUtils.toBytes(8, bitSet)[0]);
-		assertTrue(BitfieldUtils.toBitSet(new byte[] {(byte) 0B1000_0000}).get(0));
-		assertTrue(BitfieldUtils.toBitSet(new byte[] {(byte) 0B0000_0001}).get(7));
-		assertEquals(16, BitfieldUtils.toBytes(127, bitSet).length);
-	}
-	
+    @Test
+    void testBitfieldUtils() {
+        BitSet bitSet = new BitSet();
+        bitSet.set(0);
+        assertEquals((byte) 0B1000_0000, BitfieldUtils.toBytes(8, bitSet)[0]);
+        bitSet = new BitSet();
+        bitSet.set(7);
+        assertEquals((byte) 0B0000_0001, BitfieldUtils.toBytes(8, bitSet)[0]);
+        assertTrue(BitfieldUtils.toBitSet(new byte[] {(byte) 0B1000_0000}).get(0));
+        assertTrue(BitfieldUtils.toBitSet(new byte[] {(byte) 0B0000_0001}).get(7));
+        assertEquals(16, BitfieldUtils.toBytes(127, bitSet).length);
+    }
+    
 }
--- snail/src/test/java/com/acgist/snail/utils/ByteUtilsTest.java
+++ snail/src/test/java/com/acgist/snail/utils/ByteUtilsTest.java
@@ -10,30 +10,30 @@ import org.junit.jupiter.api.Test;
 
 class ByteUtilsTest extends Performance {
 
-	@Test
-	void testEmpty() {
-		final ByteBuffer buffer = ByteBuffer.allocate(0);
-		assertFalse(buffer.hasRemaining());
-		final var bytes = ByteUtils.remainingToBytes(buffer);
-		final var string = ByteUtils.remainingToString(buffer);
-		assertArrayEquals(new byte[] {}, bytes);
-		assertEquals("", string);
-	}
-	
-	@Test
-	void testRemainingToBytes() {
-		final var source = "这是一个测试".getBytes();
-		final ByteBuffer buffer = ByteBuffer.wrap(source);
-		final var bytes = ByteUtils.remainingToBytes(buffer);
-		assertArrayEquals(source, bytes);
-	}
-	
-	@Test
-	void testRemainingToString() {
-		final var source = "这是一个测试";
-		final ByteBuffer buffer = ByteBuffer.wrap(source.getBytes());
-		final var bytes = ByteUtils.remainingToString(buffer);
-		assertEquals(source, bytes);
-	}
-	
+    @Test
+    void testEmpty() {
+        final ByteBuffer buffer = ByteBuffer.allocate(0);
+        assertFalse(buffer.hasRemaining());
+        final byte[] bytes  = ByteUtils.remainingToBytes(buffer);
+        final String string = ByteUtils.remainingToString(buffer);
+        assertArrayEquals(new byte[] {}, bytes);
+        assertEquals("", string);
+    }
+    
+    @Test
+    void testRemainingToBytes() {
+        final byte[] source = "这是一个测试".getBytes();
+        final ByteBuffer buffer = ByteBuffer.wrap(source);
+        final byte[] bytes = ByteUtils.remainingToBytes(buffer);
+        assertArrayEquals(source, bytes);
+    }
+    
+    @Test
+    void testRemainingToString() {
+        final String source = "这是一个测试";
+        final ByteBuffer buffer = ByteBuffer.wrap(source.getBytes());
+        final String bytes = ByteUtils.remainingToString(buffer);
+        assertEquals(source, bytes);
+    }
+    
 }
