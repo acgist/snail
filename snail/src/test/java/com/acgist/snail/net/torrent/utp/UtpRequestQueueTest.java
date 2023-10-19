aa1d0611f5e0fc1336b3de1540005b1f6f54f23a
acgist <289547414@qq.com>
2023-10-20 07:41:59 +0800

    [*] 日常优化

--- snail/src/test/java/com/acgist/snail/net/torrent/utp/UtpRequestQueueTest.java
+++ snail/src/test/java/com/acgist/snail/net/torrent/utp/UtpRequestQueueTest.java
@@ -4,14 +4,16 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
+import com.acgist.snail.net.torrent.peer.PeerSubMessageHandler;
+
 class UtpRequestQueueTest {
 
-	@Test
-	void testUtpRequestQueue() {
-		final UtpRequestQueue manager = UtpRequestQueue.getInstance();
-		manager.queue().add(UtpRequest.newInstance(null, null));
-		manager.queue().add(UtpRequest.newInstance(null, null));
-		assertNotEquals(manager.queue(), manager.queue());
-	}
+    @Test
+    void testUtpRequestQueue() throws InterruptedException {
+        final UtpRequestQueue manager = UtpRequestQueue.getInstance();
+        manager.queue().add(UtpRequest.newInstance(null, PeerSubMessageHandler.newInstance()));
+        manager.queue().add(UtpRequest.newInstance(null, PeerSubMessageHandler.newInstance()));
+        assertNotEquals(manager.queue(), manager.queue());
+    }
 
 }
--- snail/src/test/java/com/acgist/snail/net/torrent/utp/UtpWindowTest.java
+++ snail/src/test/java/com/acgist/snail/net/torrent/utp/UtpWindowTest.java
@@ -8,20 +8,20 @@ import com.acgist.snail.logger.LoggerConfig;
 import com.acgist.snail.utils.Performance;
 
 class UtpWindowTest extends Performance {
-	
-	void ack(UtpWindow window) {
-		for (int index = 0; index < 15; index++) {
-			window.build();
-		}
-		final short seqnr = window.build().getSeqnr();
-		window.ack(seqnr, 100);
-	}
-	
-	@Test
-	void testAck() {
-		LoggerConfig.off();
-		final UtpWindow window = UtpWindow.newSendInstance();
-		assertDoesNotThrow(() -> this.costed(100000, () -> this.ack(window)));
-	}
+    
+    void ack(UtpWindow window) {
+        for (int index = 0; index < 15; index++) {
+            window.build();
+        }
+        final short seqnr = window.build().getSeqnr();
+        window.ack(seqnr, 100);
+    }
+    
+    @Test
+    void testAck() {
+        LoggerConfig.off();
+        final UtpWindow window = UtpWindow.newSendInstance();
+        assertDoesNotThrow(() -> this.costed(100000, () -> this.ack(window)));
+    }
 
 }
