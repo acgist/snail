f465d894d553262d89972f7e3b618ea63627d620
acgist <289547414@qq.com>
2023-10-24 08:04:06 +0800

    [*] 日常优化

--- snail/src/test/java/com/acgist/snail/net/torrent/tracker/TrackerLauncherGroupTest.java
+++ snail/src/test/java/com/acgist/snail/net/torrent/tracker/TrackerLauncherGroupTest.java
@@ -15,23 +15,23 @@ import com.acgist.snail.utils.ThreadUtils;
 
 class TrackerLauncherGroupTest extends Performance {
 
-	@Test
-	void testRelease() throws DownloadException, InterruptedException {
-		final var entity = new TaskEntity();
-		entity.setUrl("1".repeat(40));
-		entity.setSize(0L);
-		final var session = TorrentSession.newInstance(InfoHash.newInstance("1".repeat(40)), null);
-		session.magnet(TaskSession.newInstance(entity));
-		final var group = TrackerLauncherGroup.newInstance(session);
-		group.loadTracker();
-		final var thread = new Thread(() -> group.findPeer());
-		thread.start();
-		ThreadUtils.sleep(1000);
-		group.release();
-		final long start = System.currentTimeMillis();
-		thread.join();
-		final long end = System.currentTimeMillis();
-		assertTrue(SystemConfig.CONNECT_TIMEOUT_MILLIS + 1000 > end - start);
-	}
-	
+    @Test
+    void testRelease() throws DownloadException, InterruptedException {
+        final TaskEntity entity = new TaskEntity();
+        entity.setUrl("1".repeat(40));
+        entity.setSize(0L);
+        final TorrentSession session = TorrentSession.newInstance(InfoHash.newInstance("1".repeat(40)), null);
+        session.magnet(TaskSession.newInstance(entity));
+        final TrackerLauncherGroup group = TrackerLauncherGroup.newInstance(session);
+        group.loadTracker();
+        final Thread thread = new Thread(() -> group.findPeer());
+        thread.start();
+        ThreadUtils.sleep(1000);
+        group.release();
+        final long start = System.currentTimeMillis();
+        thread.join();
+        final long end = System.currentTimeMillis();
+        assertTrue(SystemConfig.CONNECT_TIMEOUT_MILLIS + 1000 > end - start);
+    }
+    
 }
--- snail/src/test/java/com/acgist/snail/net/torrent/tracker/UdpTrackerSessionTest.java
+++ snail/src/test/java/com/acgist/snail/net/torrent/tracker/UdpTrackerSessionTest.java
@@ -2,6 +2,8 @@ package com.acgist.snail.net.torrent.tracker;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import com.acgist.snail.net.DownloadException;
@@ -13,21 +15,21 @@ import com.acgist.snail.utils.ThreadUtils;
 
 class UdpTrackerSessionTest extends Performance {
 
-	@Test
-	void testAnnounce() throws NetException, DownloadException {
-		final String path = "E:/snail/902FFAA29EE632C8DC966ED9AB573409BA9A518E.torrent";
-		final String announceUrl = "udp://tracker.opentrackr.org:1337/announce";
-//		final String announceUrl = "udp://[2001:19f0:6c01:1b7d:5400:1ff:fefc:3c2a]:6969/announce";
-		final TorrentSession torrentSession = TorrentContext.getInstance().newTorrentSession(path);
-		final var list = TrackerContext.getInstance().sessions(announceUrl);
-		final TrackerSession session = list.stream()
-			.filter(value -> value.equalsAnnounceUrl(announceUrl))
-			.findFirst()
-			.get();
-		session.started(1000, torrentSession);
-		session.scrape(1000, torrentSession);
-		ThreadUtils.sleep(5000);
-		assertNotNull(session);
-	}
-	
+    @Test
+    void testAnnounce() throws NetException, DownloadException {
+        final String path = "D:/tmp/snail/ebfc2cf2ce69ba2f7aea36bbef290f0cce21386c.torrent";
+        final String announceUrl = "udp://tracker.opentrackr.org:1337/announce";
+//      final String announceUrl = "udp://[2001:19f0:6c01:1b7d:5400:1ff:fefc:3c2a]:6969/announce";
+        final TorrentSession torrentSession = TorrentContext.getInstance().newTorrentSession(path);
+        final List<TrackerSession> list = TrackerContext.getInstance().sessions(announceUrl);
+        final TrackerSession session = list.stream()
+            .filter(value -> value.equalsAnnounceUrl(announceUrl))
+            .findFirst()
+            .get();
+        session.started(1000, torrentSession);
+        session.scrape(1000, torrentSession);
+        ThreadUtils.sleep(5000);
+        assertNotNull(session);
+    }
+    
 }
