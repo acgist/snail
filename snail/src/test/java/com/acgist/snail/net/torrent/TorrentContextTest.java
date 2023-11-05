fe845dea6f243698b28e95effafe7a3e04ff5972
acgist <289547414@qq.com>
2023-11-06 07:25:19 +0800

    [*] 日常优化

--- snail/src/test/java/com/acgist/snail/net/torrent/TorrentContextTest.java
+++ snail/src/test/java/com/acgist/snail/net/torrent/TorrentContextTest.java
@@ -11,22 +11,22 @@ import com.acgist.snail.utils.Performance;
 
 class TorrentContextTest extends Performance {
 
-	@Test
-	void testTorrentContext() throws DownloadException {
-		String path = "E:/snail/902FFAA29EE632C8DC966ED9AB573409BA9A518E.torrent";
-		final var context = TorrentContext.getInstance();
-		final var a = context.newTorrentSession(path);
-		context.newTorrentSession(path);
-		assertNotNull(a);
-		final var b = context.newTorrentSession("0000000000000000000000000000000000000000", null);
-		context.newTorrentSession("0000000000000000000000000000000000000000", null);
-		assertNotNull(b);
-		assertEquals(2, context.allInfoHash().size());
-		assertEquals(2, context.allTorrentSession().size());
-		assertTrue(context.exist("902FFAA29EE632C8DC966ED9AB573409BA9A518E".toLowerCase()));
-		assertTrue(context.exist("0000000000000000000000000000000000000000"));
-		assertNotNull(context.torrentSession("902FFAA29EE632C8DC966ED9AB573409BA9A518E".toLowerCase()));
-		assertNotNull(context.torrentSession("0000000000000000000000000000000000000000"));
-	}
-	
+    @Test
+    void testTorrentContext() throws DownloadException {
+        final String path = "D:/tmp/snail/ebfc2cf2ce69ba2f7aea36bbef290f0cce21386c.torrent";
+        final TorrentContext context = TorrentContext.getInstance();
+        final TorrentSession a = context.newTorrentSession(path);
+        context.newTorrentSession(path);
+        assertNotNull(a);
+        final TorrentSession b = context.newTorrentSession("0000000000000000000000000000000000000000", null);
+        context.newTorrentSession("0000000000000000000000000000000000000000", null);
+        assertNotNull(b);
+        assertEquals(2, context.allInfoHash().size());
+        assertEquals(2, context.allTorrentSession().size());
+        assertTrue(context.exist("ebfc2cf2ce69ba2f7aea36bbef290f0cce21386c".toLowerCase()));
+        assertTrue(context.exist("0000000000000000000000000000000000000000"));
+        assertNotNull(context.torrentSession("ebfc2cf2ce69ba2f7aea36bbef290f0cce21386c".toLowerCase()));
+        assertNotNull(context.torrentSession("0000000000000000000000000000000000000000"));
+    }
+    
 }
