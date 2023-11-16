23e89aad80ea6afc02d1d2bb8cee023ccb371b10
acgist <289547414@qq.com>
2023-11-17 07:37:54 +0800

    [*] 日常优化

--- snail/src/main/java/com/acgist/snail/utils/Performance.java
+++ snail/src/main/java/com/acgist/snail/utils/Performance.java
@@ -1,6 +1,7 @@
 package com.acgist.snail.utils;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.atomic.AtomicLong;
 
 import com.acgist.snail.config.SystemConfig;
@@ -10,123 +11,129 @@ import com.acgist.snail.logger.LoggerFactory;
 import com.acgist.snail.logger.Tuple;
 
 /**
- * <p>性能分析工具</p>
+ * 性能分析工具
  * 
  * @author acgist
  */
 public abstract class Performance {
 
-	protected final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
-	
-	/**
-	 * <p>消耗时间统计</p>
-	 */
-	protected final AtomicLong costTime = new AtomicLong();
-	
-	/**
-	 * <p>记录日志</p>
-	 * 
-	 * @param message 日志信息
-	 */
-	protected final void log(Object message) {
-		this.log(Tuple.FORMAT_CODE, message);
-	}
-	
-	/**
-	 * <p>记录日志</p>
-	 * 
-	 * @param message 日志信息
-	 * @param args 日志参数
-	 */
-	protected final void log(String message, Object ... args) {
-		LOGGER.info(message, args);
-	}
-	
-	/**
-	 * <p>开始统计消耗时间</p>
-	 */
-	protected final void cost() {
-		this.costTime.set(System.currentTimeMillis());
-	}
-	
-	/**
-	 * <p>结束统计消耗时间</p>
-	 * <p>重置消耗时间统计</p>
-	 * 
-	 * @return 消耗时间
-	 */
-	protected final long costed() {
-		final long time = System.currentTimeMillis();
-		final long costed = time - this.costTime.getAndSet(time);
-		if(LOGGER.isInfoEnabled()) {
-			LOGGER.info("消耗时间（毫秒-秒）：{}-{}", costed, costed / SystemConfig.ONE_SECOND_MILLIS);
-		}
-		return costed;
-	}
-	
-	/**
-	 * <p>重复执行消耗任务</p>
-	 * 
-	 * @param count 执行次数
-	 * @param coster 消耗任务
-	 * 
-	 * @return 消耗时间
-	 */
-	protected final long costed(int count, Coster coster) {
-		this.cost();
-		for (int index = 0; index < count; index++) {
-			coster.execute();
-		}
-		return this.costed();
-	}
-	
-	/**
-	 * <p>重复执行消耗任务</p>
-	 * 
-	 * @param count 任务数量
-	 * @param thread 线程数量
-	 * @param coster 消耗任务
-	 * 
-	 * @return 消耗时间
-	 */
-	protected final long costed(int count, int thread, Coster coster) {
-		final var latch = new CountDownLatch(count);
-		final var executor = SystemThreadContext.newExecutor(thread, thread, count, 60L, SystemThreadContext.SNAIL_THREAD_COSTED);
-		this.cost();
-		for (int index = 0; index < count; index++) {
-			executor.submit(() -> {
-				try {
-					coster.execute();
-				} catch (Exception e) {
-					LOGGER.error("执行任务异常", e);
-				} finally {
-					latch.countDown();
-				}
-			});
-		}
-		try {
-			latch.await();
-		} catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
-			LOGGER.error("线程等待异常", e);
-		}
-		final long costed = this.costed();
-		SystemThreadContext.shutdownNow(executor);
-		return costed;
-	}
+    protected final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+    
+    /**
+     * 消耗时间统计
+     */
+    protected final AtomicLong costTime = new AtomicLong();
+    
+    /**
+     * 记录日志
+     * 
+     * @param message 日志信息
+     */
+    protected final void log(Object message) {
+        this.log(Tuple.FORMAT_CODE, message);
+    }
+    
+    /**
+     * 记录日志
+     * 
+     * @param message 日志信息
+     * @param args    日志参数
+     */
+    protected final void log(String message, Object ... args) {
+        LOGGER.info(message, args);
+    }
+    
+    /**
+     * 开始统计消耗时间
+     */
+    protected final void cost() {
+        this.costTime.set(System.currentTimeMillis());
+    }
+    
+    /**
+     * 结束统计消耗时间
+     * 重置消耗时间统计
+     * 
+     * @return 消耗时间
+     */
+    protected final long costed() {
+        final long time   = System.currentTimeMillis();
+        final long costed = time - this.costTime.getAndSet(time);
+        if(LOGGER.isInfoEnabled()) {
+            LOGGER.info("消耗时间（毫秒-秒）：{} - {}", costed, costed / SystemConfig.ONE_SECOND_MILLIS);
+        }
+        return costed;
+    }
+    
+    /**
+     * 重复执行消耗任务
+     * 
+     * @param count  执行次数
+     * @param coster 消耗任务
+     * 
+     * @return 消耗时间
+     */
+    protected final long costed(int count, Coster coster) {
+        this.cost();
+        for (int index = 0; index < count; index++) {
+            coster.execute();
+        }
+        return this.costed();
+    }
+    
+    /**
+     * 重复执行消耗任务
+     * 
+     * @param count  任务数量
+     * @param thread 线程数量
+     * @param coster 消耗任务
+     * 
+     * @return 消耗时间
+     */
+    protected final long costed(int count, int thread, Coster coster) {
+        final CountDownLatch latch     = new CountDownLatch(count);
+        final ExecutorService executor = SystemThreadContext.newExecutor(
+            thread,
+            thread,
+            count,
+            60L,
+            SystemThreadContext.SNAIL_THREAD_COSTED
+        );
+        this.cost();
+        for (int index = 0; index < count; index++) {
+            executor.submit(() -> {
+                try {
+                    coster.execute();
+                } catch (Exception e) {
+                    LOGGER.error("执行任务异常", e);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            LOGGER.error("线程等待异常", e);
+        }
+        final long costed = this.costed();
+        SystemThreadContext.shutdownNow(executor);
+        return costed;
+    }
 
-	/**
-	 * <p>执行任务接口</p>
-	 * 
-	 * @author acgist
-	 */
-	public interface Coster {
+    /**
+     * 执行任务接口
+     * 
+     * @author acgist
+     */
+    public interface Coster {
 
-		/**
-		 * <p>执行任务</p>
-		 */
-		void execute();
-		
-	}
-	
+        /**
+         * 执行任务
+         */
+        void execute();
+        
+    }
+    
 }
