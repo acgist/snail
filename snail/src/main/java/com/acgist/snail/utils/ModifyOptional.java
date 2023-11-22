d7adc96dd0fd6f4c5e781aba6ccbf5dfb2f10c17
acgist <289547414@qq.com>
2023-11-23 07:43:49 +0800

    [*] 日常优化

--- snail/src/main/java/com/acgist/snail/utils/ModifyOptional.java
+++ snail/src/main/java/com/acgist/snail/utils/ModifyOptional.java
@@ -1,7 +1,7 @@
 package com.acgist.snail.utils;
 
 /**
- * <p>可修改的Optional</p>
+ * 可修改的Optional
  * 
  * @param <T> 数据类型
  * 
@@ -9,101 +9,87 @@ package com.acgist.snail.utils;
  */
 public final class ModifyOptional<T> {
 
-	/**
-	 * <p>数据</p>
-	 */
-	private T t;
-	
-	private ModifyOptional() {
-	}
-	
-	/**
-	 * @param t 数据
-	 */
-	private ModifyOptional(T t) {
-		this.t = t;
-	}
+    /**
+     * 数据
+     */
+    private T t;
+    
+    private ModifyOptional() {
+    }
+    
+    /**
+     * @param t 数据
+     */
+    private ModifyOptional(T t) {
+        this.t = t;
+    }
 
-	/**
-	 * <p>获取工具对象</p>
-	 * 
-	 * @param <T> 数据类型
-	 * 
-	 * @return {@link ModifyOptional}
-	 */
-	public static final <T> ModifyOptional<T> newInstance() {
-		return new ModifyOptional<>();
-	}
-	
-	/**
-	 * <p>获取工具对象</p>
-	 * 
-	 * @param <T> 数据类型
-	 * 
-	 * @param t 数据
-	 * 
-	 * @return {@link ModifyOptional}
-	 */
-	public static final <T> ModifyOptional<T> newInstance(T t) {
-		return new ModifyOptional<>(t);
-	}
-	
-	/**
-	 * <p>设置数据</p>
-	 * 
-	 * @param t 数据
-	 */
-	public void set(T t) {
-		this.t = t;
-	}
-	
-	/**
-	 * <p>获取数据</p>
-	 * 
-	 * @return 数据
-	 */
-	public T get() {
-		return this.t;
-	}
-	
-	/**
-	 * 删除旧的数据
-	 * 
-	 * @return 旧的数据
-	 */
-	public T delete() {
-		final T old = this.t;
-		this.t = null;
-		return old;
-	}
+    /**
+     * @param <T> 数据类型
+     * 
+     * @return {@link ModifyOptional}
+     */
+    public static final <T> ModifyOptional<T> newInstance() {
+        return new ModifyOptional<>();
+    }
+    
+    /**
+     * @param <T> 数据类型
+     * 
+     * @param t 数据
+     * 
+     * @return {@link ModifyOptional}
+     */
+    public static final <T> ModifyOptional<T> newInstance(T t) {
+        return new ModifyOptional<>(t);
+    }
+    
+    /**
+     * @param t 数据
+     */
+    public void set(T t) {
+        this.t = t;
+    }
+    
+    /**
+     * @return 数据
+     */
+    public T get() {
+        return this.t;
+    }
+    
+    /**
+     * @param defaultValue 默认数据
+     * 
+     * @return 数据
+     */
+    public T get(T defaultValue) {
+        return this.isEmpty() ? defaultValue : this.get();
+    }
+    
+    /**
+     * 删除旧的数据
+     * 
+     * @return 旧的数据
+     */
+    public T delete() {
+        final T old = this.t;
+        this.t = null;
+        return old;
+    }
 
-	/**
-	 * <p>获取数据</p>
-	 * 
-	 * @param defaultValue 默认数据
-	 * 
-	 * @return 数据
-	 */
-	public T get(T defaultValue) {
-		return this.isEmpty() ? defaultValue : this.get();
-	}
-	
-	/**
-	 * <p>判断数据是否为空</p>
-	 * 
-	 * @return 数据是否为空
-	 */
-	public boolean isEmpty() {
-		return this.t == null;
-	}
-	
-	/**
-	 * <p>判断数据是否非空</p>
-	 * 
-	 * @return 数据是否非空
-	 */
-	public boolean isPresent() {
-		return !this.isEmpty();
-	}
-	
+    /**
+     * @return 数据是否为空
+     */
+    public boolean isEmpty() {
+        return this.t == null;
+    }
+    
+    /**
+     * @return 数据是否非空
+     */
+    public boolean isPresent() {
+        return !this.isEmpty();
+    }
+    
 }
