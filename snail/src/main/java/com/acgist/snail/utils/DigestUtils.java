895107b3aab5e692e167d4d7c6e9c3c83bba60df
acgist <289547414@qq.com>
2023-12-05 08:05:46 +0800

    [*] 日常优化

--- snail/src/main/java/com/acgist/snail/utils/DigestUtils.java
+++ snail/src/main/java/com/acgist/snail/utils/DigestUtils.java
@@ -4,104 +4,98 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 /**
- * <p>散列算法工具</p>
- * <p>散列算法：计算数据摘要</p>
+ * 散列算法工具
+ * 散列算法：计算数据摘要
  * 
  * @author acgist
  */
 public final class DigestUtils {
-	
-	private DigestUtils() {
-	}
-	
-	/**
-	 * <p>散列算法：{@value}</p>
-	 */
-	public static final String ALGO_MD5 = "MD5";
-	/**
-	 * <p>散列算法：{@value}</p>
-	 */
-	public static final String ALGO_SHA1 = "SHA-1";
-	
-	/**
-	 * <p>获取MD5散列算法对象</p>
-	 * 
-	 * @return MD5散列算法对象
-	 * 
-	 * @see #digest(String)
-	 */
-	public static final MessageDigest md5() {
-		return DigestUtils.digest(ALGO_MD5);
-	}
+    
+    private DigestUtils() {
+    }
+    
+    /**
+     * 散列算法：{@value}
+     */
+    public static final String ALGO_MD5 = "MD5";
+    /**
+     * 散列算法：{@value}
+     */
+    public static final String ALGO_SHA1 = "SHA-1";
+    
+    /**
+     * @return MD5散列算法对象
+     * 
+     * @see #digest(String)
+     */
+    public static final MessageDigest md5() {
+        return DigestUtils.digest(ALGO_MD5);
+    }
 
-	/**
-	 * <p>获取SHA-1散列算法对象</p>
-	 * 
-	 * @return SHA-1散列算法对象
-	 * 
-	 * @see #digest(String)
-	 */
-	public static final MessageDigest sha1() {
-		return DigestUtils.digest(ALGO_SHA1);
-	}
-	
-	/**
-	 * <p>获取散列算法对象</p>
-	 * 
-	 * @param algo 算法名称
-	 * 
-	 * @return 算法对象
-	 */
-	public static final MessageDigest digest(String algo) {
-		try {
-			return MessageDigest.getInstance(algo);
-		} catch (NoSuchAlgorithmException e) {
-			throw new IllegalArgumentException("不支持的散列算法：" + algo, e);
-		}
-	}
-	
-	/**
-	 * <p>计算字节数组的MD5散列值</p>
-	 * 
-	 * @param bytes 字节数组
-	 * 
-	 * @return MD5散列值
-	 */
-	public static final byte[] md5(byte[] bytes) {
-		return DigestUtils.md5().digest(bytes);
-	}
-	
-	/**
-	 * 计算字符串的MD5散列值
-	 * 
-	 * @param value 字符串
-	 * 
-	 * @return MD5散列值
-	 */
-	public static final String md5Hex(String value) {
-	    return StringUtils.hex(DigestUtils.md5(value.getBytes()));
-	}
-	
-	/**
-	 * <p>计算字节数组的SHA-1散列值</p>
-	 * 
-	 * @param bytes 字节数组
-	 * 
-	 * @return SHA-1散列值
-	 */
-	public static final byte[] sha1(byte[] bytes) {
-		return DigestUtils.sha1().digest(bytes);
-	}
-	
-	/**
-	 * 计算字符串的SHA-1散列值
-	 * 
-	 * @param value 字符串
-	 * 
-	 * @return SHA-1散列值
-	 */
-	public static final String sha1Hex(String value) {
-	    return StringUtils.hex(DigestUtils.sha1(value.getBytes()));
-	}
-	
+    /**
+     * @return SHA-1散列算法对象
+     * 
+     * @see #digest(String)
+     */
+    public static final MessageDigest sha1() {
+        return DigestUtils.digest(ALGO_SHA1);
+    }
+    
+    /**
+     * @param algo 算法名称
+     * 
+     * @return 散列算法对象
+     */
+    public static final MessageDigest digest(String algo) {
+        try {
+            return MessageDigest.getInstance(algo);
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalArgumentException("不支持的散列算法：" + algo, e);
+        }
+    }
+    
+    /**
+     * 计算字节数组的MD5散列值
+     * 
+     * @param bytes 字节数组
+     * 
+     * @return MD5散列值
+     */
+    public static final byte[] md5(byte[] bytes) {
+        return DigestUtils.md5().digest(bytes);
+    }
+    
+    /**
+     * 计算字符串的MD5散列值
+     * 
+     * @param value 字符串
+     * 
+     * @return MD5散列值
+     */
+    public static final String md5Hex(String value) {
+        return StringUtils.hex(DigestUtils.md5(value.getBytes()));
+    }
+    
+    /**
+     * 计算字节数组的SHA-1散列值
+     * 
+     * @param bytes 字节数组
+     * 
+     * @return SHA-1散列值
+     */
+    public static final byte[] sha1(byte[] bytes) {
+        return DigestUtils.sha1().digest(bytes);
+    }
+    
+    /**
+     * 计算字符串的SHA-1散列值
+     * 
+     * @param value 字符串
+     * 
+     * @return SHA-1散列值
+     */
+    public static final String sha1Hex(String value) {
+        return StringUtils.hex(DigestUtils.sha1(value.getBytes()));
+    }
+    
 }
--- snail/src/main/java/com/acgist/snail/utils/EnumUtils.java
+++ snail/src/main/java/com/acgist/snail/utils/EnumUtils.java
@@ -15,60 +15,60 @@ import java.util.stream.Stream;
  */
 public final class EnumUtils {
 
-	private EnumUtils() {
-	}
-	
-	/**
-	 * 创建枚举索引数组
-	 * 注意：只有频繁使用枚举转义同时枚举索引和枚举数量差距不大时使用
-	 * 
-	 * @param <T> 枚举泛型
-	 * 
-	 * @param clazz 枚举类型
-	 * @param mapper 枚举索引函数
-	 * 
-	 * @return 枚举索引数组
-	 * 
-	 * TODO：类型安全
-	 */
-	@SuppressWarnings("unchecked")
-	public static final <T> T[] index(Class<T> clazz, Function<T, Byte> mapper) {
-		if(clazz == null || !clazz.isEnum()) {
-			throw new IllegalArgumentException("必须输入枚举类型");
-		}
-		final T[] array = clazz.getEnumConstants();
-		final int length = Stream.of(array)
-			.map(mapper::apply)
-			.map(v -> Byte.toUnsignedInt(v))
-			.max(Integer::compare)
-			.get() + 1;
-		if(array.length * 2 < length || Byte.MAX_VALUE < length) {
-			// 如果最大索引大于枚举数量两倍不适合建索引
-			throw new IllegalArgumentException("枚举类型不适合建索引");
-		}
-		final T[] index = (T[]) Array.newInstance(clazz, length);
-		for (final T value : array) {
-			index[mapper.apply(value)] = value;
-		}
-		return index;
-	}
-	
-	/**
-	 * 创建枚举索引Map
-	 * 
-	 * @param <T> 枚举泛型
-	 * 
-	 * @param clazz 枚举类型
-	 * @param mapper 枚举索引函数
-	 * 
-	 * @return 枚举Map索引
-	 */
-	public static final <T> Map<Integer, T> map(Class<T> clazz, Function<T, Integer> mapper) {
-		if(clazz == null || !clazz.isEnum()) {
-			throw new IllegalArgumentException("必须输入枚举类型");
-		}
-		final T[] array = clazz.getEnumConstants();
-		return Stream.of(array).collect(Collectors.toMap(v -> mapper.apply(v), UnaryOperator.identity()));
-	}
-	
+    private EnumUtils() {
+    }
+    
+    /**
+     * 创建枚举索引数组
+     * 注意：只有频繁使用枚举转义同时枚举索引和枚举数量差距不大时使用
+     * 
+     * @param <T> 枚举泛型
+     * 
+     * @param clazz  枚举类型
+     * @param mapper 枚举索引函数
+     * 
+     * @return 枚举索引数组
+     * 
+     * TODO：类型安全
+     */
+    @SuppressWarnings("unchecked")
+    public static final <T> T[] index(Class<T> clazz, Function<T, Byte> mapper) {
+        if(clazz == null || !clazz.isEnum()) {
+            throw new IllegalArgumentException("必须输入枚举类型");
+        }
+        final T[] array = clazz.getEnumConstants();
+        final int length = Stream.of(array)
+            .map(mapper::apply)
+            .map(v -> Byte.toUnsignedInt(v))
+            .max(Integer::compare)
+            .get() + 1;
+        if(array.length * 2 < length || Byte.MAX_VALUE < length) {
+            // 如果最大索引大于枚举数量两倍不适合建索引
+            throw new IllegalArgumentException("枚举类型不适合建索引");
+        }
+        final T[] index = (T[]) Array.newInstance(clazz, length);
+        for (final T value : array) {
+            index[mapper.apply(value)] = value;
+        }
+        return index;
+    }
+    
+    /**
+     * 创建枚举索引Map
+     * 
+     * @param <T> 枚举泛型
+     * 
+     * @param clazz  枚举类型
+     * @param mapper 枚举索引函数
+     * 
+     * @return 枚举Map索引
+     */
+    public static final <T> Map<Integer, T> map(Class<T> clazz, Function<T, Integer> mapper) {
+        if(clazz == null || !clazz.isEnum()) {
+            throw new IllegalArgumentException("必须输入枚举类型");
+        }
+        final T[] array = clazz.getEnumConstants();
+        return Stream.of(array).collect(Collectors.toMap(v -> mapper.apply(v), UnaryOperator.identity()));
+    }
+    
 }
