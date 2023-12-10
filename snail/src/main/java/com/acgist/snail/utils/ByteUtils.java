4281928fc5c299eb84ef2430e9adfd5962ad15a1
acgist <289547414@qq.com>
2023-12-11 07:39:02 +0800

    [*] 日常优化

--- snail/src/main/java/com/acgist/snail/utils/ByteUtils.java
+++ snail/src/main/java/com/acgist/snail/utils/ByteUtils.java
@@ -3,62 +3,37 @@ package com.acgist.snail.utils;
 import java.nio.ByteBuffer;
 
 /**
- * <p>字符工具</p>
+ * 字符工具
  * 
  * @author acgist
  */
 public final class ByteUtils {
 
-	private ByteUtils() {
-	}
-	
-	/**
-	 * <p>读取剩余字节数据</p>
-	 * 
-	 * @param buffer 缓冲数据
-	 * 
-	 * @return 剩余字节数据
-	 */
-	public static final byte[] remainingToBytes(ByteBuffer buffer) {
-		final byte[] bytes = new byte[buffer.remaining()];
-		buffer.get(bytes);
-		return bytes;
-	}
-	
-	/**
-	 * <p>读取剩余字节数据转为字符串</p>
-	 * 
-	 * @param buffer 缓冲数据
-	 * 
-	 * @return 剩余字节数据字符串
-	 */
-	public static final String remainingToString(ByteBuffer buffer) {
-		return new String(remainingToBytes(buffer));
-	}
-	
-	/**
-	 * 读取int字节数据
-	 * 
-	 * @param buffer 缓冲数据
-	 * 
-	 * @return int字节数据
-	 */
-	public static final byte[] intToBytes(ByteBuffer buffer) {
-		final int length = buffer.getInt();
-		final byte[] bytes = new byte[length];
-		buffer.get(bytes);
-		return bytes;
-	}
-	
-	/**
-	 * 读取int字节数据转为字符串
-	 * 
-	 * @param buffer 缓冲数据
-	 * 
-	 * @return int字节数据字符串
-	 */
-	public static final String intToString(ByteBuffer buffer) {
-		return new String(intToBytes(buffer));
-	}
-	
+    private ByteUtils() {
+    }
+    
+    /**
+     * 读取剩余字节数据
+     * 
+     * @param buffer 缓冲数据
+     * 
+     * @return 剩余字节数据
+     */
+    public static final byte[] remainingToBytes(ByteBuffer buffer) {
+        final byte[] bytes = new byte[buffer.remaining()];
+        buffer.get(bytes);
+        return bytes;
+    }
+    
+    /**
+     * 读取剩余字节数据转为字符串
+     * 
+     * @param buffer 缓冲数据
+     * 
+     * @return 剩余字节数据字符串
+     */
+    public static final String remainingToString(ByteBuffer buffer) {
+        return new String(ByteUtils.remainingToBytes(buffer));
+    }
+    
 }
--- snail/src/main/java/com/acgist/snail/utils/CollectionUtils.java
+++ snail/src/main/java/com/acgist/snail/utils/CollectionUtils.java
@@ -4,61 +4,53 @@ import java.util.Collection;
 import java.util.List;
 
 /**
- * <p>集合工具</p>
+ * 集合工具
  * 
  * @author acgist
  */
 public final class CollectionUtils {
 
-	private CollectionUtils() {
-	}
-	
-	/**
-	 * <p>判断是否为空</p>
-	 * 
-	 * @param list 集合
-	 * 
-	 * @return 是否为空
-	 */
-	public static final boolean isEmpty(Collection<?> list) {
-		return list == null || list.isEmpty();
-	}
-	
-	/**
-	 * <p>判断是否非空</p>
-	 * 
-	 * @param list 集合
-	 * 
-	 * @return 是否非空
-	 */
-	public static final boolean isNotEmpty(Collection<?> list) {
-		return !CollectionUtils.isEmpty(list);
-	}
-	
-	/**
-	 * 获取集合首个元素
-	 * 
-	 * @param <T> 泛型
-	 * 
-	 * @param list 集合
-	 * 
-	 * @return 首个元素
-	 */
-	public static final <T> T getFirst(List<T> list) {
-		return (list == null || list.isEmpty()) ? null : list.get(0);
-	}
+    private CollectionUtils() {
+    }
+    
+    /**
+     * @param list 集合
+     * 
+     * @return 是否为空
+     */
+    public static final boolean isEmpty(Collection<?> list) {
+        return list == null || list.isEmpty();
+    }
+    
+    /**
+     * @param list 集合
+     * 
+     * @return 是否非空
+     */
+    public static final boolean isNotEmpty(Collection<?> list) {
+        return !CollectionUtils.isEmpty(list);
+    }
+    
+    /**
+     * @param <T> 泛型
+     * 
+     * @param list 集合
+     * 
+     * @return 首个元素
+     */
+    public static final <T> T getFirst(List<T> list) {
+        return CollectionUtils.isEmpty(list) ? null : list.get(0);
+    }
 
-	/**
-	 * 获取集合尾部元素
-	 * 
-	 * @param <T> 泛型
-	 * 
-	 * @param list 集合
-	 * 
-	 * @return 尾部元素
-	 */
-	public static final <T> T getLast(List<T> list) {
-		return (list == null || list.isEmpty()) ? null : list.get(list.size() - 1);
-	}
-	
+    /**
+     * @param <T> 泛型
+     * 
+     * @param list 集合
+     * 
+     * @return 尾部元素
+     */
+    public static final <T> T getLast(List<T> list) {
+        return CollectionUtils.isEmpty(list) ? null : list.get(list.size() - 1);
+    }
+    
 }
