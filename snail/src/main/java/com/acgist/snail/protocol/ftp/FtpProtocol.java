aa38d9133d63ceb2fc39c319a77a2fcf0d39637c
acgist <289547414@qq.com>
2023-12-21 08:03:38 +0800

    [*] 日常优化

--- snail/src/main/java/com/acgist/snail/protocol/ftp/FtpProtocol.java
+++ snail/src/main/java/com/acgist/snail/protocol/ftp/FtpProtocol.java
@@ -9,44 +9,44 @@ import com.acgist.snail.net.ftp.FtpClient;
 import com.acgist.snail.protocol.Protocol;
 
 /**
- * <p>FTP协议</p>
+ * FTP协议
  * 
  * @author acgist
  */
 public final class FtpProtocol extends Protocol {
-	
-	private static final FtpProtocol INSTANCE = new FtpProtocol();
-	
-	public static final FtpProtocol getInstance() {
-		return INSTANCE;
-	}
-	
-	private FtpProtocol() {
-		super(Type.FTP, "FTP");
-	}
-	
-	@Override
-	public boolean available() {
-		return true;
-	}
-	
-	@Override
-	public IDownloader buildDownloader(ITaskSession taskSession) {
-		return FtpDownloader.newInstance(taskSession);
-	}
+    
+    private static final FtpProtocol INSTANCE = new FtpProtocol();
+    
+    public static final FtpProtocol getInstance() {
+        return INSTANCE;
+    }
+    
+    private FtpProtocol() {
+        super(Type.FTP, "FTP");
+    }
+    
+    @Override
+    public boolean available() {
+        return true;
+    }
+    
+    @Override
+    public IDownloader buildDownloader(ITaskSession taskSession) {
+        return FtpDownloader.newInstance(taskSession);
+    }
 
-	@Override
-	protected void buildSize() throws DownloadException {
-		final FtpClient client = FtpClient.newInstance(this.url);
-		try {
-			client.connect();
-			final long size = client.size();
-			this.taskEntity.setSize(size);
-		} catch (NetException e) {
-			throw new DownloadException(e);
-		} finally {
-			client.close();
-		}
-	}
+    @Override
+    protected void buildSize() throws DownloadException {
+        final FtpClient client = FtpClient.newInstance(this.url);
+        try {
+            client.connect();
+            final long size = client.size();
+            this.taskEntity.setSize(size);
+        } catch (NetException e) {
+            throw new DownloadException(e);
+        } finally {
+            client.close();
+        }
+    }
 
 }
