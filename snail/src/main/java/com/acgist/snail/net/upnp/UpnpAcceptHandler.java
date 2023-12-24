b9d9f6ba69f8f781591e32445ae1385ca5c0efc3
acgist <289547414@qq.com>
2023-12-25 07:15:02 +0800

    [*] 日常优化

--- snail/src/main/java/com/acgist/snail/net/upnp/UpnpAcceptHandler.java
+++ snail/src/main/java/com/acgist/snail/net/upnp/UpnpAcceptHandler.java
@@ -8,34 +8,34 @@ import com.acgist.snail.net.UdpAcceptHandler;
 import com.acgist.snail.net.UdpMessageHandler;
 
 /**
- * <p>UPNP消息接收代理</p>
+ * UPNP消息接收代理
  * 
  * @author acgist
  */
 public final class UpnpAcceptHandler extends UdpAcceptHandler {
 
-	private static final UpnpAcceptHandler INSTANCE = new UpnpAcceptHandler();
-	
-	public static final UpnpAcceptHandler getInstance() {
-		return INSTANCE;
-	}
-	
-	/**
-	 * <p>消息代理</p>
-	 */
-	private final UpnpMessageHandler upnpMessageHandler = new UpnpMessageHandler();
-	
-	private UpnpAcceptHandler() {
-	}
+    private static final UpnpAcceptHandler INSTANCE = new UpnpAcceptHandler();
+    
+    public static final UpnpAcceptHandler getInstance() {
+        return INSTANCE;
+    }
+    
+    /**
+     * 消息代理
+     */
+    private final UpnpMessageHandler upnpMessageHandler = new UpnpMessageHandler();
+    
+    private UpnpAcceptHandler() {
+    }
 
-	@Override
-	public void handle(DatagramChannel channel) {
-		this.upnpMessageHandler.handle(channel);
-	}
-	
-	@Override
-	public UdpMessageHandler messageHandler(ByteBuffer buffer, InetSocketAddress socketAddress) {
-		return this.upnpMessageHandler;
-	}
+    @Override
+    public void handle(DatagramChannel channel) {
+        this.upnpMessageHandler.handle(channel);
+    }
+    
+    @Override
+    public UdpMessageHandler messageHandler(ByteBuffer buffer, InetSocketAddress socketAddress) {
+        return this.upnpMessageHandler;
+    }
 
 }
--- snail/src/main/java/com/acgist/snail/net/upnp/UpnpClient.java
+++ snail/src/main/java/com/acgist/snail/net/upnp/UpnpClient.java
@@ -11,60 +11,60 @@ import com.acgist.snail.net.UdpClient;
 import com.acgist.snail.utils.NetUtils;
 
 /**
- * <p>UPNP客户端</p>
+ * UPNP客户端
  * 
  * @author acgist
  */
 public final class UpnpClient extends UdpClient<UpnpMessageHandler> {
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(UpnpClient.class);
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(UpnpClient.class);
 
-	/**
-	 * <p>M-SEARCH协议：{@value}</p>
-	 */
-	private static final String PROTOCOL = "M-SEARCH * HTTP/1.1";
-	
-	/**
-	 * @param socketAddress 地址
-	 */
-	private UpnpClient(InetSocketAddress socketAddress) {
-		super("UPNP Client", new UpnpMessageHandler(socketAddress));
-	}
-	
-	public static final UpnpClient newInstance() {
-		return new UpnpClient(NetUtils.buildSocketAddress(UpnpServer.upnpHost(), UpnpServer.UPNP_PORT));
-	}
-	
-	@Override
-	public boolean open() {
-		return this.open(UpnpServer.getInstance().getChannel());
-	}
+    /**
+     * M-SEARCH协议：{@value}
+     */
+    private static final String PROTOCOL = "M-SEARCH * HTTP/1.1";
+    
+    /**
+     * @param socketAddress 地址
+     */
+    private UpnpClient(InetSocketAddress socketAddress) {
+        super("UPNP Client", new UpnpMessageHandler(socketAddress));
+    }
+    
+    public static final UpnpClient newInstance() {
+        return new UpnpClient(NetUtils.buildSocketAddress(UpnpServer.upnpHost(), UpnpServer.UPNP_PORT));
+    }
+    
+    @Override
+    public boolean open() {
+        return this.open(UpnpServer.getInstance().getChannel());
+    }
 
-	/**
-	 * <p>发送M-SEARCH消息</p>
-	 */
-	public void mSearch() {
-		LOGGER.debug("发送M-SEARCH消息");
-		try {
-			this.send(this.buildMSearch());
-		} catch (NetException e) {
-			LOGGER.error("发送M-SEARCH消息异常", e);
-		}
-	}
-	
-	/**
-	 * <p>新建M-SEARCH消息</p>
-	 * 
-	 * @return 消息
-	 */
-	private String buildMSearch() {
-		final HeaderWrapper builder = HeaderWrapper.newBuilder(PROTOCOL);
-		builder
-			.setHeader("HOST", SymbolConfig.Symbol.COLON.join(UpnpServer.upnpHost(), UpnpServer.UPNP_PORT))
-			.setHeader("ST", UpnpServer.UPNP_ROOT_DEVICE)
-			.setHeader("MAN", "\"ssdp:discover\"")
-			.setHeader("MX", "3");
-		return builder.build();
-	}
+    /**
+     * 发送M-SEARCH消息
+     */
+    public void mSearch() {
+        LOGGER.debug("发送M-SEARCH消息");
+        try {
+            this.send(this.buildMSearch());
+        } catch (NetException e) {
+            LOGGER.error("发送M-SEARCH消息异常", e);
+        }
+    }
+    
+    /**
+     * 新建M-SEARCH消息
+     * 
+     * @return 消息
+     */
+    private String buildMSearch() {
+        final HeaderWrapper builder = HeaderWrapper.newBuilder(PROTOCOL);
+        builder
+            .setHeader("HOST", SymbolConfig.Symbol.COLON.join(UpnpServer.upnpHost(), UpnpServer.UPNP_PORT))
+            .setHeader("ST", UpnpServer.UPNP_ROOT_DEVICE)
+            .setHeader("MAN", "\"ssdp:discover\"")
+            .setHeader("MX", "3");
+        return builder.build();
+    }
 
 }
